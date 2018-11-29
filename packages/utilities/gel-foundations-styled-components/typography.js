14d4708fc4f5b75f3c92836bbd2266772d7c9dc1
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-11-29 12:02:17 +0000

    Update to use new constant names for media queries

--- packages/utilities/gel-foundations-styled-components/typography.js
+++ packages/utilities/gel-foundations-styled-components/typography.js
@@ -4,7 +4,7 @@ import MEDIA_QUERY_TYPOGRAPHY from '@bbc/gel-constants/breakpoints';
 export const GEL_LONG_PRIMER = css`
   font-size: 0.9375em;
   line-height: 1.125rem;
-  ${MEDIA_QUERY_TYPOGRAPHY.laptopAndLarger} {
+  ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
     font-size: 0.875em;
   }
 `;
@@ -13,7 +13,7 @@ export const GEL_LONG_PRIMER = css`
 export const GEL_BODY_COPY = css`
   font-size: 0.9375em;
   line-height: 1.25rem;
-  ${MEDIA_QUERY_TYPOGRAPHY.smartPhoneAndLarger} {
+  ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_AND_LARGER} {
     font-size: 1em;
     line-height: 1.375rem;
   }
@@ -22,10 +22,10 @@ export const GEL_BODY_COPY = css`
 export const GEL_BREVIER = css`
   font-size: 0.875em;
   line-height: 1rem;
-  ${MEDIA_QUERY_TYPOGRAPHY.smartPhoneOnly} {
+  ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_ONLY} {
     line-height: 1.125rem;
   }
-  ${MEDIA_QUERY_TYPOGRAPHY.laptopAndLarger} {
+  ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
     font-size: 0.8125em;
   }
 `;
@@ -34,11 +34,11 @@ export const GEL_BREVIER = css`
 export const GEL_CANON = css`
   font-size: 1.75em;
   line-height: 2rem;
-  ${MEDIA_QUERY_TYPOGRAPHY.smartPhoneOnly} {
+  ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_ONLY} {
     font-size: 2em;
     line-height: 2.25rem;
   }
-  ${MEDIA_QUERY_TYPOGRAPHY.laptopAndLarger} {
+  ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
     font-size: 2.75em;
     line-height: 3rem;
   }
@@ -48,11 +48,11 @@ export const GEL_CANON = css`
 export const GEL_TRAFALGAR = css`
   font-size: 1.25em;
   line-height: 1.5rem;
-  ${MEDIA_QUERY_TYPOGRAPHY.smartPhoneOnly} {
+  ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_ONLY} {
     font-size: 1.5em;
     line-height: 1.75rem;
   }
-  ${MEDIA_QUERY_TYPOGRAPHY.laptopAndLarger} {
+  ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
     font-size: 2em;
     line-height: 2.25rem;
   }
