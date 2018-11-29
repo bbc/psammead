bea2c5df74589aa6e161c929fad7331f8c43df0a
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-11-29 08:47:10 +0000

    clean up script

--- .scripts/sanity.js
+++ .scripts/sanity.js
@@ -1,10 +1,65 @@
 const shell = require('shelljs');
+const semver = require('semver');
+const chalk = require('chalk');
+const fs = require('fs');
+let good = true;
+
+const uniq = arr => Array.from(new Set(arr));
 
 const getChangedPackages = () => {
-	const diff = shell.exec('git diff --raw packages', {silent:true}).stdout;
+	const diff = shell.exec('git diff --raw origin/latest -- packages', {silent:true}).stdout;
 	const diffArr = diff.split('\n').filter(el => el.length);
 	var packageMatch = /.*packages\/(.*)\//;
-	return diffArr.map(el => packageMatch.exec(el)[1]);
+	return uniq(diffArr.map(el => packageMatch.exec(el)[1]));
 }
 
-console.log(getChangedPackages());
+const fileExists = (packageName, file) => {
+	if (!fs.existsSync(`packages/${packageName}/${file}`)) {
+		console.log(chalk.red(`OH NO! Package ${packageName} doesnt have a ${file}!`));
+		good = false;
+		return false;
+	}
+
+	return true;
+};
+
+const checkVersionBump = (packageName) =>  {
+	const diff = shell.exec(`git diff origin/latest -- packages/${packageName}/package.json`, {silent:true}).stdout;
+	const versionMatch = /"version": "(\d*.\d*.\d*)/g;
+	const versions = [];
+
+	diff.replace(versionMatch, (match, g1) => versions.push(g1));
+
+	if(versions.length !== 2 || !semver.lt(...versions)){
+		console.log(chalk.red(`OH NO! Package ${packageName} doesnt have a version bump!`));
+		good = false;
+	}
+}
+
+const checkFileChange = (packageName, file) => {
+	const diff = shell.exec(`git diff origin/latest -- packages/${packageName}/${file}`, {silent:true}).stdout;
+	
+	if(!diff){
+		console.log(chalk.red(`OH NO! Package ${packageName} doesnt have an updated ${file}!`));
+		good = false;
+	}
+};
+
+const changedPackages = getChangedPackages();
+
+changedPackages.forEach(packageName => {
+	if(fileExists(packageName, 'package.json')){
+		checkVersionBump(packageName);
+	}
+
+	if(fileExists(packageName, 'CHANGELOG.md')){
+		checkFileChange(packageName, 'CHANGELOG.md');
+	}
+
+	fileExists(packageName, 'package-lock.json');
+	fileExists(packageName, 'README.md');
+});
+
+if (!good) {
+	process.exit(1);
+}
--- packages/utilities/psammead-styles/CHANGELOG.md
+++ packages/utilities/psammead-styles/CHANGELOG.md
@@ -2,6 +2,7 @@
 
 | Version | Description |
 |---------|-------------|
+awdaw
 | 0.1.2 | [PR#72](https://github.com/BBC-News/psammead/pull/72) Update Readme's usage examples |
 | 0.1.1 | [PR#67](https://github.com/BBC-News/psammead/pull/67) Update psammead-styles' DOD |
 | 0.1.0 | [PR#44](https://github.com/BBC-News/psammead/pull/44) Initial creation of package |
