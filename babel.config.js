59916818f66233163d5d451d6b9fe2ba9aba78aa
ChrisBAshton <chrisashtonweb@gmail.com>
2018-12-04 13:49:50 +0000

    adds dependencies to get Jest working with Babel v7

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
+const presets = ['@babel/preset-env', '@babel/preset-react'];
+
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets,
+  env: {
+    // used by Jest
+    test: {
+      presets,
+    },
+  },
 };
