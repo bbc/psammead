d2522dc318266b135a579ee9a4a398a3c9df6963
Ben Newcombe <bcmn96@gmail.com>
2018-12-05 16:33:23 +0000

    Simplify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
-
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
-  env: {
-    test: {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
 };
