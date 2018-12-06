db4d12788f64199aee44d2a6c5bde3552f109acc
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-12-06 12:51:07 +0000

    WIP - update test dep

--- packages/components/psammead-brand/index.test.jsx
+++ packages/components/psammead-brand/index.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import Brand from './index';
-
-import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
 describe('Brand', () => {
   shouldMatchSnapshot(
