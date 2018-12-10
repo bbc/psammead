a7936ae8998a200c5532800683dc7d2ead1a292c
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-12-10 07:48:32 +0000

    Add stylelint comment

--- packages/components/psammead-sitewide-links/src/Link/index.jsx
+++ packages/components/psammead-sitewide-links/src/Link/index.jsx
@@ -13,8 +13,8 @@ const StyledLink = styled.a`
 `;
 
 const StyleLinkText = styled.span`
-  ${StyledLink}:hover &,
-  ${StyledLink}:focus & {
+  ${/* sc-selector */ StyledLink}:hover &,
+  ${/* sc-selector */ StyledLink}:focus & {
     padding-bottom: 2px;
     border-bottom: 2px solid ${C_WHITE};
   }
