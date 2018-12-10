0806ae4ad6a054908f7006b890054fdd76ce579e
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-12-10 17:31:59 +0000

    Update script

--- scripts/publish.js
+++ scripts/publish.js
@@ -22,24 +22,23 @@ const getPackages = () =>
 const publish = (packageDir, packageJson) => {
   console.log(chalk.blue(`Publishing ${packageJson.name}`));
 
-  const execute = exec(`npm publish ${packageDir} --access public --dry-run`, {
-    silent: true,
-  });
+  const execute = exec(
+    `npm publish ${packageDir} --access public`,
+    {
+      silent: true,
+    },
+  );
 
   if (execute.code !== 0) {
     console.log(
-      chalk.red(
-        `Error publishing ${packageJson.name} version ${packageJson.version}`,
-      ),
+      chalk.red(`Error publishing ${packageJson.name}@${packageJson.version}`),
     );
     console.log(chalk.red(execute.stderr));
     attempted.failure.push(`${packageJson.name}@${packageJson.version}`);
   } else {
     console.log(
       chalk.green(
-        `Successfully published ${packageJson.name} version ${
-          packageJson.version
-        }`,
+        `Successfully published ${packageJson.name}@${packageJson.version}`,
       ),
     );
     attempted.success.push(`${packageJson.name}@${packageJson.version}`);
@@ -69,7 +68,7 @@ const report = () => {
   }
 
   if (failureCount) {
-    console.log(chalk.underline.red('\n Failed'));
+    console.log(chalk.underline.red('\nFailed'));
     attempted.failure.forEach(published => console.log(chalk.red(published)));
   }
 };
