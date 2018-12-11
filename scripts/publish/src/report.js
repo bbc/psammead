ea17812a4d3b6efab401688238c991fb3b684ab7
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-12-11 14:10:56 +0000

    Add slack notification logic

--- scripts/publish/src/report.js
+++ scripts/publish/src/report.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const chalk = require('chalk');
+const slackNotification = require('./slackNotification');
 
 module.exports = attempted => {
   const successfulCount = attempted.success.length;
@@ -20,11 +21,17 @@ module.exports = attempted => {
 
   if (successfulCount) {
     console.log(chalk.underline.green('\nSuccessful'));
-    attempted.success.forEach(published => console.log(chalk.green(published)));
+    attempted.success.forEach(published => {
+      console.log(chalk.red(published));
+      slackNotification(published, true);
+    });
   }
 
   if (failureCount) {
     console.log(chalk.underline.red('\nFailed'));
-    attempted.failure.forEach(published => console.log(chalk.red(published)));
+    attempted.failure.forEach(published => {
+      slackNotification(published, false);
+      console.log(chalk.red(published));
+    });
   }
 };
