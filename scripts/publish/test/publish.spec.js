ee242a6b054ce2148dbca646c7de1db4509d89dd
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-12-12 19:19:18 +0000

    Add working slack notification tests

--- scripts/publish/test/publish.spec.js
+++ scripts/publish/test/publish.spec.js
@@ -2,37 +2,59 @@
 /* eslint-disable no-console */
 const stripAnsi = require('strip-ansi');
 
+const getSuccessfulShellJsMock = () => {
+  jest.mock('shelljs', () => ({
+    exec: jest.fn(() => ({ code: 0 })),
+  }));
+
+  return require('shelljs');
+};
+
+const getFailingShellJsMock = () => {
+  jest.mock('shelljs', () => ({
+    exec: jest.fn(() => ({ code: 1 })),
+  }));
+
+  return require('shelljs');
+};
+
+const getSlackNotificationMock = () => {
+  jest.mock('../src/slackNotification');
+  return require('../src/slackNotification');
+};
+
+const packageJson = {
+  name: '@foo/psammead-foobar',
+  version: '0.1.2',
+};
+
+let attempted = {};
+
 describe(`Publish Script - publish`, () => {
   beforeEach(() => {
     jest.resetModules();
-  });
 
-  it('runs correct publish command and publish is successful ', () => {
     console.log = jest.fn();
 
-    jest.mock('shelljs', () => ({
-      exec: jest.fn(() => ({ code: 0 })),
-    }));
+    process.argv = process.argv.filter(v => !v.includes('--otp'));
 
-    const shelljs = require('shelljs');
-
-    jest.mock('../src/slackNotification');
-
-    const slackNotification = require('../src/slackNotification');
-
-    const publish = require('../src/publish');
-
-    const packageJson = {
-      name: '@foo/psammead-foobar',
-      version: '0.1.2',
-    };
-
-    const attempted = {
+    attempted = {
       success: [],
       failure: [],
     };
+  });
+
+  it('runs correct publish command and publish is successful ', () => {
+    const shelljs = getSuccessfulShellJsMock();
+    const slackNotification = getSlackNotificationMock();
+    const publish = require('../src/publish');
 
     publish('/foo/bar', packageJson, attempted);
+
+    expect(shelljs.exec).toHaveBeenCalledWith(
+      'npm publish /foo/bar --access public --tag latest',
+      { silent: true },
+    );
 
     const expectedOutput = [
       'Publishing @foo/psammead-foobar',
@@ -52,24 +74,52 @@ describe(`Publish Script - publish`, () => {
       '@foo/psammead-foobar@0.1.2',
       true,
     );
+  });
+
+  it('runs correct publish command and publish is unsuccessful ', () => {
+    const shelljs = getFailingShellJsMock();
+    const slackNotification = getSlackNotificationMock();
+    const publish = require('../src/publish');
+
+    publish('/foo/bar', packageJson, attempted);
 
     expect(shelljs.exec).toHaveBeenCalledWith(
       'npm publish /foo/bar --access public --tag latest',
       { silent: true },
     );
-  });
 
-  it('runs correct publish command and publish is unsuccessful ', () => {
-    // Exec is called with expected command
-    // Console log 'Successfully published ${packageReleaseTag}'
-    // attempted.success has 1 more value
-    // slackNotification is called with tag and true
+    const expectedOutput = [
+      'Publishing @foo/psammead-foobar',
+      'Error publishing @foo/psammead-foobar@0.1.2',
+    ];
+
+    expectedOutput.forEach((element, index) => {
+      expect(stripAnsi(console.log.mock.calls[index][0])).toEqual(
+        expect.stringContaining(element),
+      );
+    });
+
+    expect(attempted.failure.length).toEqual(1);
+    expect(attempted.failure[0]).toEqual('@foo/psammead-foobar@0.1.2');
+
+    expect(slackNotification).toHaveBeenCalledWith(
+      '@foo/psammead-foobar@0.1.2',
+      false,
+    );
   });
 
   it('runs correct publish command when opt argument is set and publish is successful', () => {
-    // Exec is called with expected command
-    // Console log 'Error publishing ${packageReleaseTag}'
-    // attempted.failure has 1 more value
-    // slackNotification is called with tag and false
+    const shelljs = getFailingShellJsMock();
+
+    process.argv.push('--otp=123456');
+
+    const publish = require('../src/publish');
+
+    publish('/foo/bar', packageJson, attempted);
+
+    expect(shelljs.exec).toHaveBeenCalledWith(
+      'npm publish /foo/bar --access public --tag latest --otp 123456',
+      { silent: true },
+    );
   });
 });
