ddd308687532f7066d8ba66d259f23f962feda64
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-12-17 14:02:58 +0000

    create a helper for testing exported consts from packages

--- packages/utilities/psammead-test-helpers/src/index.js
+++ packages/utilities/psammead-test-helpers/src/index.js
@@ -30,3 +30,33 @@ export const isNull = (title, component) => {
     expect(tree).toBeNull();
   });
 };
+
+export const testUtilityPackages = (
+  acutalExports,
+  expectedExports,
+  utilityName,
+) => {
+  let count = 0;
+  const utilities = Object.keys(acutalExports);
+
+  utilities.forEach(utility => {
+    const actualExportsByName = Object.keys(acutalExports[utility]);
+
+    actualExportsByName.forEach(actualExportName => {
+      if (!(actualExportName in expectedExports[utility])) {
+        // eslint-disable-next-line no-console
+        console.log(
+          `No expected export value declared in unit tests for ${utilityName}/${utility}.js - ${actualExportName}`,
+        );
+      }
+
+      const actualExportValue = acutalExports[utility][actualExportName];
+      const expectedExport = expectedExports[utility][actualExportName];
+      const typeCheck = typeof actualExportValue === expectedExport; // eslint-disable-line valid-typeof
+
+      expect(typeCheck).toBe(true);
+      count += 1;
+    });
+  });
+  console.log(`Tested all ${count} exports from utilities/${utilityName}`); // eslint-disable-line no-console
+};
