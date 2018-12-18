ef0184cad5cb1c102e3e1bace823bcb1f9fee0d8
radiocontrolled <radiocontrolled@gmail.com>
2018-12-18 11:55:41 +0000

    psammead-paragraph

--- packages/components/psammead-paragraph/src/index.jsx
+++ packages/components/psammead-paragraph/src/index.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { C_STORM } from '@bbc/psammead-styles/colours';
 import { FF_NEWS_SANS_REG } from '@bbc/psammead-styles/fonts';
-import { GEL_SPACING_DBL } from '@bbc/gel-constants/spacings';
-import { GEL_BODY_COPY } from '@bbc/gel-foundations-styled-components/typography';
+import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import { GEL_BODY_COPY } from '@bbc/gel-foundations/typography';
 
 const Paragraph = styled.p`
   color: ${C_STORM};
