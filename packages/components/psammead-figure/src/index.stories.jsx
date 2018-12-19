c6c152b63e3d0905ddc0ec0e6de5195a2aad3f55
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-12-19 12:30:45 +0000

    storybook story

---
+++ packages/components/psammead-figure/src/index.stories.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withReadme } from 'storybook-readme';
+import Image from '@bbc/psammead-image';
+import Readme from '../README.md';
+import Figure from './index';
+
+const imageAlt =
+  'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.';
+const imageSrc =
+  'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png';
+const imageWidth = 853;
+const imageHeight = 1067;
+
+storiesOf('Figure', module)
+  .addDecorator(withReadme(Readme))
+  .add('default', () => (
+    <Figure>
+      <Image
+        alt={imageAlt}
+        src={imageSrc}
+        width={imageWidth}
+        height={imageHeight}
+      />
+    </Figure>
+  ));
