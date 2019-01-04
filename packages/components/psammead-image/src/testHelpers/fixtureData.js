4b3f670e2001c94918f7e6abb1043b9463ec35c6
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-01-04 12:35:44 +0000

    Update to dynamic fixture srcset

--- packages/components/psammead-image/src/testHelpers/fixtureData.js
+++ packages/components/psammead-image/src/testHelpers/fixtureData.js
@@ -1,10 +1,13 @@
+const sizes = [300, 450, 600, 1000, 1200];
+const landscapeImageUrl = `https://ichef.bbci.co.uk/news/[WIDTH]/cpsprodpb/7098/production/_104842882_students.jpg`;
+const portraitImageUrl = `https://ichef.bbci.co.uk/news/[WIDTH]/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png`;
+const squareImageUrl = `https://ichef.bbci.co.uk/news/[WIDTH]/cpsprodpb/114FE/production/_104801907_79010.jpg`;
+const customImageUrl = `https://ichef.bbci.co.uk/news/[WIDTH]/cpsprodpb/164BB/production/_104032319_03270dcc-9dda-4bd4-96a0-db89f6b915ae.jpg`;
+
 export const landscape = {
   alt: 'Student sitting an exam',
-  src:
-    'https://ichef.bbci.co.uk/news/1024/cpsprodpb/7098/production/_104842882_students.jpg',
-  srcset:
-    'https://ichef.bbci.co.uk/news/1024/cpsprodpb/7098/production/_104842882_students.jpg 1024w, https://ichef.bbci.co.uk/news/1024/cpsprodpb/7098/production/_104842882_students.jpg 1024w, https://ichef.bbci.co.uk/news/1024/cpsprodpb/7098/production/_104842882_students.jpg 1024w, https://ichef.bbci.co.uk/news/1024/cpsprodpb/7098/production/_104842882_students.jpg 1024w, https://ichef.bbci.co.uk/news/1024/cpsprodpb/7098/production/_104842882_students.jpg 1024w',
-
+  src: landscapeImageUrl,
+  srcset: sizes.map(size => landscapeImageUrl.replace('[WIDTH]', size)),
   width: 1024,
   height: 576,
 };
@@ -12,25 +15,24 @@ export const landscape = {
 export const portrait = {
   alt:
     'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.',
-  src:
-    'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png',
+  src: portraitImageUrl,
+  srcset: sizes.map(size => portraitImageUrl.replace('[WIDTH]', size)),
   width: 640,
   height: 1138,
 };
 
 export const square = {
   alt: 'Tracks through the snow',
-  src:
-    'https://ichef.bbci.co.uk/news/1024/cpsprodpb/114FE/production/_104801907_79010.jpg',
+  src: squareImageUrl,
+  srcset: sizes.map(size => squareImageUrl.replace('[WIDTH]', size)),
   width: 1024,
   height: 1024,
 };
 
 export const custom = {
   alt: 'By Elisa Decker, from her series "Sidewalk"',
-  src:
-    'https://ichef.bbci.co.uk/news/445/cpsprodpb/164BB/production/_104032319_03270dcc-9dda-4bd4-96a0-db89f6b915ae.jpg',
-
+  src: customImageUrl,
+  srcset: sizes.map(size => customImageUrl.replace('[WIDTH]', size)),
   width: 445,
   height: 547,
 };
