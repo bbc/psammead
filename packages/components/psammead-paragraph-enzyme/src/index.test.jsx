58f245a9bb627710a22b68cc4ac262db0073a3c0
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-07 12:28:19 +0000

    mount shallow and render tests

--- packages/components/psammead-paragraph-enzyme/src/index.test.jsx
+++ packages/components/psammead-paragraph-enzyme/src/index.test.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-// Enzyme adapters that provides compatibility with React 16.x:
 import Enzyme from 'enzyme';
+// Enzyme adapter provides compatibility with React 16.x:
 import Adapter from 'enzyme-adapter-react-16';
 import Paragraph from './index';
 
 // configure enzyme to use the adapter you want it to use:
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('Paragraph Enzyme test', () => {
-  it('should render to static HTML', () => {
-    expect(
-      Enzyme.render(<Paragraph>This is text in a paragraph.</Paragraph>).text(),
-    ).toEqual('This is text in a paragraph.');
+const p = <Paragraph>This is text in a paragraph.</Paragraph>;
+
+// This suite uses Jest as its test runner, and is thus run from the root level `npm test`:
+describe('Mounting, shallow and render tests', () => {
+  describe('Paragraph tested with Enzyme mount', () => {
+    // not sure what this does
+    it('should mount a paragraph without throwing an error', () => {
+      const wrapper = Enzyme.mount(p);
+      // .c0
+      expect(wrapper.length).toBe(1);
+    });
+  });
+
+  describe('Paragraph tested with Enzyme shallow render', () => {
+    it('should render correctly with no props', () => {
+      const component = Enzyme.shallow(<Paragraph />);
+      expect(component).toMatchSnapshot();
+    });
+
+    it('should render correctly with text', () => {
+      const component = Enzyme.shallow(p);
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('Paragraph tested with Enzyme rendering to static HTML', () => {
+    it('should render to static HTML', () => {
+      expect(Enzyme.render(p).text()).toEqual('This is text in a paragraph.');
+    });
   });
 });
