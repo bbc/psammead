1a561e5f7a4728995b0a91a8a3d035eb505fc86c
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-07 15:16:03 +0000

    add psammead-paragraph-react-testing-library

---
+++ packages/components/psammead-paragraph-react-testing-library/src/index.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { C_STORM } from '@bbc/psammead-styles/colours';
+import { FF_NEWS_SANS_REG } from '@bbc/psammead-styles/fonts';
+import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import { GEL_BODY_COPY } from '@bbc/gel-foundations/typography';
+
+const Paragraph = styled.p`
+  color: ${C_STORM};
+  font-family: ${FF_NEWS_SANS_REG};
+  padding-bottom: ${GEL_SPACING_DBL};
+  margin: 0; /* Reset */
+  ${GEL_BODY_COPY};
+`;
+
+export default Paragraph;
