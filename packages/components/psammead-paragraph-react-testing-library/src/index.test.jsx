e832a626b1cfa79f33e95100009c5f9b2da85ca0
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-10 14:54:33 +0000

    saving work

---
+++ packages/components/psammead-paragraph-react-testing-library/src/index.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import react-testing-library methods
+import { render } from 'react-testing-library';
+
+import Paragraph from './index';
+
+// The render method renders a React element into the DOM and returns utility functions for testing the component.
+const { container } = render(
+  <Paragraph>This is text in a paragraph.</Paragraph>,
+);
+
+// react-testing-library can use Jest as its test runner
+// therefore there is no 'test' script in the local packaage.json
+describe('Paragraph react-testing-library suite', () => {
+  it('runs a snapshot test', () => {
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
