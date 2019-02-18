cde599d58a430252a5e94a0d111a5c1516457628
Ben Newcombe <bcmn96@gmail.com>
2019-02-18 14:14:09 +0000

    Change colour of paragraph text

--- packages/components/psammead-paragraph/src/index.jsx
+++ packages/components/psammead-paragraph/src/index.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { C_STORM } from '@bbc/psammead-styles/colours';
+import { C_SHADOW } from '@bbc/psammead-styles/colours';
 import { FF_NEWS_SANS_REG } from '@bbc/psammead-styles/fonts';
 import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import { GEL_BODY_COPY } from '@bbc/gel-foundations/typography';
 
 const Paragraph = styled.p`
-  color: ${C_STORM};
+  color: ${C_SHADOW};
   font-family: ${FF_NEWS_SANS_REG};
   padding-bottom: ${GEL_SPACING_DBL};
   margin: 0; /* Reset */
