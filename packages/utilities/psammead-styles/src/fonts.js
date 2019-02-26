1dc6e604f5846aefd64300004262c741c347ea2e
Ben Newcombe <bcmn96@gmail.com>
2019-02-26 14:18:37 +0000

    Change occurrences of font-weight: bold to font-weight: 700

--- packages/utilities/psammead-styles/src/fonts.js
+++ packages/utilities/psammead-styles/src/fonts.js
@@ -20,7 +20,7 @@ export const T_REITH_SERIF_BOLD = `
   @font-face {
     font-family: "ReithSerif";
     src: url("${baseFontUrl}BBCReithSerif_W_Bd.woff2") format("woff2"), url("${baseFontUrl}BBCReithSerif_W_Bd.woff") format("woff");
-    font-weight: bold;
+    font-weight: 700;
     font-display: optional;
   }`;
 
@@ -28,7 +28,7 @@ export const T_REITH_SERIF_BOLD_ITALIC = `
   @font-face {
     font-family: "ReithSerif";
     src: url("${baseFontUrl}BBCReithSerif_W_BdIt.woff2") format("woff2"), url("${baseFontUrl}BBCReithSerif_W_BdIt.woff") format("woff");
-    font-weight: bold;
+    font-weight: 700;
     font-style: italic;
     font-display: optional;
   }`;
@@ -104,7 +104,7 @@ export const T_REITH_SANS_BOLD = `
   @font-face {
     font-family: "ReithSans";
     src: url("${baseFontUrl}BBCReithSans_W_Bd.woff2") format("woff2"), url("${baseFontUrl}BBCReithSans_W_Bd.woff") format("woff");
-    font-weight: bold;
+    font-weight: 700;
     font-display: optional;
   }`;
 
@@ -112,7 +112,7 @@ export const T_REITH_SANS_BOLD_ITALIC = `
   @font-face {
     font-family: "ReithSans";
     src: url("${baseFontUrl}BBCReithSans_W_BdIt.woff2") format("woff2"), url("${baseFontUrl}BBCReithSans_W_BdIt.woff") format("woff");
-    font-weight: bold;
+    font-weight: 700;
     font-style: italic;
     font-display: optional;
   }`;
@@ -179,7 +179,7 @@ export const T_REITH_SANS_CONDENSED_REGULAR = `
 export const T_REITH_SANS_CONDENSED_BOLD = `
     @font-face {
         font-family: "ReithSansCondensed";
-        font-weight: bold;
+        font-weight: 700;
         src: url("${baseFontUrl}BBCReithSansCd_W_Bd.woff2") format("woff2"), url("${baseFontUrl}BBCReithSansCd_W_Bd.woff") format("woff");
         font-display: optional;
     }`;
