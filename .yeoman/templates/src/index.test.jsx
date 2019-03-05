6bb377507ac93135f68be2c38b7015f2271fad1c
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-05 22:42:26 +0000

    :rocket: Update template for linting

--- .yeoman/templates/src/index.test.jsx
+++ .yeoman/templates/src/index.test.jsx
@@ -3,8 +3,6 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import <%= componentName %> from './index';
 
 describe('<%= componentName %>', () => {
-  shouldMatchSnapshot(
-    'should render correctly',
-    <<%= componentName %> />,
-  );
+  shouldMatchSnapshot('should render correctly', <<%= componentName %> />);
 });
+
