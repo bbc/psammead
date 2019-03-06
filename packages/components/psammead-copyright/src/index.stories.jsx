f2619bc26818165d19193404094532793ff9c55b
Ross Gaskell <ross.gaskell@bbc.co.uk>
2019-03-06 11:48:36 +0000

    Added visually hidden text to component story

--- packages/components/psammead-copyright/src/index.stories.jsx
+++ packages/components/psammead-copyright/src/index.stories.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withReadme } from 'storybook-readme';
+import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
 import Readme from '../README.md';
 import Copyright from './index';
 
 storiesOf('Copyright', module)
   .addDecorator(withReadme(Readme))
-  .add('default', () => <Copyright>Getty Images</Copyright>);
+  .add('default', () => <Copyright>Getty Images</Copyright>)
+  .add('with visually hidden text', () => (
+    <Copyright>
+      <VisuallyHiddenText>Offscreen text </VisuallyHiddenText>
+      Getty Images
+    </Copyright>
+  ));
