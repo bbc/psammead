8054c793c659dc68a00d0312e8936b6b6a8904c5
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-03-07 16:27:29 +0000

    add a Figure story with fuller example

--- packages/components/psammead-figure/src/index.stories.jsx
+++ packages/components/psammead-figure/src/index.stories.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withReadme } from 'storybook-readme';
+import Caption from '@bbc/psammead-caption';
+import Copyright from '@bbc/psammead-copyright';
 import Image from '@bbc/psammead-image';
+import ImagePlaceholder from '@bbc/psammead-image-placeholder';
 import Readme from '../README.md';
 import Figure from '.';
 
@@ -10,10 +13,21 @@ const imageAlt =
 const imageSrc =
   'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png';
 const imageWidth = 853;
+const imageRatio = 125;
+
 storiesOf('Figure', module)
   .addDecorator(withReadme(Readme))
   .add('containing Image', () => (
     <Figure>
       <Image alt={imageAlt} src={imageSrc} width={imageWidth} />
+    </Figure>
+  ))
+  .add('containing Image, ImagePlaceholder, Copyright and Caption', () => (
+    <Figure>
+      <ImagePlaceholder ratio={imageRatio}>
+        <Image alt={imageAlt} src={imageSrc} width={imageWidth} />
+        <Copyright>Copyright</Copyright>
+      </ImagePlaceholder>
+      <Caption>Caption</Caption>
     </Figure>
   ));
