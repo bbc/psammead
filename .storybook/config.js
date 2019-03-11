156ce1c84206f45787c50a9185a59ffd4f0a55ff
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-11 08:35:54 +0000

    Damn spelling

--- .storybook/config.js
+++ .storybook/config.js
@@ -19,7 +19,7 @@ import Helmet from 'react-helmet';
 
 const theme = create({
   base: 'light',
-  brandTitle: 'BBC Pssammead',
+  brandTitle: 'BBC Psammead',
   brandUrl: 'https://github.com/bbc/psammead',
   brandImage:
     'https://user-images.githubusercontent.com/11341355/54079666-af202780-42d8-11e9-9108-e47ea27fddc5.png',
