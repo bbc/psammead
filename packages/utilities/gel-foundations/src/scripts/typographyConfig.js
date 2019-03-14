55b1ba3dc265af7ec8be06ab3b4ef51ff580c4a6
Denis Hernandez <denis.hernandz@gmail.com>
2019-03-14 15:37:04 +0000

    Add news config

--- packages/utilities/gel-foundations/src/scripts/typographyConfig.js
+++ packages/utilities/gel-foundations/src/scripts/typographyConfig.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-useless-path-segments */
+import newsConfig from '../../src/scripts/news.json';
 import arabicConfig from '../../src/scripts/arabic.json';
 import latinConfig from '../../src/scripts/latin.json';
 import mundoConfig from '../../src/scripts/mundo.json';
 
 const scriptConfig = {
+  news: newsConfig,
   arabic: arabicConfig,
   latin: latinConfig,
   mundo: mundoConfig,
