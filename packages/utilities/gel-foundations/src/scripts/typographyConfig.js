28d2ffad2146ec85897e03579cc5a2e9a3b468e0
alex-magana <alex.magana@andela.com>
2019-03-14 12:23:06 +0300

    Use script names and language codes for typography config

--- packages/utilities/gel-foundations/src/scripts/typographyConfig.js
+++ packages/utilities/gel-foundations/src/scripts/typographyConfig.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-useless-path-segments */
 import arabicConfig from '../../src/scripts/arabic.json';
 import latinConfig from '../../src/scripts/latin.json';
-import mundoConfig from '../../src/scripts/mundo.json';
+import latinEsConfig from '../../src/scripts/latin-es.json';
 
 const scriptConfig = {
   arabic: arabicConfig,
   latin: latinConfig,
-  mundo: mundoConfig,
+  latinEs: latinEsConfig,
 };
 
 export default scriptConfig;
