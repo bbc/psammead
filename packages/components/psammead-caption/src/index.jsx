7e40bb1d7cdf350bde7e2cb7250bc677e8e128f3
Sareh Heidari <sareh.heidari@gmail.com>
2019-03-18 15:02:11 +0000

    Ensure text is italic

--- packages/components/psammead-caption/src/index.jsx
+++ packages/components/psammead-caption/src/index.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import { MEDIA_QUERY_TYPOGRAPHY } from '@bbc/gel-foundations/breakpoints';
 import {
@@ -8,10 +8,17 @@ import {
 
 import { C_CLOUD_DARK } from '@bbc/psammead-styles/colours';
 
+// Defined separately since in future will need to apply
+// only when the script supports italic text
+const FS_ITALIC = css`
+  font-style: italic;
+`;
+
 const Caption = styled.figcaption`
   ${GEL_LONG_PRIMER};
   color: ${C_CLOUD_DARK};
   font-family: ${GEL_FF_REITH_SANS};
+  ${FS_ITALIC};
   padding: ${GEL_SPACING};
   width: 100%;
   ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
