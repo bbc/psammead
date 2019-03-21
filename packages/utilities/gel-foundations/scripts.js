6fd3f0657ee091c9096f11f227238d04bd439b5d
Denis Hernandez <denis.hernandz@gmail.com>
2019-03-21 09:52:54 +0000

    Tree shaking typographies

---
+++ packages/utilities/gel-foundations/scripts.js
@@ -0,0 +1,12 @@
+/* eslint-disable global-require */
+module.exports = {
+  news: require('./dist/scripts/news').default,
+  arabic: require('./dist/scripts/arabic').default,
+  burmese: require('./dist/scripts/burmese').default,
+  cjkThai: require('./dist/scripts/cjk_thai').default,
+  hindiNepaliBengali: require('./dist/scripts/hindi_nepali_bengali').default,
+  latin: require('./dist/scripts/arabic').default,
+  mundo: require('./dist/scripts/arabic').default,
+  sinhala: require('./dist/scripts/sinhala').default,
+  tamil: require('./dist/scripts/tamil').default,
+};
