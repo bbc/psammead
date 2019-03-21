b65733eba20e1b156a8aa6f283e46099eb9080ca
alex-magana <alex.magana@andela.com>
2019-03-21 17:20:36 +0300

    Add preset storybook knob text variants

--- packages/components/psammead-caption/src/index.stories.jsx
+++ packages/components/psammead-caption/src/index.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { LANGUAGE_VARIANTS } from '@bbc/psammead-storybook-helpers/text-variants';
 import InlineLink from '@bbc/psammead-inline-link';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
 import notes from '../README.md';
@@ -9,14 +10,25 @@ import Caption from '.';
 storiesOf('Caption', module)
   .addDecorator(withKnobs)
   .add(
-    'default',
+    'custom - default',
     () => <Caption>{text('Content', 'This is a caption.')}</Caption>,
     {
       notes,
     },
   )
   .add(
-    'with offscreen text',
+    'preset - default',
+    () => (
+      <Caption>
+        {select('Content', LANGUAGE_VARIANTS, 'This is a caption.')}
+      </Caption>
+    ),
+    {
+      notes,
+    },
+  )
+  .add(
+    'custom - with offscreen text',
     () => (
       <Caption>
         <VisuallyHiddenText>
@@ -28,12 +40,45 @@ storiesOf('Caption', module)
     { notes },
   )
   .add(
-    'containing an inline link',
+    'preset - with offscreen text',
+    () => (
+      <Caption>
+        <VisuallyHiddenText>
+          {select('Hidden Text', LANGUAGE_VARIANTS, 'Image caption, ')}
+        </VisuallyHiddenText>
+        {select(
+          'Content',
+          LANGUAGE_VARIANTS,
+          'This is a caption with preceding offscreen text.',
+        )}
+      </Caption>
+    ),
+    { notes },
+  )
+  .add(
+    'custom - containing an inline link',
     () => (
       <Caption>
         {text('Content', 'This is a caption ')}
         <InlineLink href="https://www.bbc.com">
           {text('Inline link', 'containing an inline link')}
+        </InlineLink>
+        .
+      </Caption>
+    ),
+    { notes },
+  )
+  .add(
+    'preset - containing an inline link',
+    () => (
+      <Caption>
+        {select('Content', LANGUAGE_VARIANTS, 'This is a caption.')}
+        <InlineLink href="https://www.bbc.com">
+          {select(
+            'Inline link',
+            LANGUAGE_VARIANTS,
+            'containing an inline link',
+          )}
         </InlineLink>
         .
       </Caption>
