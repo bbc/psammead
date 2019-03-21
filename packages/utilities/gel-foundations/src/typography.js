fcc85e745ccbb1e42ca0c5e64ff5dc1262298ede
Denis Hernandez <denis.hernandz@gmail.com>
2019-03-21 11:30:48 +0000

    Move typography function to a separate file, fix identation and update breakpoints path

--- packages/utilities/gel-foundations/src/typography.js
+++ packages/utilities/gel-foundations/src/typography.js
@@ -1,44 +1,11 @@
-import { MEDIA_QUERY_TYPOGRAPHY } from '../breakpoints';
+import { MEDIA_QUERY_TYPOGRAPHY } from './breakpoints';
+import getTypeSizes from './getTypeSizes';
 
 const fontFamilyBase = ', Helvetica, Arial, sans-serif';
 
 export const GEL_FF_REITH_SERIF = `ReithSerif${fontFamilyBase}`;
 export const GEL_FF_REITH_SANS = `ReithSans${fontFamilyBase}`;
 export const GEL_FF_REITH_SANS_COND = `ReithSansCondensed${fontFamilyBase}`;
-
-function getTypeSizes(typogStyleName, importedScript) {
-  const typographyStyle = importedScript[typogStyleName];
-
-  const GROUP_A_FONT_SIZE_PX = typographyStyle.groupA.fontSize;
-  const GROUP_A_FONT_SIZE = `${GROUP_A_FONT_SIZE_PX / 16}rem`;
-  const GROUP_A_LINE_HEIGHT_PX = typographyStyle.groupA.lineHeight;
-  const GROUP_A_LINE_HEIGHT = `${GROUP_A_LINE_HEIGHT_PX / 16}rem`;
-
-  const GROUP_B_FONT_SIZE_PX = typographyStyle.groupB.fontSize;
-  const GROUP_B_FONT_SIZE = `${GROUP_B_FONT_SIZE_PX / 16}rem`;
-  const GROUP_B_LINE_HEIGHT_PX = typographyStyle.groupB.lineHeight;
-  const GROUP_B_LINE_HEIGHT = `${GROUP_B_LINE_HEIGHT_PX / 16}rem`;
-
-  const GROUP_D_FONT_SIZE_PX = typographyStyle.groupD.fontSize;
-  const GROUP_D_FONT_SIZE = `${GROUP_D_FONT_SIZE_PX / 16}rem`;
-  const GROUP_D_LINE_HEIGHT_PX = typographyStyle.groupD.lineHeight;
-  const GROUP_D_LINE_HEIGHT = `${GROUP_D_LINE_HEIGHT_PX / 16}rem`;
-
-  return `
-        font-size: ${GROUP_A_FONT_SIZE};
-        line-height: ${GROUP_A_LINE_HEIGHT};
-   
-        ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_ONLY} {
-          font-size: ${GROUP_B_FONT_SIZE};
-          line-height: ${GROUP_B_LINE_HEIGHT};
-        }
-   
-        ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
-          font-size: ${GROUP_D_FONT_SIZE};
-          line-height: ${GROUP_D_LINE_HEIGHT};
-        }
-      `;
-}
 
 export const getCanon = script => getTypeSizes('canon', script);
 
