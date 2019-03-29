2d054a4809c317c306309aadffcbe4d53fb7ded6
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-03-29 09:03:02 +0000

    Now we don't need to test from src and dist

--- packages/utilities/psammead-storybook-helpers/index.test.jsx
+++ packages/utilities/psammead-storybook-helpers/index.test.jsx
@@ -1,6 +1,5 @@
 import { testUtilityPackages } from '@bbc/psammead-test-helpers';
-import * as index from '.';
-import * as indexFromSrc from './src/index';
+import * as index from './dist/index';
 
 const textVariantsExpectedExports = {
   LANGUAGE_VARIANTS: 'object',
@@ -20,23 +19,10 @@ const actualExports = {
   inputProvider: { inputProvider: index.inputProvider },
 };
 
-const actualExportsFromSrc = {
-  textVariants: { LANGUAGE_VARIANTS: indexFromSrc.LANGUAGE_VARIANTS },
-  inputProvider: { inputProvider: indexFromSrc.inputProvider },
-};
-
 describe('Psammead storybook helpers', () => {
   it('should test all the utility exports exist and are the correct type', () => {
     testUtilityPackages(
       actualExports,
-      expectedExports,
-      'psammead-storybook-helpers',
-    );
-  });
-
-  it('should test all the utility exports exist and are the correct type when coming from src/', () => {
-    testUtilityPackages(
-      actualExportsFromSrc,
       expectedExports,
       'psammead-storybook-helpers',
     );
