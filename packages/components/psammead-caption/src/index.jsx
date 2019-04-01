15b48aa3ed3704ad8ca6e9f8e7d2cb40af4905a9
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-04-01 13:10:42 +0100

    :bath: Update style ordering

--- packages/components/psammead-caption/src/index.jsx
+++ packages/components/psammead-caption/src/index.jsx
@@ -7,6 +7,7 @@ import {
 import {
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MAX,
+  GEL_GROUP_4_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
 import {
   GEL_LONG_PRIMER,
@@ -26,14 +27,14 @@ const Caption = styled.figcaption`
   color: ${C_CLOUD_DARK};
   font-family: ${GEL_FF_REITH_SANS};
   ${FS_ITALIC};
-  padding: ${GEL_SPACING} 0;
+  padding: ${GEL_SPACING} ${GEL_MARGIN_BELOW_400PX};
   width: 100%;
 
-  @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-    padding: ${GEL_SPACING} ${GEL_MARGIN_BELOW_400PX};
-  }
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
     padding: ${GEL_SPACING} ${GEL_MARGIN_ABOVE_400PX};
+  }
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    padding: ${GEL_SPACING} 0;
   }
 `;
 
