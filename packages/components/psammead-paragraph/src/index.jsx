181f7a322de3c5948cfd5673bede6551184c7db6
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-04-05 11:23:18 +0100

    Add script as a required prop and import the latin script in the story and test

--- packages/components/psammead-paragraph/src/index.jsx
+++ packages/components/psammead-paragraph/src/index.jsx
@@ -1,37 +1,22 @@
-import React from 'react';
 import styled from 'styled-components';
-import { node, objectOf, object } from 'prop-types';
+import { objectOf, object } from 'prop-types';
 import { C_SHADOW } from '@bbc/psammead-styles/colours';
 import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
 import {
   getBodyCopy,
   GEL_FF_REITH_SANS,
 } from '@bbc/gel-foundations/typography';
-import { latin } from '@bbc/gel-foundations/scripts';
 
-const StyledParagraph = styled.p`
-  ${props => (props.typography ? props.typography : '')};
+const Paragraph = styled.p`
+  ${props => (props.script ? getBodyCopy(props.script) : '')};
   color: ${C_SHADOW};
   font-family: ${GEL_FF_REITH_SANS};
   padding-bottom: ${GEL_SPACING_TRPL};
   margin: 0; /* Reset */
 `;
 
-const Paragraph = ({ children, script }) => {
-  const GEL_BODY_COPY = getBodyCopy(script);
-
-  return (
-    <StyledParagraph typography={GEL_BODY_COPY}>{children}</StyledParagraph>
-  );
-};
-
 Paragraph.propTypes = {
-  children: node.isRequired,
-  script: objectOf(object),
-};
-
-Paragraph.defaultProps = {
-  script: latin,
+  script: objectOf(object).isRequired,
 };
 
 export default Paragraph;
--- packages/components/psammead-paragraph/src/index.test.jsx
+++ packages/components/psammead-paragraph/src/index.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { latin } from '@bbc/gel-foundations/scripts';
 import Paragraph from './index';
 
 describe('Paragraph', () => {
   shouldMatchSnapshot(
     'should render a correctly',
-    <Paragraph>This is text in a paragraph.</Paragraph>,
+    <Paragraph script={latin}>This is text in a paragraph.</Paragraph>,
   );
 });
