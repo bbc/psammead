cd4ba6aca54d648bf340c5c485286cddaa6b425e
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-04-05 11:16:32 +0100

    Add language variants knob to Copyright

--- packages/components/psammead-copyright/src/index.stories.jsx
+++ packages/components/psammead-copyright/src/index.stories.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
 import notes from '../README.md';
 import Copyright from './index';
 
 storiesOf('Copyright', module)
-  .add('default', () => <Copyright>Getty Images</Copyright>, { notes })
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(['copyright'], copyright => (
+      <Copyright>{copyright}</Copyright>
+    )),
+    { notes, knobs: { escapeHTML: false } },
+  )
   .add(
     'with visually hidden text',
-    () => (
-      <Copyright>
-        <VisuallyHiddenText>Image source, </VisuallyHiddenText>
-        Getty Images
-      </Copyright>
+    inputProvider(
+      ['visually hidden text', 'copyright'],
+      (visuallyHiddenText, copyright) => (
+        <Copyright>
+          <VisuallyHiddenText>{visuallyHiddenText} </VisuallyHiddenText>
+          {copyright}
+        </Copyright>
+      ),
     ),
-    { notes },
+    { notes, knobs: { escapeHTML: false } },
   );
