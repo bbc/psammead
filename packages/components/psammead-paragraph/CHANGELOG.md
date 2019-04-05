d80d6104204d86488014171c9cd03896064da248
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-04-05 14:35:42 +0100

    Add language variants knob to Paragraph stories

--- packages/components/psammead-paragraph/CHANGELOG.md
+++ packages/components/psammead-paragraph/CHANGELOG.md
@@ -3,6 +3,7 @@
 <!-- prettier-ignore -->
 | Version | Description |
 |---------|-------------|
+| 0.3.5   | [PR#???](https://github.com/bbc/psammead/pull/???) Add language variants knob to Paragraph stories |
 | 0.3.4   | [PR#407](https://github.com/bbc/psammead/pull/407) Organise dependencies properly |
 | 0.3.3   | [PR#392](https://github.com/bbc/psammead/pull/392) Increase padding-bottom from 16px to 24px |
 | 0.3.2   | [PR#398](https://github.com/bbc/psammead/pull/398) Update dependencies. Using `rem` for media queries. |
--- packages/components/psammead-paragraph/src/index.stories.jsx
+++ packages/components/psammead-paragraph/src/index.stories.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
 import Paragraph from './index';
 
-storiesOf('Paragraph', module).add(
-  'default',
-  () => <Paragraph>This is text in a paragraph.</Paragraph>,
-  { notes },
-);
+storiesOf('Paragraph', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(['paragraph'], paragraph => (
+      <Paragraph>{paragraph}</Paragraph>
+    )),
+    { notes },
+  );
