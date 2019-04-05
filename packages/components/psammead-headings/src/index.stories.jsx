7f90f63e94cd57d6cbd6367d4b5824d6a46cbdba
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-04-05 11:49:45 +0100

    Add language variants knob to Headline and SubHeading

--- packages/components/psammead-headings/src/index.stories.jsx
+++ packages/components/psammead-headings/src/index.stories.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
 import { Headline, SubHeading } from './index';
 
-storiesOf('Headline', module).add(
-  'default',
-  () => <Headline>This is my headline.</Headline>,
-  { notes },
-);
+storiesOf('Headline', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(['headline'], headline => <Headline>{headline}</Headline>),
+    { notes, knobs: { escapeHTML: false } },
+  );
 
-storiesOf('SubHeading', module).add(
-  'default',
-  () => (
-    <SubHeading text="This is a SubHeading">This is a SubHeading</SubHeading>
-  ),
-  { notes },
-);
+storiesOf('SubHeading', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(['subheading'], subheader => (
+      <SubHeading text={subheader}>{subheader}</SubHeading>
+    )),
+    { notes, knobs: { escapeHTML: false } },
+  );
