169e1531383c6d0bb98e3e8051442f5803f77c22
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-04-08 08:58:32 +0100

    Add snapshot test for arabic script

--- packages/components/psammead-paragraph/src/index.test.jsx
+++ packages/components/psammead-paragraph/src/index.test.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { latin } from '@bbc/gel-foundations/scripts';
+import { latin, arabic } from '@bbc/gel-foundations/scripts';
 import Paragraph from './index';
 
 describe('Paragraph', () => {
   shouldMatchSnapshot(
     'should render a correctly',
     <Paragraph script={latin}>This is text in a paragraph.</Paragraph>,
+  );
+
+  shouldMatchSnapshot(
+    'should render correctly with arabic script typography values',
+    <Paragraph script={arabic}>بعض محتوى النص</Paragraph>,
   );
 });
