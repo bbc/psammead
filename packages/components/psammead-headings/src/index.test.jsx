1f9807ec773893527d4e1cc7c2349574d48e8e38
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-04-08 10:51:43 +0100

    Add snapshot tests for arabic script

--- packages/components/psammead-headings/src/index.test.jsx
+++ packages/components/psammead-headings/src/index.test.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { latin } from '@bbc/gel-foundations/scripts';
+import { latin, arabic } from '@bbc/gel-foundations/scripts';
 import { Headline, SubHeading } from './index';
 
 describe('Headline component', () => {
   shouldMatchSnapshot(
     'should render correctly',
     <Headline script={latin}>This is my headline.</Headline>,
+  );
+
+  shouldMatchSnapshot(
+    'should render correctly with arabic script typography values',
+    <Headline script={arabic}>This is my headline.</Headline>,
   );
 });
 
@@ -36,6 +41,13 @@ describe('SubHeading component', () => {
     'attribute id should render without exclamation marks',
     <SubHeading text="This is! a SubHeading!" script={latin}>
       This is a SubHeading
+    </SubHeading>,
+  );
+
+  shouldMatchSnapshot(
+    'should render correctly with arabic script typography values',
+    <SubHeading text="هذا عنوان فرعي" script={arabic}>
+      هذا عنوان فرعي
     </SubHeading>,
   );
 });
