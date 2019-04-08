791d85e57c74849978a449daf45ff8e23bdf4730
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-04-08 09:58:42 +0100

    Add snapshot test for arabic script

--- packages/components/psammead-caption/src/index.test.jsx
+++ packages/components/psammead-caption/src/index.test.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { latin } from '@bbc/gel-foundations/scripts';
+import { latin, arabic } from '@bbc/gel-foundations/scripts';
 import Caption from '.';
 
-shouldMatchSnapshot(
-  'should render Caption with some offscreen text',
-  <Caption script={latin}>
-    This is some Caption text
-    <VisuallyHiddenText>Some offscreen text</VisuallyHiddenText>
-  </Caption>,
-);
+describe('Caption', () => {
+  shouldMatchSnapshot(
+    'should render with some offscreen text',
+    <Caption script={latin}>
+      This is some Caption text
+      <VisuallyHiddenText>Some offscreen text</VisuallyHiddenText>
+    </Caption>,
+  );
+
+  shouldMatchSnapshot(
+    'should render with some offscreen text and arabic script typography values',
+    <Caption script={arabic}>
+      هذا هو بعض النص التسمية التوضيحي
+      <VisuallyHiddenText>Some offscreen text</VisuallyHiddenText>
+    </Caption>,
+  );
+});
