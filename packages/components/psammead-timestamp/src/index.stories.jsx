c111424e44cd92bdb502d121c7bc553a19ac3fb5
sadick <sadickjunior@gmail.com>
2019-04-11 22:28:47 +0300

    Add knob

--- packages/components/psammead-timestamp/src/index.stories.jsx
+++ packages/components/psammead-timestamp/src/index.stories.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { text, withKnobs } from '@storybook/addon-knobs';
 import notes from '../README.md';
 import Timestamp from '.';
 
 storiesOf('Timestamp', module)
+  .addDecorator(withKnobs)
   .add(
     'default',
-    () => <Timestamp datetime="1530947227000">7 July 2018</Timestamp>,
+    () => (
+      <Timestamp datetime="1530947227000">
+        {text('Timestamp Text', '7 July 2018')}
+      </Timestamp>
+    ),
     { notes },
   )
   .add(
     'with "updated" prefix',
-    () => <Timestamp datetime="1530947227000">Updated 7 July 2018</Timestamp>,
+    () => (
+      <Timestamp datetime="1530947227000">
+        {text('Timestamp Text', 'Updated 7 July 2018')}
+      </Timestamp>
+    ),
     { notes },
   );
