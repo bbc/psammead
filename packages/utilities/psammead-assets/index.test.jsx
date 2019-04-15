382207291b651c69207082ea950b454482419573
sadick <sadickjunior@gmail.com>
2019-04-15 16:13:51 +0300

    Add service SVG's

--- packages/utilities/psammead-assets/index.test.jsx
+++ packages/utilities/psammead-assets/index.test.jsx
@@ -12,6 +12,10 @@ const ampBoilerplateExpectedExports = {
 const svgsExpectedExports = {
   BBC_BLOCKS: 'string',
   news: 'object',
+  yoruba: 'object',
+  igbo: 'object',
+  thai: 'object',
+  pidgin: 'object',
 };
 
 const expectedExports = {
