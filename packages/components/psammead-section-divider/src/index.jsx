c2e5b1747939ac466fb6298b31c49abbf75455c0
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-04-16 12:37:51 +0100

    Try a non-flexbox approach

--- packages/components/psammead-section-divider/src/index.jsx
+++ packages/components/psammead-section-divider/src/index.jsx
@@ -1,55 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
 import { boolean, shape, string } from 'prop-types';
-import { scriptPropTypes } from '@bbc/gel-foundations/prop-types';
+import { scriptPropType } from '@bbc/gel-foundations/prop-types';
+import { MEDIA_QUERY_TYPOGRAPHY } from '@bbc/gel-foundations/breakpoints';
 import {
   getDoublePica,
   GEL_FF_REITH_SANS,
 } from '@bbc/gel-foundations/typography';
 import { C_CLOUD_LIGHT, C_EBON } from '@bbc/psammead-styles/colours';
 
+const calcTop = ({ inline, script }) =>
+  inline
+    ? `
+      top: ${script.doublePica.groupA.lineHeight / 2}px;
+
+      ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_ONLY} {
+        top: ${script.doublePica.groupB.lineHeight / 2}px;
+      }
+
+      ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
+        top: ${script.doublePica.groupD.lineHeight / 2}px;
+      }`
+    : 'top: 0px;';
+
+const SectionDividerOuterWrapper = styled.div`
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    height: 1px;
+    border: none;
+    background: ${C_CLOUD_LIGHT};
+    left: 0px;
+    right: 0px;
+    ${calcTop};
+  }
+`;
+
+const SectionDividerInnerWrapper = styled.div`
+  position: relative;
+  display: inline;
+  background-color: #fff;
+  html:not([dir='rtl']) & {
+    padding-right: 8px;
+  }
+  html[dir='rtl'] & {
+    padding-left: 8px;
+  }
+`;
+
 const SectionTitle = styled.h2`
   ${props => getDoublePica(props.script)};
   color: ${C_EBON};
   font-family: ${GEL_FF_REITH_SANS};
-
-  html:not([dir='rtl']) & {
-    margin-left: 8px;
-  }
-
-  html[dir='rtl'] & {
-    margin-right: 8px;
-  }
+  display: inline-block;
+  margin-top: 0px;
 `;
 
 SectionTitle.propTypes = {
-  script: shape(scriptPropTypes).isRequired,
+  script: shape(scriptPropType).isRequired,
 };
 
-const SectionRule = styled.div`
-  background: ${C_CLOUD_LIGHT};
-  height: 1px;
-  border: none;
-  flex-grow: 100;
-  margin: ${props => (props.inline ? 'auto 8px' : '8px')};
-`;
-
-const SectionDividerWrapper = styled.div`
-  ${props =>
-    props.inline
-      ? `
-        display: flex;
-        flex-direction: row-reverse;
-      `
-      : ''}
-`;
-
 const SectionDivider = ({ children, inline, script }) => (
-  // Only modify the Rule to account for an inline title if there is a title to render.
-  <SectionDividerWrapper inline={inline && children}>
-    <SectionRule inline={inline && children} />
-    {children && <SectionTitle script={script}>{children}</SectionTitle>}
-  </SectionDividerWrapper>
+  // Only modify the Divider to account for an inline title if there is a title to render.
+  <SectionDividerOuterWrapper inline={inline && children} script={script}>
+    <SectionDividerInnerWrapper inline={inline && children}>
+      {children && <SectionTitle script={script}>{children}</SectionTitle>}
+    </SectionDividerInnerWrapper>
+  </SectionDividerOuterWrapper>
 );
 
 SectionDivider.defaultProps = {
@@ -60,7 +78,7 @@ SectionDivider.defaultProps = {
 SectionDivider.propTypes = {
   children: string,
   inline: boolean,
-  script: shape(scriptPropTypes).isRequired,
+  script: shape(scriptPropType).isRequired,
 };
 
 export default SectionDivider;
--- packages/components/psammead-section-divider/src/index.stories.jsx
+++ packages/components/psammead-section-divider/src/index.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { latin } from '@bbc/gel-foundations/scripts';
+import { latin, thai } from '@bbc/gel-foundations/scripts';
 import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import notes from '../README.md';
 import SectionDivider from './index';
@@ -14,5 +14,23 @@ storiesOf('SectionDivider', module)
         {text('Title', 'Most Read')}
       </SectionDivider>
     ),
-    { notes },
+    { notes, knobs: { escapeHTML: false } },
+  )
+  .add(
+    'igbo',
+    () => (
+      <SectionDivider script={latin} inline={boolean('inline', true)}>
+        {text('Title', 'Ọ bụru')}
+      </SectionDivider>
+    ),
+    { notes, knobs: { escapeHTML: false } },
+  )
+  .add(
+    'thai',
+    () => (
+      <SectionDivider script={thai} inline={boolean('inline', true)}>
+        {text('Title', 'ภาพวาดขอ')}
+      </SectionDivider>
+    ),
+    { notes, knobs: { escapeHTML: false } },
   );
