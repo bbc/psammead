27c0888d32b211ffda8a10766b3c073a4aa7bdd2
sadick <sadickjunior@gmail.com>
2019-04-16 17:23:38 +0300

    Refactor tests

--- packages/utilities/psammead-assets/index.test.jsx
+++ packages/utilities/psammead-assets/index.test.jsx
@@ -1,9 +1,4 @@
-import React from 'react';
-
-import {
-  shouldMatchSnapshot,
-  testUtilityPackages,
-} from '@bbc/psammead-test-helpers';
+import { testUtilityPackages } from '@bbc/psammead-test-helpers';
 import * as svgs from './svgs';
 import * as svgsFromSrc from './src/svgs';
 import * as ampBoilerplate from './amp-boilerplate';
@@ -50,51 +45,4 @@ describe('Psammead assets', () => {
       'psammead-assets',
     );
   });
-});
-
-const { news, yoruba, igbo, pidgin, thai } = svgs;
-
-describe('News Svg', () => {
-  shouldMatchSnapshot(
-    'should render news svg correctly',
-    <svg viewBox={`${svgs.news.viewbox.width} ${svgs.news.viewbox.height}`}>
-      {news.group}
-    </svg>,
-  );
-});
-
-describe('Yoruba Svg', () => {
-  shouldMatchSnapshot(
-    'should render yoruba service svg correctly',
-    <svg viewBox={`${yoruba.viewbox.width} ${yoruba.viewbox.height}`}>
-      {yoruba.group}
-    </svg>,
-  );
-});
-
-describe('Igbo Svg', () => {
-  shouldMatchSnapshot(
-    'should render yoruba service svg correctly',
-    <svg viewBox={`${igbo.viewbox.width} ${igbo.viewbox.height}`}>
-      {igbo.group}
-    </svg>,
-  );
-});
-
-describe('Pidgin Svg', () => {
-  shouldMatchSnapshot(
-    'should render pidgin service svg correctly',
-    <svg viewBox={`${pidgin.viewbox.width} ${pidgin.viewbox.height}`}>
-      {pidgin.group}
-    </svg>,
-  );
-});
-
-describe('Thai Svg', () => {
-  shouldMatchSnapshot(
-    'should render thai service svg correctly',
-    <svg viewBox={`${thai.viewbox.width} ${thai.viewbox.height}`}>
-      {thai.group}
-    </svg>,
-  );
 });
