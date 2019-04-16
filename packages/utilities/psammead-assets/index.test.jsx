17e8d63bef6006fe0654731b219e681b70beadaa
sadick <sadickjunior@gmail.com>
2019-04-16 13:01:44 +0300

    Add tests

--- packages/utilities/psammead-assets/index.test.jsx
+++ packages/utilities/psammead-assets/index.test.jsx
@@ -1,4 +1,9 @@
-import { testUtilityPackages } from '@bbc/psammead-test-helpers';
+import React from 'react';
+
+import {
+  shouldMatchSnapshot,
+  testUtilityPackages,
+} from '@bbc/psammead-test-helpers';
 import * as svgs from './svgs';
 import * as svgsFromSrc from './src/svgs';
 import * as ampBoilerplate from './amp-boilerplate';
@@ -16,7 +21,6 @@ const svgsExpectedExports = {
   igbo: 'object',
   thai: 'object',
   pidgin: 'object',
-  getSVG: 'function',
 };
 
 const expectedExports = {
@@ -46,4 +50,51 @@ describe('Psammead assets', () => {
       'psammead-assets',
     );
   });
+});
+
+const { news, yoruba, igbo, pidgin, thai } = svgs;
+
+describe('News Svg', () => {
+  shouldMatchSnapshot(
+    'should render news svg correctly',
+    <svg viewBox={`${svgs.news.viewbox.width} ${svgs.news.viewbox.height}`}>
+      {news.group}
+    </svg>,
+  );
+});
+
+describe('Yoruba Svg', () => {
+  shouldMatchSnapshot(
+    'should render yoruba service svg correctly',
+    <svg viewBox={`${yoruba.viewbox.width} ${yoruba.viewbox.height}`}>
+      {yoruba.group}
+    </svg>,
+  );
+});
+
+describe('Igbo Svg', () => {
+  shouldMatchSnapshot(
+    'should render yoruba service svg correctly',
+    <svg viewBox={`${igbo.viewbox.width} ${igbo.viewbox.height}`}>
+      {igbo.group}
+    </svg>,
+  );
+});
+
+describe('Pidgin Svg', () => {
+  shouldMatchSnapshot(
+    'should render pidgin service svg correctly',
+    <svg viewBox={`${pidgin.viewbox.width} ${pidgin.viewbox.height}`}>
+      {pidgin.group}
+    </svg>,
+  );
+});
+
+describe('Thai Svg', () => {
+  shouldMatchSnapshot(
+    'should render thai service svg correctly',
+    <svg viewBox={`${thai.viewbox.width} ${thai.viewbox.height}`}>
+      {thai.group}
+    </svg>,
+  );
 });
