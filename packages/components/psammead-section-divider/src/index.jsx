e241e410e15162f733364d489910c57537c7b401
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-04-16 14:39:01 +0100

    continue non-flexbox approach

--- packages/components/psammead-section-divider/src/index.jsx
+++ packages/components/psammead-section-divider/src/index.jsx
@@ -3,27 +3,34 @@ import styled from 'styled-components';
 import { boolean, shape, string } from 'prop-types';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
 import { MEDIA_QUERY_TYPOGRAPHY } from '@bbc/gel-foundations/breakpoints';
+import { GEL_SPACING } from '@bbc/gel-foundations/spacings';
 import {
   getDoublePica,
   GEL_FF_REITH_SANS,
 } from '@bbc/gel-foundations/typography';
-import { C_CLOUD_LIGHT, C_EBON } from '@bbc/psammead-styles/colours';
+import { C_CLOUD_LIGHT, C_EBON, C_WHITE } from '@bbc/psammead-styles/colours';
+
+const TOP_MARGIN_PX = 1;
 
 const calcTop = ({ inline, script }) =>
   inline
     ? `
-      top: ${script.doublePica.groupA.lineHeight / 2}px;
+      // place at middle of text line height
+      top: ${(script.doublePica.groupA.lineHeight / 2 + TOP_MARGIN_PX) / 16}rem;
 
       ${MEDIA_QUERY_TYPOGRAPHY.SMART_PHONE_ONLY} {
-        top: ${script.doublePica.groupB.lineHeight / 2}px;
+        top: ${(script.doublePica.groupB.lineHeight / 2 + TOP_MARGIN_PX) /
+          16}rem;
       }
 
       ${MEDIA_QUERY_TYPOGRAPHY.LAPTOP_AND_LARGER} {
-        top: ${script.doublePica.groupD.lineHeight / 2}px;
-      }`
-    : 'top: 0px;';
+        top: ${(script.doublePica.groupD.lineHeight / 2 + TOP_MARGIN_PX) /
+          16}rem;
+      }
+    `
+    : 'top: 0;';
 
-const SectionDividerOuterWrapper = styled.div`
+const SectionDividerWrapper = styled.div`
   position: relative;
   &::before {
     content: '';
@@ -31,43 +38,39 @@ const SectionDividerOuterWrapper = styled.div`
     height: 1px;
     border: none;
     background: ${C_CLOUD_LIGHT};
-    left: 0px;
-    right: 0px;
+    left: 0;
+    right: 0;
     ${calcTop};
-  }
-`;
-
-const SectionDividerInnerWrapper = styled.div`
-  position: relative;
-  display: inline;
-  background-color: #fff;
-  html:not([dir='rtl']) & {
-    padding-right: 8px;
-  }
-  html[dir='rtl'] & {
-    padding-left: 8px;
   }
 `;
 
 const SectionTitle = styled.h2`
   ${props => getDoublePica(props.script)};
   color: ${C_EBON};
+  background-color: ${C_WHITE};
   font-family: ${GEL_FF_REITH_SANS};
   display: inline-block;
-  margin-top: 0px;
+  position: relative;
+  margin: ${TOP_MARGIN_PX}px 0 0 0;
+
+  html:not([dir='rtl']) & {
+    padding-right: ${GEL_SPACING};
+  }
+
+  html[dir='rtl'] & {
+    padding-left: ${GEL_SPACING};
+  }
 `;
 
 SectionTitle.propTypes = {
   script: shape(scriptPropType).isRequired,
 };
 
-const SectionDivider = ({ children, inline, script }) => (
+const SectionDivider = ({ children: text, inline, script }) => (
   // Only modify the Divider to account for an inline title if there is a title to render.
-  <SectionDividerOuterWrapper inline={inline && children} script={script}>
-    <SectionDividerInnerWrapper inline={inline && children}>
-      {children && <SectionTitle script={script}>{children}</SectionTitle>}
-    </SectionDividerInnerWrapper>
-  </SectionDividerOuterWrapper>
+  <SectionDividerWrapper inline={inline && text} script={script}>
+    {text && <SectionTitle script={script}>{text}</SectionTitle>}
+  </SectionDividerWrapper>
 );
 
 SectionDivider.defaultProps = {
