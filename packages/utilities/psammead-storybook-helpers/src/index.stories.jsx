87b8ef441faa5b477a7a64251b146dd68144aa19
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-04-25 12:00:29 +0100

    Add a story displaying the inputProviders capabilities

---
+++ packages/utilities/psammead-storybook-helpers/src/index.stories.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { inputProvider } from '.';
+
+storiesOf('Input Provider', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(
+      [
+        {
+          name: 'first slot',
+          defaultText:
+            "this slot overwrites the English default; the next one doesn't",
+        },
+        { name: 'second slot' },
+      ],
+      ([first, second], script, dir) => (
+        <ul dir={dir}>
+          <li>{first}</li>
+          <li>{second}</li>
+          <li>Selected direction: {dir}</li>
+          <li>
+            Content of selected script:
+            <pre>{JSON.stringify(script, null, ' ')}</pre>
+          </li>
+        </ul>
+      ),
+    ),
+    { knobs: { escapeHTML: false } },
+  );
