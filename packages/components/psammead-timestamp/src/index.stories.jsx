72e20b9675873baeeb5f31b511ea9ee355b8886c
sadick <sadickjunior@gmail.com>
2019-04-25 16:02:51 +0300

    Update storybook

--- packages/components/psammead-timestamp/src/index.stories.jsx
+++ packages/components/psammead-timestamp/src/index.stories.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
-import { storiesOf, forceReRender } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import { text, select, withKnobs } from '@storybook/addon-knobs';
-import scripts from '@bbc/gel-foundations/scripts';
 import * as typography from '@bbc/gel-foundations/typography';
+import { latin } from '@bbc/gel-foundations/scripts';
 import notes from '../README.md';
 import Timestamp from '.';
+
+const styles = Object.keys(typography)
+  .map(key => {
+    if (
+      typeof typography[key] === 'function' &&
+      key.substring(0, 3) === 'get'
+    ) {
+      return key.substring(3);
+    }
+    return null;
+  })
+  .filter(style => style);
 
 storiesOf('Timestamp', module)
   .addDecorator(withKnobs)
   .add(
     'default',
     () => {
-      const selectedOption = select('Script', Object.keys(scripts), 'latin');
-      const selectedScript = scripts[selectedOption];
-
-      const style = select('Typography', Object.keys(selectedScript), 'canon');
-      const functionName = style.charAt(0).toUpperCase() + style.slice(1);
-      const typographyFunc = typography[`get${functionName}`];
+      const style = select('Typography', styles, 'Brevier');
+      const typographyFunc = typography[`get${style}`];
 
       return (
         <Timestamp
           datetime="1530947227000"
           typographyFunc={typographyFunc}
-          script={selectedScript}
+          script={latin}
         >
           {text('Timestamp Text', '7 July 2018')}
         </Timestamp>
@@ -33,17 +41,14 @@ storiesOf('Timestamp', module)
   .add(
     'with "updated" prefix',
     () => {
-      const selectedOption = select('Script', Object.keys(scripts), 'latin');
-      const selectedScript = scripts[selectedOption];
-
-      const style = select('Typography', Object.keys(selectedScript), 'canon');
+      const style = select('Typography', styles, 'Brevier');
       const typographyFunc = typography[`get${style}`];
 
       return (
         <Timestamp
           datetime="1530947227000"
           typographyFunc={typographyFunc}
-          script={selectedScript}
+          script={latin}
         >
           {text('Timestamp Text', 'Updated 7 July 2018')}
         </Timestamp>
