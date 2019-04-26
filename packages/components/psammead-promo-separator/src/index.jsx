3498c5c711a7df83302fd040f04ab65f4aaf14fb
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-04-26 14:49:38 +0100

    Update space above and below for different breakpoints

--- packages/components/psammead-promo-separator/src/index.jsx
+++ packages/components/psammead-promo-separator/src/index.jsx
@@ -1,11 +1,27 @@
 import styled from 'styled-components';
 import { C_LUNAR } from '@bbc/psammead-styles/colours';
-import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import {
+  GEL_SPACING,
+  GEL_SPACING_DBL,
+  GEL_SPACING_TRPL,
+} from '@bbc/gel-foundations/spacings';
+import {
+  GEL_GROUP_3_SCREEN_WIDTH_MIN,
+  GEL_GROUP_4_SCREEN_WIDTH_MIN,
+} from '@bbc/gel-foundations/breakpoints';
 
 const PromoSeparator = styled.div`
   background-color: ${C_LUNAR};
-  margin: ${GEL_SPACING} 0 ${GEL_SPACING_DBL};
+  margin: ${GEL_SPACING_DBL} 0 ${GEL_SPACING};
   height: 1px;
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    margin: ${GEL_SPACING_DBL} 0 ${GEL_SPACING_DBL};
+  }
+
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    margin: ${GEL_SPACING_TRPL} 0 ${GEL_SPACING_TRPL};
+  }
 `;
 
 export default PromoSeparator;
