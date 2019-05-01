51ba4f0aa85f1365ba9caf3a61e32846f49b1dc4
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-05-01 17:37:02 +0100

    :rocket: Bump and package stuff

--- packages/utilities/psammead-assets/src/svgs.test.jsx
+++ packages/utilities/psammead-assets/src/svgs.test.jsx
@@ -1,57 +1,16 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { news, yoruba, igbo, pidgin, thai, persian } from './svgs';
+import * as svgs from './svgs';
 
-describe('News Svg', () => {
-  shouldMatchSnapshot(
-    'should render news svg correctly',
-    <svg viewBox={`${news.viewbox.width} ${news.viewbox.height}`}>
-      {news.group}
-    </svg>,
-  );
-});
-
-describe('Persian Svg', () => {
-  shouldMatchSnapshot(
-    'should render persian svg correctly',
-    <svg viewBox={`${persian.viewbox.width} ${persian.viewbox.height}`}>
-      {persian.group}
-    </svg>,
-  );
-});
-
-describe('Yoruba Svg', () => {
-  shouldMatchSnapshot(
-    'should render yoruba service svg correctly',
-    <svg viewBox={`${yoruba.viewbox.width} ${yoruba.viewbox.height}`}>
-      {yoruba.group}
-    </svg>,
-  );
-});
-
-describe('Igbo Svg', () => {
-  shouldMatchSnapshot(
-    'should render yoruba service svg correctly',
-    <svg viewBox={`${igbo.viewbox.width} ${igbo.viewbox.height}`}>
-      {igbo.group}
-    </svg>,
-  );
-});
-
-describe('Pidgin Svg', () => {
-  shouldMatchSnapshot(
-    'should render pidgin service svg correctly',
-    <svg viewBox={`${pidgin.viewbox.width} ${pidgin.viewbox.height}`}>
-      {pidgin.group}
-    </svg>,
-  );
-});
-
-describe('Thai Svg', () => {
-  shouldMatchSnapshot(
-    'should render thai service svg correctly',
-    <svg viewBox={`${thai.viewbox.width} ${thai.viewbox.height}`}>
-      {thai.group}
-    </svg>,
-  );
-});
+Object.keys(svgs)
+  .filter(key => key !== 'BBC_BLOCKS')
+  .forEach(key => {
+    describe(`${key} SVG`, () => {
+      shouldMatchSnapshot(
+        'should render correctly',
+        <svg viewBox={`${svgs[key].viewbox.width} ${svgs[key].viewbox.height}`}>
+          {svgs[key].group}
+        </svg>,
+      );
+    });
+  });
