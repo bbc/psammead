3b4a4dd3b8340c6be51c48b9dc1abbc9f4afed6a
sadick <isaac.odhiambo@andela.com>
2019-05-07 16:18:22 +0300

    Revert testing

--- packages/components/psammead-image/src/index.test.jsx
+++ packages/components/psammead-image/src/index.test.jsx
@@ -1,5 +1,9 @@
-import Image from '.';
+import Image, { Img } from '.';
 import snapshotTests from './testHelpers/snapshotTests';
+
+describe("Image - imported as default 'Image'", () => {
+  snapshotTests(Img);
+});
 
 describe("Image - Img'", () => {
   snapshotTests(Image);
