12b27c2e3e1f1a42e2997bc653646184bade61a9
Chris Hinds <chris.hinds@bbc.co.uk>
2019-05-15 11:42:17 +0100

    Update getVersionScript to return array of published package versions

--- scripts/publish/src/getRegistryVersion.js
+++ scripts/publish/src/getRegistryVersion.js
@@ -2,6 +2,15 @@ const { exec } = require('shelljs');
 
 // Get version of package in NPM regsitry. Returns 0.0.0 if doesn't exist.
 module.exports = name => {
-  const npmVersion = exec(`npm show ${name} version`, { silent: true }).stdout;
-  return npmVersion ? npmVersion.split('\n')[0] : '0.0.0';
+  let packageVersions = [];
+  const npmVersion = exec(`npm view ${name} versions -json`, { silent: true })
+    .stdout;
+
+  if (typeof npmVersion === 'string' && JSON.parse(npmVersion)) {
+    const jsonResponse = JSON.parse(npmVersion);
+    packageVersions = Array.isArray(jsonResponse) ? jsonResponse : [];
+  }
+
+  const latestPublishedVersion = packageVersions.pop(); // removes the last element from the array and returns it i.e. the latest published version
+  return latestPublishedVersion || '0.0.0';
 };
