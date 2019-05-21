32fe6621680f90d091b0104dfbc9f9216816845a
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-05-21 15:46:31 +0100

    Use GEL Sizes and Reith font

--- packages/utilities/psammead-styles/src/index.stories.jsx
+++ packages/utilities/psammead-styles/src/index.stories.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
+import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import {
+  getBodyCopy,
+  GEL_FF_REITH_SANS,
+} from '@bbc/gel-foundations/typography';
+import { latin } from '@bbc/gel-foundations/scripts';
 import notes from '../README.md';
 import * as colours from './colours';
 
 const ColourContainer = styled.div`
-  padding: 10px;
-  font-size: 14px;
-  font-family: 'Nunito Sans', -apple-system, Helvetica, Arial, sans-serif;
+  padding: ${GEL_SPACING_DBL};
+  font-family: ${GEL_FF_REITH_SANS};
 `;
 
 const ColourRow = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 10px;
+  margin-bottom: ${GEL_SPACING_DBL};
 `;
 
 const ColourBox = styled.div`
   background: ${props => props.colour};
   color: #000;
-  padding: 10px 5px;
-  border-radius: 5px;
+  padding: ${GEL_SPACING_DBL} ${GEL_SPACING};
+  border-radius: 0.3125rem;
   display: inline-block;
+  ${getBodyCopy(latin)};
 `;
 
 const ColourValue = styled.div`
-  padding-left: 10px;
+  padding-left: ${GEL_SPACING};
   display: inline-block;
+  ${getBodyCopy(latin)};
 `;
 
 storiesOf('Psammead Styles', module).add(
