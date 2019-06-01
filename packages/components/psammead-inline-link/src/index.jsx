ca3cd4dabf9278fda647878e30219750eb737ab7
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-06-01 17:11:55 +0200

    testing things

--- packages/components/psammead-inline-link/src/index.jsx
+++ packages/components/psammead-inline-link/src/index.jsx
@@ -3,12 +3,10 @@ import { C_POSTBOX, C_CLOUD_DARK, C_WHITE } from '@bbc/psammead-styles/colours';
 
 const InlineLink = styled.a`
   color: ${C_POSTBOX};
-  // border-bottom: 1px solid ${C_POSTBOX};
-  // padding-bottom: 1px;
+  border-bottom: 1px solid ${C_POSTBOX};
   white-space: pre-wrap; // needed so words on the end of sentences, especially italics, don't cut off background-color
-  padding: 0 2px 2px;
-  margin: 0 -2px; // offset the padding so there is no additional spacing
-  box-shadow: inset 0 -1px 0 ${C_POSTBOX};
+  padding: 0 2px 1px; // increases highlight start and end of the link plus increases click error
+  margin: 0 -2px -1px; // offset the padding so there is no additional spacing between words
   text-decoration: none;
 
   &:visited {
@@ -20,6 +18,10 @@ const InlineLink = styled.a`
   &:hover {
     color: ${C_WHITE};
     background-color: ${C_POSTBOX};
+    border-bottom: 0; // could remove this and the visual experience would feel less jumpy, currently the eye notices a bounce IMO
+    @supports (transition) {
+      transition: all 1s ease; // speak with Natalie as the direct swap in colour and background is gross. maybe change designs
+    }
   }
 `;
 
