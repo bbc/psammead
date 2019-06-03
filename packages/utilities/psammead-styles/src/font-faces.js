f12958abb29395e69578bdceed62e61af80e250f
sadick <isaac.odhiambo@andela.com>
2019-06-03 18:08:43 +0300

    Update news base url and font families

--- packages/utilities/psammead-styles/src/font-faces.js
+++ packages/utilities/psammead-styles/src/font-faces.js
@@ -1,15 +1,16 @@
 const baseFontUrl = 'https://ws-downloads.files.bbci.co.uk/fonts/';
+const gelBaseFontUrl = 'https://gel.files.bbci.co.uk/r2.511/';
 
 export const persian = {
   sansBold: `@font-face {
-    font-family: NassimPersian, Arial, Verdana,	Geneva,	Helvetica, sans serif;
+    font-family: NassimPersian;
     font-weight: 700;
     font-style: normal;
-    src: url('${baseFontUrl}NassimPersian/latest/bold.woff') format('woff'), url('${baseFontUrl}NassimPersian/latest/bold.eot') format('eot'), url('${baseFontUrl}NassimPersian/latest/bold.ttf') format('ttf');
+    src: url('${baseFontUrl}NassimPersian/v1.051/bold.woff') format('woff'), url('${baseFontUrl}NassimPersian/v1.051/bold.eot') format('eot'), url('${baseFontUrl}NassimPersian/v1.051/bold.ttf') format('ttf');
     font-display: optional;
   }`,
   sansRegular: `@font-face {
-    font-family: NassimPersian, Arial, Verdana,	Geneva,	Helvetica, sans serif;
+    font-family: NassimPersian;
     font-weight: 400;
     font-style: normal;
     src: url('${baseFontUrl}NassimPersian/latest/normal.woff') format('woff'), url('${baseFontUrl}NassimPersian/latest/normal.eot') format('eot'), url('${baseFontUrl}NassimPersian/latest/normal.ttf') format('ttf');
@@ -19,17 +20,17 @@ export const persian = {
 
 export const arabic = {
   sansBold: `@font-face {
-    font-family: NassimArabic, Arial, Verdana,	Geneva,	Helvetica, sans serif;
+    font-family: NassimArabic;
     font-weight: 700;
     font-style: normal;
-    src: url('${baseFontUrl}NassimArabic/latest/bold.woff') format('woff'), url('${baseFontUrl}NassimArabic/latest/bold.eot') format('eot'), url('${baseFontUrl}NassimArabic/latest/bold.ttf') format('ttf');
+    src: url('${baseFontUrl}NassimArabic/v1.044/bold.woff') format('woff'), url('${baseFontUrl}NassimArabic/v1.044/bold.eot') format('eot'), url('${baseFontUrl}NassimArabic/v1.044/bold.ttf') format('ttf');
     font-display: optional;
   }`,
   sansRegular: `@font-face {
-    font-family: NassimArabic, Arial, Verdana,	Geneva,	Helvetica, sans serif;
+    font-family: NassimArabic;
     font-weight: 400;
     font-style: normal;
-    src: url('${baseFontUrl}NassimArabic/latest/normal.woff') format('woff'), url('${baseFontUrl}NassimArabic/latest/normal.eot') format('eot'), url('${baseFontUrl}NassimArabic/latest/normal.ttf') format('ttf');
+    src: url('${baseFontUrl}NassimArabic/v1.044/normal.woff') format('woff'), url('${baseFontUrl}NassimArabic/v1.044/normal.eot') format('eot'), url('${baseFontUrl}NassimArabic/v1.044/normal.ttf') format('ttf');
     font-display: optional;
   }`,
 };
@@ -39,56 +40,56 @@ export const news = {
     font-family: "ReithSans";
     font-weight: 700;
     font-style: normal;
-    src: url('${baseFontUrl}ReithSans/latest/bold.woff2') format('woff2'), url('${baseFontUrl}ReithSans/latest/bold.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSans_W_Bd.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSans_W_Bd.woff") format("woff");
     font-display: optional;
   }`,
   sansBoldItalic: `@font-face {
     font-family: "ReithSans";
     font-weight: 700;
     font-style: italic;
-    src: url('${baseFontUrl}ReithSans/latest/bold-italic.woff2') format('woff2'), url('${baseFontUrl}ReithSans/latest/bold-italic.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSans_W_BdIt.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSans_W_BdIt.woff") format("woff");
     font-display: optional;
   }`,
   sansRegular: `@font-face {
     font-family: "ReithSans";
     font-weight: 400;
     font-style: normal;
-    src: url('${baseFontUrl}ReithSans/latest/normal.woff2') format('woff2'), url('${baseFontUrl}ReithSans/latest/normal.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSans_W_Rg.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSans_W_Rg.woff") format("woff");
     font-display: optional;
   }`,
   sansItalic: `@font-face {
     font-family: "ReithSans";
     font-weight: 400;
     font-style: italic;
-    src: url('${baseFontUrl}ReithSans/latest/normal-italic.woff2') format('woff2'), url('${baseFontUrl}ReithSans/latest/normal-italic.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSans_W_It.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSans_W_It.woff") format("woff");
     font-display: optional;
   }`,
   serifMedium: `@font-face {
     font-family: "ReithSerif";
     font-weight: 500;
     font-style: normal;
-    src: url('${baseFontUrl}ReithSerif/latest/medium.woff2') format('woff2'), url('${baseFontUrl}ReithSerif/latest/medium.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSerif_W_Md.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSerif_W_Md.woff") format("woff");
     font-display: optional;
   }`,
   serifMediumItalic: `@font-face {
     font-family: "ReithSerif";
     font-weight: 500;
     font-style: italic;
-    src: url('${baseFontUrl}ReithSerif/latest/medium-italic.woff2') format('woff2'), url('${baseFontUrl}ReithSerif/latest/medium-italic.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSerif_W_MdIt.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSerif_W_MdIt.woff") format("woff");
     font-display: optional;
   }`,
   serifBold: `@font-face {
     font-family: "ReithSerif";
     font-weight: 700;
     font-style: normal;
-    src: url('${baseFontUrl}ReithSerif/latest/bold.woff2') format('woff2'), url('${baseFontUrl}ReithSerif/latest/bold.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSerif_W_Bd.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSerif_W_Bd.woff") format("woff");
     font-display: optional;
   }`,
   serifBoldItalic: `@font-face {
     font-family: "ReithSerif";
     font-weight: 700;
     font-style: italic;
-    src: url('${baseFontUrl}ReithSerif/latest/bold-italic.woff2') format('woff2'), url('${baseFontUrl}ReithSerif/latest/bold-italic.woff') format('woff');
+    src: url("${gelBaseFontUrl}BBCReithSerif_W_BdIt.woff2") format("woff2"), url("${gelBaseFontUrl}BBCReithSerif_W_BdIt.woff") format("woff");
     font-display: optional;
   }`,
 };
