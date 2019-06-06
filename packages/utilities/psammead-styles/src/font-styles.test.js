c33262cd688a02a740a90e3f5672c7c92c1ea33d
sadick <isaac.odhiambo@andela.com>
2019-06-06 19:11:40 +0300

    Remove redundant tests

--- packages/utilities/psammead-styles/src/font-styles.test.js
+++ packages/utilities/psammead-styles/src/font-styles.test.js
@@ -2,9 +2,10 @@ import * as fontStyles from './font-styles';
 import * as fontFamilies from './font-families';
 
 const services = Object.keys(fontFamilies);
+const fontStyleKeys = Object.keys(fontStyles);
 
 services.forEach(service => {
-  Object.keys(fontStyles).forEach(fontStyle => {
+  fontStyleKeys.forEach(fontStyle => {
     const style = fontStyle.replace('get', '');
     it(`should render ${style} correctly for ${service}`, () => {
       expect(fontStyles[fontStyle](service)).toMatchSnapshot();
@@ -12,50 +13,10 @@ services.forEach(service => {
   });
 });
 
-describe('fonts', () => {
-  describe('getSansRegular', () => {
-    it('should return regular sans font css', () => {
-      expect(fontStyles.getSansRegular('igbo')).toMatchSnapshot();
-    });
-
-    it('should fail gracefully given an invalid service name', () => {
-      expect(fontStyles.getSansRegular('default')).toBeUndefined();
-    });
-  });
-
-  describe('getSansItalic', () => {
-    it('should return regular sans italic font css', () => {
-      expect(fontStyles.getSansItalic('yoruba')).toMatchSnapshot();
-    });
-  });
-
-  describe('getSansBold', () => {
-    it('should return bold sans font css', () => {
-      expect(fontStyles.getSansBold('pidgin')).toMatchSnapshot();
-    });
-  });
-
-  describe('getSansBoldItalic', () => {
-    it('should return bold italic font css', () => {
-      expect(fontStyles.getSansBoldItalic('igbo')).toMatchSnapshot();
-    });
-  });
-
-  describe('getSerifMedium', () => {
-    it('should return medium serif font css', () => {
-      expect(fontStyles.getSerifMedium('news')).toMatchSnapshot();
-    });
-    it('should return regular sans bold font css for sevices without serif medium', () => {
-      expect(fontStyles.getSerifMedium('igbo')).toMatchSnapshot();
-    });
-  });
-
-  describe('getSerifMediumItalic', () => {
-    it('should return serif medium italic font css', () => {
-      expect(fontStyles.getSerifMedium('news')).toMatchSnapshot();
-    });
-    it('should return sans bold italic font css for sevice without serif medium italic', () => {
-      expect(fontStyles.getSerifMediumItalic('yoruba')).toMatchSnapshot();
+fontStyleKeys.forEach(fontStyle => {
+  describe(fontStyle, () => {
+    it('should fail gracefully and return undefined given an invalid service name', () => {
+      expect(fontStyles[fontStyle]('default')).toBeUndefined();
     });
   });
 });
