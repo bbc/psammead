20a8e68fdb0af29bab69af9029745120b61f0cad
sadick <isaac.odhiambo@andela.com>
2019-06-06 18:51:32 +0300

    Add tests back with snaps

--- packages/utilities/psammead-styles/src/font-styles.test.js
+++ packages/utilities/psammead-styles/src/font-styles.test.js
@@ -11,3 +11,51 @@ services.forEach(service => {
     });
   });
 });
+
+describe('fonts', () => {
+  describe('getSansRegular', () => {
+    it('should return regular sans font css', () => {
+      expect(fontStyles.getSansRegular('igbo')).toMatchSnapshot();
+    });
+
+    it('should fail gracefully given an invalid service name', () => {
+      expect(fontStyles.getSansRegular('default')).toBeUndefined();
+    });
+  });
+
+  describe('getSansItalic', () => {
+    it('should return regular sans italic font css', () => {
+      expect(fontStyles.getSansItalic('yoruba')).toMatchSnapshot();
+    });
+  });
+
+  describe('getSansBold', () => {
+    it('should return bold sans font css', () => {
+      expect(fontStyles.getSansBold('pidgin')).toMatchSnapshot();
+    });
+  });
+
+  describe('getSansBoldItalic', () => {
+    it('should return bold italic font css', () => {
+      expect(fontStyles.getSansBoldItalic('igbo')).toMatchSnapshot();
+    });
+  });
+
+  describe('getSerifMedium', () => {
+    it('should return medium serif font css', () => {
+      expect(fontStyles.getSerifMedium('news')).toMatchSnapshot();
+    });
+    it('should return regular sans bold font css for sevices without serif medium', () => {
+      expect(fontStyles.getSerifMedium('igbo')).toMatchSnapshot();
+    });
+  });
+
+  describe('getSerifMediumItalic', () => {
+    it('should return serif medium italic font css', () => {
+      expect(fontStyles.getSerifMedium('news')).toMatchSnapshot();
+    });
+    it('should return sans bold italic font css for sevice without serif medium italic', () => {
+      expect(fontStyles.getSerifMediumItalic('yoruba')).toMatchSnapshot();
+    });
+  });
+});
