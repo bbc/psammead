8752756e614831efed155524caf2270288d9938f
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-06-14 10:09:48 +0100

    Remove igbo test links

--- packages/components/psammead-navigation/testHelpers/igbo.js
+++ packages/components/psammead-navigation/testHelpers/igbo.js
@@ -15,26 +15,6 @@ const igboNavData = {
     title: 'Nke ka ewuewu',
     url: '/igbo/popular/read',
   },
-  test: {
-    title: 'Centroamérica Cuenta',
-    url: '/igbo/popular/read',
-  },
-  test2: {
-    title: 'América Latina',
-    url: '/igbo/popular/read',
-  },
-  test4: {
-    title: 'International',
-    url: '/igbo/popular/read',
-  },
-  test5: {
-    title: 'Economía',
-    url: '/igbo/popular/read',
-  },
-  test6: {
-    title: 'Tecnología',
-    url: '/igbo/popular/read',
-  },
 };
 
 export default igboNavData;
