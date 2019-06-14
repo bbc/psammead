26335ea204f0ec3b92697bc1db85fd72a7b6f2f1
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-06-14 14:29:14 +0100

    :wrench: Get some ES5 up in here

--- packages/utilities/psammead-locales/moment/ig.js
+++ packages/utilities/psammead-locales/moment/ig.js
@@ -1,66 +1,51 @@
-import moment from 'moment';
-/* eslint-disable */
-//! moment.js locale configuration
+const moment = require('moment');
 
-// ;(function (global, factory) {
-//    typeof exports === 'object' && typeof module !== 'undefined'
-//        && typeof require === 'function' ? factory(require('../moment')) :
-//    typeof define === 'function' && define.amd ? define(['../moment'], factory) :
-//    factory(global.moment)
-// }(this, (function (moment) { 'use strict';
-
-  // let ig = moment.defineLocale('ig', {
-  moment.defineLocale('ig', {
-    months: 'Jenụwarị_Febụwarị_Maachị_Eprel_Mee_Juun_Julaị_Ọgọọst_Septemba_Ọktọba_Nọvemba_Disemba'.split(
-      '_',
-    ),
-    monthsShort: 'Jen_Feb_Maa_Epr_Mee_Juu_Jul_Ọgọ_Sep_Ọkt_Nov_Dis'.split('_'),
-    weekdays: 'Sọnde_Mọnde_Tuzde_Wenesde_Tọsde_Fraịde_Satọde'.split(
-      '_',
-    ),
-    weekdaysShort: 'Sọn_Mọn_Tuz_We_Tọs_Frai_Sat'.split('_'),
-    weekdaysMin: 'Su_Mọ_Tu_We_Tọ_Fr_Sa'.split('_'),
-    longDateFormat: {
-      LT: 'HH:mm',
-      LTS: 'HH:mm:ss',
-      L: 'DD/MM/YYYY',
-      LL: 'D MMMM YYYY',
-      LLL: 'D MMMM YYYY HH:mm',
-      LLLL: 'dddd, D MMMM YYYY HH:mm',
-    },
-    calendar: {
-      sameDay: '[Taa na] LT',
-      nextDay: '[Echi na] LT',
-      nextWeek: 'dddd [na] LT',
-      lastDay: '[Unyaahụ na] LT',
-      lastWeek: 'dddd [gara aga na] LT',
-      sameElse: 'L',
-    },
-    relativeTime: {
-      future: 'na %s',
-      past: '%s gara aga',
-      s: 'ntabịanya ole na ole',
-      ss: 'ntabịanya %d',
-      m: 'otu nkeji',
-      mm: 'nkeji %d',
-      h: 'otu elekere',
-      hh: 'elekere %d',
-      d: 'otu ụbọchị',
-      dd: 'Ụbọchị %d',
-      M: 'otu ọnwa',
-      MM: 'Ọnwa %d',
-      y: 'Otu afọ',
-      yy: 'Afọ %d',
-    },
-    dayOfMonthOrdinalParse: /(Nke )\d{1,2}/,
-    ordinal : function (number) {
-        return 'Nke ' + number;
-    },
-    week: {
-      dow: 1, // Monday is the first day of the week.
-      doy: 4, // The week that contains Jan 4th is the first week of the year.
-    },
-  });
-
-//   return ig;
-// });
+moment.defineLocale('ig', {
+  months: 'Jenụwarị_Febụwarị_Maachị_Eprel_Mee_Juun_Julaị_Ọgọọst_Septemba_Ọktọba_Nọvemba_Disemba'.split(
+    '_',
+  ),
+  monthsShort: 'Jen_Feb_Maa_Epr_Mee_Juu_Jul_Ọgọ_Sep_Ọkt_Nov_Dis'.split('_'),
+  weekdays: 'Sọnde_Mọnde_Tuzde_Wenesde_Tọsde_Fraịde_Satọde'.split('_'),
+  weekdaysShort: 'Sọn_Mọn_Tuz_We_Tọs_Frai_Sat'.split('_'),
+  weekdaysMin: 'Su_Mọ_Tu_We_Tọ_Fr_Sa'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd, D MMMM YYYY HH:mm',
+  },
+  calendar: {
+    sameDay: '[Taa na] LT',
+    nextDay: '[Echi na] LT',
+    nextWeek: 'dddd [na] LT',
+    lastDay: '[Unyaahụ na] LT',
+    lastWeek: 'dddd [gara aga na] LT',
+    sameElse: 'L',
+  },
+  relativeTime: {
+    future: 'na %s',
+    past: '%s gara aga',
+    s: 'ntabịanya ole na ole',
+    ss: 'ntabịanya %d',
+    m: 'otu nkeji',
+    mm: 'nkeji %d',
+    h: 'otu elekere',
+    hh: 'elekere %d',
+    d: 'otu ụbọchị',
+    dd: 'Ụbọchị %d',
+    M: 'otu ọnwa',
+    MM: 'Ọnwa %d',
+    y: 'Otu afọ',
+    yy: 'Afọ %d',
+  },
+  dayOfMonthOrdinalParse: /(Nke )\d{1,2}/,
+  ordinal: function ordinal(number) {
+    return 'Nke '.concat(number);
+  },
+  week: {
+    dow: 1, // Monday is the first day of the week.
+    doy: 4, // The week that contains Jan 4th is the first week of the year.
+  },
+});
--- packages/utilities/psammead-locales/moment/pcm.js
+++ packages/utilities/psammead-locales/moment/pcm.js
@@ -1,71 +1,60 @@
-import moment from 'moment';
-/* eslint-disable */
-//! moment.js locale configuration
+const moment = require('moment');
 
-// ;(function (global, factory) {
-//    typeof exports === 'object' && typeof module !== 'undefined'
-//        && typeof require === 'function' ? factory(require('../moment')) :
-//    typeof define === 'function' && define.amd ? define(['../moment'], factory) :
-//    factory(global.moment)
-// }(this, (function (moment) { 'use strict';
-
-  // let pcm = moment.defineLocale('pcm', {
-  moment.defineLocale('pcm', {
-    months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split(
-      '_',
-    ),
-    monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
-    weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split(
-      '_',
-    ),
-    weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
-    weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
-    longDateFormat: {
-      LT: 'HH:mm',
-      LTS: 'HH:mm:ss',
-      L: 'DD/MM/YYYY',
-      LL: 'D MMMM YYYY',
-      LLL: 'D MMMM YYYY HH:mm',
-      LLLL: 'dddd, D MMMM YYYY HH:mm',
-    },
-    calendar: {
-      sameDay: '[Today at] LT',
-      nextDay: '[Tomorrow at] LT',
-      nextWeek: 'dddd [at] LT',
-      lastDay: '[Yesterday at] LT',
-      lastWeek: '[Last] dddd [at] LT',
-      sameElse: 'L',
-    },
-    relativeTime: {
-      future: 'inside %s',
-      past: '%s wey don pass',
-      s: 'just some seconds',
-      ss: '%d seconds',
-      m: 'one minute',
-      mm: '%d minutes',
-      h: 'one hour',
-      hh: '%d hours',
-      d: 'one day',
-      dd: '%d days',
-      M: 'one month',
-      MM: '%d months',
-      y: 'one year',
-      yy: '%d years',
-    },
-    dayOfMonthOrdinalParse: /\d{1,2}(st|nd|rd|th)/,
-    ordinal : function (number) {
-        var b = number % 10,
-            output = (~~(number % 100 / 10) === 1) ? 'th' :
-            (b === 1) ? 'st' :
-            (b === 2) ? 'nd' :
-            (b === 3) ? 'rd' : 'th';
-        return number + output;
-    },
-    week: {
-      dow: 1, // Monday is the first day of the week.
-      doy: 4, // The week that contains Jan 4th is the first week of the year.
-    },
-  });
-
-//   return pcm;
-// });
+moment.defineLocale('pcm', {
+  months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split(
+    '_',
+  ),
+  monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
+  weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split(
+    '_',
+  ),
+  weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
+  weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd, D MMMM YYYY HH:mm',
+  },
+  calendar: {
+    sameDay: '[Today at] LT',
+    nextDay: '[Tomorrow at] LT',
+    nextWeek: 'dddd [at] LT',
+    lastDay: '[Yesterday at] LT',
+    lastWeek: '[Last] dddd [at] LT',
+    sameElse: 'L',
+  },
+  relativeTime: {
+    future: 'inside %s',
+    past: '%s wey don pass',
+    s: 'just some seconds',
+    ss: '%d seconds',
+    m: 'one minute',
+    mm: '%d minutes',
+    h: 'one hour',
+    hh: '%d hours',
+    d: 'one day',
+    dd: '%d days',
+    M: 'one month',
+    MM: '%d months',
+    y: 'one year',
+    yy: '%d years',
+  },
+  dayOfMonthOrdinalParse: /\d{1,2}(st|nd|rd|th)/,
+  /* eslint-disable */
+  // Copied exactly from https://github.com/moment/moment/blob/develop/locale/en-gb.js#L50-L57
+  ordinal : function (number) {
+      var b = number % 10,
+          output = (~~(number % 100 / 10) === 1) ? 'th' :
+          (b === 1) ? 'st' :
+          (b === 2) ? 'nd' :
+          (b === 3) ? 'rd' : 'th';
+      return number + output;
+  },
+  week: {
+    dow: 1, // Monday is the first day of the week.
+    doy: 4, // The week that contains Jan 4th is the first week of the year.
+  },
+});
