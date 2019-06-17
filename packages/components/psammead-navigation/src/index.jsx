b4c8c6ffa0a36644307308f0f9223ec98267141e
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-06-17 15:31:36 +0100

    Add focus state

--- packages/components/psammead-navigation/src/index.jsx
+++ packages/components/psammead-navigation/src/index.jsx
@@ -66,6 +66,20 @@ const StyledLink = styled.a`
   &:hover::before {
     ${ListItemBorder}
   }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: 0.25rem solid ${C_WHITE};
+  }
 `;
 
 const StyledSpan = styled.span`
