1588cce5cd6db599ee926abb334c775acf5da38b
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-06-17 14:37:54 +0100

    Remove border bottom, space on the sides and rtl support. Add current item span

--- packages/components/psammead-navigation/src/index.jsx
+++ packages/components/psammead-navigation/src/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled, { css } from 'styled-components';
-import { shape, string, node, bool, oneOf } from 'prop-types';
+import { shape, string, node, bool } from 'prop-types';
+import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
 import { C_WHITE, C_POSTBOX, C_GHOST } from '@bbc/psammead-styles/colours';
-import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import { GEL_SPACING } from '@bbc/gel-foundations/spacings';
 import {
   GEL_GROUP_2_SCREEN_WIDTH_MAX,
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
@@ -10,23 +11,12 @@ import {
 import { getPica, GEL_FF_REITH_SANS } from '@bbc/gel-foundations/typography';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
 
-const NavigationWrapper = styled.div`
-  padding: 0 ${GEL_SPACING_DBL};
+const NavigationWrapper = styled.nav`
+  padding: 0 ${GEL_SPACING};
   background-color: ${C_POSTBOX};
-  position: relative;
 
   @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
-    padding: 0 ${GEL_SPACING};
-    /* Trick to hide the last line border bottom */
-    &::after {
-      content: ' ';
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      border-bottom: 1px solid #ffffff;
-      z-index: 2;
-    }
+    padding: 0;
   }
 `;
 
@@ -34,7 +24,6 @@ const StyledUnorderedList = styled.ul`
   list-style-type: none;
   padding: 0;
   margin: 0;
-  overflow: hidden;
 `;
 
 const StyledListItem = styled.li`
@@ -42,96 +31,67 @@ const StyledListItem = styled.li`
   position: relative;
   z-index: 1;
 
-  ${({ dir }) =>
-    dir === 'ltr'
-      ? css`
-          &:last-child > a {
-            padding-right: ${GEL_SPACING};
-          }
-
-          &:last-child > a:hover::before {
-            right: ${GEL_SPACING};
-          }
-        `
-      : css`
-          &:last-child > a {
-            padding-left: ${GEL_SPACING};
-          }
-
-          &:last-child > a:hover::before {
-            left: ${GEL_SPACING};
-          }
-        `};
-
   @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
     /* Trick a separator line which takes the full width */
     &::after {
       content: ' ';
       position: absolute;
+      left: 0;
       bottom: 0;
       width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN};
       border-bottom: 1px solid #eab3b3; /* White with 30% transparency over #B80000 */
       z-index: -1;
-
-      ${({ dir }) =>
-        dir === 'ltr'
-          ? css`
-              left: 0;
-            `
-          : css`
-              right: 0;
-            `};
     }
   }
 `;
 
-const LinkBorder = css`
+const ListItemBorder = css`
   content: '';
   position: absolute;
+  left: 0;
+  right: 0;
   bottom: 0;
   border-bottom: 0.25rem solid ${C_WHITE};
-
-  ${({ dir }) =>
-    dir === 'ltr'
-      ? css`
-          left: 0;
-          right: ${GEL_SPACING_DBL};
-        `
-      : css`
-          left: ${GEL_SPACING_DBL};
-          right: 0;
-        `}
 `;
 
-const Link = styled.a`
+const StyledLink = styled.a`
   ${props => (props.script ? getPica(props.script) : '')};
   font-family: ${GEL_FF_REITH_SANS};
   color: ${C_GHOST};
   cursor: pointer;
   text-decoration: none;
   display: inline-block;
-
-  ${({ dir }) =>
-    dir === 'ltr'
-      ? css`
-          padding: 0.75rem ${GEL_SPACING_DBL} 0.75rem 0;
-        `
-      : css`
-          padding: 0.75rem 0 0.75rem ${GEL_SPACING_DBL};
-        `}
-
-  ${({ active }) =>
-    active &&
-    css`
-      &::before {
-        ${LinkBorder}
-      }
-    `}
+  padding: 0.75rem ${GEL_SPACING};
 
   &:hover::before {
-    ${LinkBorder}
+    ${ListItemBorder}
   }
 `;
+
+const StyledSpan = styled.span`
+  ${props => (props.script ? getPica(props.script) : '')};
+  font-family: ${GEL_FF_REITH_SANS};
+  color: ${C_GHOST};
+  display: inline-block;
+  padding: 0.75rem ${GEL_SPACING};
+
+  &::before {
+    ${ListItemBorder}
+  }
+`;
+
+const CurrentItem = ({ children: link, script }) => (
+  <Fragment>
+    <StyledSpan
+      // eslint-disable-next-line jsx-a11y/aria-role
+      role="text"
+      script={script}
+    >
+      <VisuallyHiddenText>Current page,</VisuallyHiddenText>
+      {link}
+    </StyledSpan>
+  </Fragment>
+);
 
 export const NavigationUl = ({ children, ...props }) => (
   <StyledUnorderedList role="list" {...props}>
@@ -139,16 +99,20 @@ export const NavigationUl = ({ children, ...props }) => (
   </StyledUnorderedList>
 );
 
-export const NavigationLi = ({ children: link, url, script, dir, active }) => (
-  <StyledListItem role="listitem" dir={dir}>
-    <Link href={url} script={script} dir={dir} active={active}>
-      {link}
-    </Link>
+export const NavigationLi = ({ children: link, url, script, active }) => (
+  <StyledListItem role="listitem">
+    {active ? (
+      <CurrentItem script={script}>{link}</CurrentItem>
+    ) : (
+      <StyledLink href={url} script={script}>
+        {link}
+      </StyledLink>
+    )}
   </StyledListItem>
 );
 
 const Navigation = ({ children }) => (
-  <NavigationWrapper>{children}</NavigationWrapper>
+  <NavigationWrapper role="navigation">{children}</NavigationWrapper>
 );
 
 Navigation.propTypes = {
@@ -163,13 +127,16 @@ NavigationLi.propTypes = {
   children: node.isRequired,
   url: string.isRequired,
   script: shape(scriptPropType).isRequired,
-  dir: oneOf(['ltr', 'rtl']),
   active: bool,
 };
 
 NavigationLi.defaultProps = {
-  dir: 'ltr',
   active: false,
+};
+
+CurrentItem.propTypes = {
+  children: string.isRequired,
+  script: shape(scriptPropType).isRequired,
 };
 
 export default Navigation;
--- packages/components/psammead-navigation/src/index.stories.jsx
+++ packages/components/psammead-navigation/src/index.stories.jsx
@@ -1,53 +1,87 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import { dirDecorator } from '@bbc/psammead-storybook-helpers';
+import { select, number, withKnobs } from '@storybook/addon-knobs';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import { latin } from '@bbc/gel-foundations/scripts';
+import * as svgs from '@bbc/psammead-assets/svgs';
+import Brand from '@bbc/psammead-brand';
 import Navigation, { NavigationUl, NavigationLi } from './index';
 import igboNavData from '../testHelpers/igbo';
 import pidginNavData from '../testHelpers/pidgin';
 import yorubaNavData from '../testHelpers/yoruba';
 import notes from '../README.md';
 
+const inputs = () => {
+  // capitalization is only for presentation purpose on the knob
+  const options = Object.keys(svgs)
+    .filter(key => key !== 'BBC_BLOCKS')
+    .map(key => key.charAt(0).toUpperCase() + key.slice(1));
+
+  const svgChoice = select('Service SVG', options, 'News').toLowerCase();
+  const svgRatio = svgs[svgChoice].ratio;
+  const svgMaxHeight = 24;
+  const svgMinHeight = 16;
+  const minWidthInput = number('minimum svg width', svgRatio * svgMinHeight);
+  const maxWidthInput = number('maximum svg width', svgRatio * svgMaxHeight);
+  const svgHeightInput = number('desired height svg', svgMaxHeight);
+
+  return { svgChoice, svgHeightInput, minWidthInput, maxWidthInput };
+};
+
 storiesOf('Components|Navigation', module)
   .addDecorator(withKnobs)
-  .addDecorator(dirDecorator)
-
   .add(
-    'igbo',
-    ({ dir }) => (
-      <Navigation>
-        <NavigationUl>
-          {Object.keys(igboNavData).map((item, key) => {
-            const { title, url } = igboNavData[item];
+    'igbo with Brand',
+    inputProvider([], () => {
+      const {
+        svgHeightInput,
+        minWidthInput,
+        maxWidthInput,
+        svgChoice,
+      } = inputs();
 
-            let active;
-            if (key === 0) {
-              active = true;
-            }
+      return (
+        <Fragment>
+          <Brand
+            brandName="Default Brand Name"
+            svgHeight={svgHeightInput}
+            minWidth={minWidthInput}
+            maxWidth={maxWidthInput}
+            svg={svgs[svgChoice]}
+          />
+          <Navigation>
+            <NavigationUl>
+              {Object.keys(igboNavData).map((item, key) => {
+                const { title, url } = igboNavData[item];
 
-            return (
-              <NavigationLi
-                key={title}
-                url={url}
-                script={latin}
-                dir={dir}
-                active={active}
-              >
-                {title}
-              </NavigationLi>
-            );
-          })}
-        </NavigationUl>
-      </Navigation>
-    ),
+                let active;
+                if (key === 0) {
+                  active = true;
+                }
+
+                return (
+                  <NavigationLi
+                    key={title}
+                    url={url}
+                    script={latin}
+                    active={active}
+                  >
+                    {title}
+                  </NavigationLi>
+                );
+              })}
+            </NavigationUl>
+          </Navigation>
+        </Fragment>
+      );
+    }),
     {
       notes,
     },
   )
   .add(
     'pidgin',
-    ({ dir }) => (
+    () => (
       <Navigation>
         <NavigationUl>
           {Object.keys(pidginNavData).map((item, key) => {
@@ -63,7 +97,6 @@ storiesOf('Components|Navigation', module)
                 key={title}
                 url={url}
                 script={latin}
-                dir={dir}
                 active={active}
               >
                 {title}
@@ -79,7 +112,7 @@ storiesOf('Components|Navigation', module)
   )
   .add(
     'yoruba',
-    ({ dir }) => (
+    () => (
       <Navigation>
         <NavigationUl>
           {Object.keys(yorubaNavData).map((item, key) => {
@@ -95,7 +128,6 @@ storiesOf('Components|Navigation', module)
                 key={title}
                 url={url}
                 script={latin}
-                dir={dir}
                 active={active}
               >
                 {title}
