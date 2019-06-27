193fec1298c43df63c9af40c09b66d8d86589d57
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-06-27 16:19:12 +0100

    use @testing-library/react to generate snapshots

--- packages/utilities/psammead-test-helpers/src/index.js
+++ packages/utilities/psammead-test-helpers/src/index.js
@@ -1,11 +1,12 @@
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import 'jest-styled-components';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
 export const shouldMatchSnapshot = (title, component) => {
   it(title, () => {
-    const tree = renderer.create(component).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(component);
+    expect(container.firstChild).toMatchSnapshot();
   });
 };
 
