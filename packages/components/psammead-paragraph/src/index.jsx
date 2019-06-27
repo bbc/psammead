3dbbbe920d2edfcecb89ad4b26c7260b121dc728
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-06-27 10:08:35 +0100

    add all font-faces to storybook css

--- packages/components/psammead-paragraph/src/index.jsx
+++ packages/components/psammead-paragraph/src/index.jsx
@@ -2,23 +2,25 @@ import styled from 'styled-components';
 import { shape, string } from 'prop-types';
 import { C_SHADOW } from '@bbc/psammead-styles/colours';
 import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
-import {
-  getBodyCopy,
-  GEL_FF_REITH_SANS,
-} from '@bbc/gel-foundations/typography';
+import { getBodyCopy } from '@bbc/gel-foundations/typography';
+import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
 
 const Paragraph = styled.p`
   ${props => (props.script ? getBodyCopy(props.script) : '')};
+  ${props => getSansRegular(props.product)};
   color: ${C_SHADOW};
-  font-family: ${GEL_FF_REITH_SANS};
   padding-bottom: ${GEL_SPACING_TRPL};
   margin: 0; /* Reset */
 `;
 
 Paragraph.propTypes = {
-  product: string.isRequired,
+  product: string,
   script: shape(scriptPropType).isRequired,
+};
+
+Paragraph.defaultProps = {
+  product: 'news',
 };
 
 export default Paragraph;
--- packages/components/psammead-paragraph/src/index.stories.jsx
+++ packages/components/psammead-paragraph/src/index.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, select } from '@storybook/addon-knobs';
+
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
 import Paragraph from './index';
@@ -9,8 +10,13 @@ storiesOf('Components|Paragraph', module)
   .addDecorator(withKnobs)
   .add(
     'default',
-    inputProvider([{ name: 'Paragraph' }], ([paragraph], script) => (
-      <Paragraph script={script}>{paragraph}</Paragraph>
-    )),
+    inputProvider([{ name: 'Paragraph' }], ([paragraph], script) => {
+      const product = select('Product', ['news', 'persian'], 'news');
+      return (
+        <Paragraph script={script} product={product}>
+          {paragraph}
+        </Paragraph>
+      );
+    }),
     { notes, knobs: { escapeHTML: false } },
   );
