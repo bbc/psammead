66b00bcc34c84191969b061c35c5435046b376de
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-07-17 14:36:09 +0100

    use regex to match and return value of changelog table header

--- scripts/bumpChangelogs/getNewChangelogContent.js
+++ scripts/bumpChangelogs/getNewChangelogContent.js
@@ -2,8 +2,9 @@ module.exports = (version, prLink, changesDescription) => existingChangelog => {
   const changelogLines = existingChangelog.split('\n');
   const [prNumber] = prLink.split('/').splice(-1);
   const newChanges = `| ${version} | [PR#${prNumber}](${prLink}) ${changesDescription} |`;
-  const lineNumberToAddChanges =
-    changelogLines.indexOf('| Version | Description |') + 2;
+  const tableHeadRegex = /\|.+?Version.+?|.+?Description.+?\|/;
+  const tableHead = changelogLines.find(line => line.match(tableHeadRegex));
+  const lineNumberToAddChanges = changelogLines.indexOf(tableHead) + 2;
   changelogLines.splice(lineNumberToAddChanges, 0, newChanges);
 
   return changelogLines.join('\n');
