4c77593cc1d3e96c3cbdc76d832c72a54edd345d
Ruth Ogendi <ruth.ogendi@andela.com>
2019-07-23 12:10:34 +0300

    Add test PNGs. Add Storybook file-loader.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,19 @@
 const path = require('path'); 
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
   resolve: {
     alias: {
       "styled-components": path.resolve(__dirname, "..","node_modules", "styled-components"),
