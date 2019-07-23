c72f587a61cae70a51241b4314fa94562840ca59
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-07-23 17:44:03 +0100

    :rocket: Working update, bump, install & changelog

--- scripts/bumper/index.js
+++ scripts/bumper/index.js
@@ -1,11 +1,34 @@
 const getChangedPackages = require('./getChangedPackages');
 const upgradeDependencies = require('../upgradeDependencies');
+const bumpPackages = require('../bumpPackages/index.js');
+const getPackagePath = require('../utilities/getPackagePath');
+const runNpmInstall = require('../regeneratePackageLocks/runNpmInstall');
+const bumpChangelogs = require('../bumpChangelogs/index.js');
 
 const packages = getChangedPackages();
 const stuff = upgradeDependencies(packages);
 
 stuff.then(bumpedPackages => {
-  console.log(bumpedPackages);
+  const bumpedPackagesNoBBCPrefix = bumpedPackages.map(dep =>
+    dep.replace('@bbc/', ''),
+  );
 
-
+  bumpPackages({ packageNames: bumpedPackagesNoBBCPrefix, version: 'patch' })
+    .then(() => {
+      return Promise.all(
+        bumpedPackagesNoBBCPrefix.map(dep =>
+          runNpmInstall(getPackagePath(dep)),
+        ),
+      );
+    })
+    .then(() =>
+      bumpChangelogs({
+        packageNames: bumpedPackagesNoBBCPrefix,
+        prLink: 'https://hello.com',
+        changesDescription: 'Bump Dependancies',
+      }),
+    )
+    .then(() => {
+      console.log('Done');
+    });
 });
