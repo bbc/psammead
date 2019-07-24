9b47446b35803b2c70ee4d4a0da3f86dec8404e7
Sareh Heidari <sareh.heidari@gmail.com>
2019-07-24 12:54:50 +0100

    Add stories for numerals and also add entry file

---
+++ packages/utilities/psammead-locales/numerals.js
@@ -0,0 +1,1 @@
+module.exports = require('./dist/numerals'); // eslint-disable-line import/no-unresolved
---
+++ packages/utilities/psammead-locales/numerals/index.stories.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+import { GEL_SPACING } from '@bbc/gel-foundations/spacings';
+import * as numerals from '../src/numerals';
+
+const numeralSystems = Object.keys(numerals);
+const stories = storiesOf('Utilities|Psammead Locales', module);
+
+const Container = styled.div`
+  padding: ${GEL_SPACING};
+`;
+
+const Row = styled.div``;
+
+const Value = styled.div`
+  display: inline-block;
+  margin: ${GEL_SPACING};
+`;
+
+stories.add('Numerals', () => {
+  return (
+    <Container>
+      {numeralSystems.map(numeralSystem => (
+        <Row>
+          <Value>{numeralSystem}</Value>
+          <Value>{numerals[numeralSystem]}</Value>
+        </Row>
+      ))}
+    </Container>
+  );
+});
