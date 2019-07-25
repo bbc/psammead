b66ed61895468c98913ea6c28101d4db5c86f8b8
Sareh Heidari <sareh.heidari@gmail.com>
2019-07-25 16:19:10 +0100

    Fix test structure

--- packages/utilities/psammead-locales/src/numerals/index.test.js
+++ packages/utilities/psammead-locales/src/numerals/index.test.js
@@ -1,11 +1,10 @@
 import * as numerals from '.';
 
-describe('Numerals', () => {});
-
-const numeralSystems = Object.keys(numerals);
-
-numeralSystems.forEach(numeralSystem => {
-  it(`should return ${numeralSystem} numerals`, () => {
-    expect(numerals[numeralSystem]).toMatchSnapshot();
+describe('Numerals', () => {
+  const numeralSystems = Object.keys(numerals);
+  numeralSystems.forEach(numeralSystem => {
+    it(`should return ${numeralSystem} numerals`, () => {
+      expect(numerals[numeralSystem]).toMatchSnapshot();
+    });
   });
 });
