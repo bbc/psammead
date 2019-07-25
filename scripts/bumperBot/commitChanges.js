c2801b0eb34919a5f9f238f8dface782836638e8
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-07-25 13:35:04 +0100

    Missed quote

--- scripts/bumperBot/commitChanges.js
+++ scripts/bumperBot/commitChanges.js
@@ -2,7 +2,7 @@ const runExec = require('../utilities/runExec');
 
 const commitChanges = message => {
   return runExec({ command: `git add packages` })
-    .then(() => runExec({ command: `git commit -m "${message}` }))
+    .then(() => runExec({ command: `git commit -m "${message}"` }))
     .then(() => runExec({ command: `git push origin HEAD` }));
 };
 
