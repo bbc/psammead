e8a03f32e0a05ed21fd7302bf81ff6aacfa7e238
Sareh Heidari <sareh.heidari@gmail.com>
2019-07-29 11:30:38 +0100

    Add exports for all service font families
    Rename Latin Reith sansItalic to sansRegularItalic according to UX feedback

--- packages/utilities/psammead-styles/src/font-families.js
+++ packages/utilities/psammead-styles/src/font-families.js
@@ -10,12 +10,12 @@ const getFontStyleAndWeight = (style, weight) =>
   `font-weight: ${weight};
    font-style: ${style};`;
 
-const newsReithFontStyles = {
+const latinReithFontStyles = {
   sansRegular: `
    ${reithSans}
    ${getFontStyleAndWeight('normal', 400)} 
   `,
-  sansItalic: `
+  sansRegularItalic: `
    ${reithSans}
    ${getFontStyleAndWeight('italic', 400)} 
   `,
@@ -84,55 +84,270 @@ const nassimFontStyles = fontFamily => ({
     ${fontFamily}
     ${getFontStyleAndWeight('normal', 400)}
   `,
-  sansBold: `
-    ${fontFamily}
-    ${getFontStyleAndWeight('normal', 700)}
-  `,
 });
 
-export const afaanoromoo = newsReithFontStyles;
-export const afrique = newsReithFontStyles;
-export const amharic = newsReithFontStyles;
+/*
+ *  AMHARIC
+ */
+const amharicFontFamily = `font-family: "Noto Sans Ethiopic", Arial, Verdana, Geneva, Helvetica, sans-serif;`;
+
+const amharicStyles = {
+  sansRegular: `
+    ${amharicFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  BENGALI
+ */
+const bengaliFontFamily = `font-family: "Shonar Bangla", Helmet, Freesans, Helvetica, Arial, sans-serif;`;
+
+const bengaliStyles = {
+  sansRegular: `
+    ${bengaliFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  BURMESE
+ */
+const burmeseFontFamily = `font-family: Padauk, Arial, Verdana, Geneva, Helvetica, sans-serif;`;
+
+const burmeseStyles = {
+  sansRegular: `
+    ${burmeseFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  CHINESE
+ */
+const chineseFontFamily = `font-family: Helvetica, Arial, STHeiti, 华文黑体, "Microsoft YaHei", 微软雅黑, SimSun, 宋体;`;
+
+const chineseFontStyles = {
+  sansRegular: `
+    ${chineseFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+  sansBold: `
+    ${chineseFontFamily}
+    ${getFontStyleAndWeight('normal', 700)}
+  `,
+};
+
+/*
+ *  GUJARATI
+ */
+const gujaratiFontFamily = `font-family: "Mukta Vaani", Rasa, "Noto Sans Gujarati", "Hind vadodara", Shruti, Gautami, "Gujarati Sangam MN", "Gujarati MT", sans-serif;`;
+
+const gujaratiStyles = {
+  sansRegular: `
+    ${gujaratiFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  HINDI
+ */
+const hindiFontFamily = `font-family: "Noto Sans Devanagari", Arial, Verdana, Geneva, Helvetica, sans-serif;`;
+
+const hindiStyles = {
+  sansRegular: `
+    ${hindiFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  JAPANESE
+ */
+const japaneseFontFamily = `font-family: "Hiragino Kaku Gothic Pro", "ﾋﾗギﾉ角ゴPro W3", "MS PGothic", "MS UI Gothic", Helvetica, Arial, sans-serif;`;
+
+const japaneseStyles = {
+  sansRegular: `
+    ${japaneseFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  KOREAN
+ */
+const koreanFontFamily = `font-family: "Apple SD Gothic Neo", AppleGothic, "Malgun Gothic", Dotum, "Noto Sans CJK KR", sans-serif;`;
+
+const koreanStyles = {
+  sansRegular: `
+    ${koreanFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  MARATHI
+ */
+const marathiFontFamily = `font-family: "Noto Sans Devanagari", Hind, "Mukta Vaani", Gautami, "Kohinoor Devanagari", sans-serif;`;
+
+const marathiStyles = {
+  sansRegular: `
+    ${marathiFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  NEPALI
+ */
+const nepaliFontFamily = `font-family: "Noto Sans Devanagari", Arial, Verdana, Geneva, Helvetica, sans-serif;`;
+
+const nepaliStyles = {
+  sansRegular: `
+    ${nepaliFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+/*
+ *  PUNJABI
+ */
+
+const punjabiFontFamily = `font-family: "Noto Sans Gurmukhi", Gautami, "Kohinoor Gurmukhi", sans-serif;`;
+
+const punjabiStyles = {
+  sansRegular: `
+    ${punjabiFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+  sansItalic: `
+    ${punjabiFontFamily}
+    ${getFontStyleAndWeight('italic', 400)}
+  `,
+  sansBold: `
+    ${punjabiFontFamily}
+    ${getFontStyleAndWeight('normal', 700)}
+  `,
+  sansBoldItalic: `
+    ${punjabiFontFamily}
+    ${getFontStyleAndWeight('italic', 700)}
+  `,
+};
+
+/*
+ *  SINHALA
+ */
+const sinhalaFontFamily = `font-family: "Iskoola Pota BBC", Arial, Verdana, Geneva, Helvetica, sans-serif;`;
+
+const sinhalaStyles = {
+  sansRegular: `
+  ${sinhalaFontFamily}
+  ${getFontStyleAndWeight('normal', 400)}
+`,
+};
+
+/*
+ *  TAMIL
+ */
+const tamilFontFamily = `font-family: Latha, Helmet, Freesans, Helvetica, Arial, sans-serif;`;
+
+const tamilStyles = {
+  sansRegular: `
+  ${tamilFontFamily}
+  ${getFontStyleAndWeight('normal', 400)}
+`,
+};
+
+/*
+ *  TELEGU
+ */
+const teleguFontFamily = `font-family: Mallanna, "Noto Sans Telugu", "Hind Guntur", Gautami, "Kohinoor Telugu", sans-serif;`;
+
+const teleguStyles = {
+  sansRegular: `
+  ${teleguFontFamily}
+  ${getFontStyleAndWeight('normal', 400)}
+`,
+};
+
+/*
+ *  THAI
+ */
+const thaiFontFamily = `font-family: Tahoma, Helmet, freesans, Helvetica, Arial, sans-serif;`;
+
+const thaiStyles = {
+  sansRegular: `
+    ${thaiFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+  sansItalic: `
+    ${thaiFontFamily}
+    ${getFontStyleAndWeight('italic', 400)}
+  `,
+  sansBold: `
+    ${thaiFontFamily}
+    ${getFontStyleAndWeight('normal', 700)}
+  `,
+  sansBoldItalic: `
+    ${thaiFontFamily}
+    ${getFontStyleAndWeight('italic', 700)}
+  `,
+};
+
+/*
+ *  TIGRINYA
+ */
+const tigrinyaFontFamily = `font-family: "Noto Sans Ethiopic", Arial, Verdana, Geneva, Helvetica, sans-serif;`;
+
+const tigrinyaStyles = {
+  sansRegular: `
+    ${tigrinyaFontFamily}
+    ${getFontStyleAndWeight('normal', 400)}
+  `,
+};
+
+export const afaanoromoo = helmetFontStyles;
+export const afrique = helmetFontStyles;
+export const amharic = amharicStyles;
 export const arabic = nassimFontStyles(nassimArabicFontFamily);
-export const azeri = newsReithFontStyles;
-export const bengali = newsReithFontStyles;
-export const burmese = newsReithFontStyles;
-export const cymrufyw = newsReithFontStyles;
-export const gahuza = newsReithFontStyles;
-export const gujarati = newsReithFontStyles;
-export const hausa = newsReithFontStyles;
-export const hindi = newsReithFontStyles;
+export const azeri = helmetFontStyles;
+export const bengali = bengaliStyles;
+export const burmese = burmeseStyles;
+export const cymrufyw = latinReithFontStyles;
+export const gahuza = helmetFontStyles;
+export const gujarati = gujaratiStyles;
+export const hausa = helmetFontStyles;
+export const hindi = hindiStyles;
 export const igbo = helmetFontStyles;
-export const index = newsReithFontStyles;
-export const indonesia = newsReithFontStyles;
-export const japanese = newsReithFontStyles;
-export const korean = newsReithFontStyles;
-export const kyrgyz = newsReithFontStyles;
-export const loadableConfig = newsReithFontStyles;
-export const marathi = newsReithFontStyles;
-export const mundo = newsReithFontStyles;
-export const naidheachdan = newsReithFontStyles;
-export const nepali = newsReithFontStyles;
-export const news = newsReithFontStyles;
+export const indonesia = helmetFontStyles;
+export const japanese = japaneseStyles;
+export const korean = koreanStyles;
+export const kyrgyz = helmetFontStyles;
+export const marathi = marathiStyles;
+export const mundo = helmetFontStyles;
+export const naidheachdan = latinReithFontStyles;
+export const nepali = nepaliStyles;
+export const news = latinReithFontStyles;
 export const pashto = nassimFontStyles(nassimPashtoFontFamily);
 export const persian = nassimFontStyles(nassimPersianFontFamily);
 export const pidgin = helmetFontStyles;
-export const portuguese = newsReithFontStyles;
-export const punjabi = newsReithFontStyles;
-export const russian = newsReithFontStyles;
-export const serbian = newsReithFontStyles;
-export const sinhala = newsReithFontStyles;
-export const somali = newsReithFontStyles;
-export const swahili = newsReithFontStyles;
-export const tamil = newsReithFontStyles;
-export const telugu = newsReithFontStyles;
-export const thai = newsReithFontStyles;
-export const tigrinya = newsReithFontStyles;
-export const turkce = newsReithFontStyles;
-export const ukchina = newsReithFontStyles;
-export const ukrainian = newsReithFontStyles;
+export const portuguese = helmetFontStyles;
+export const punjabi = punjabiStyles;
+export const russian = helmetFontStyles;
+export const serbian = helmetFontStyles;
+export const sinhala = sinhalaStyles;
+export const somali = helmetFontStyles;
+export const swahili = helmetFontStyles;
+export const tamil = tamilStyles;
+export const telugu = teleguStyles;
+export const thai = thaiStyles;
+export const tigrinya = tigrinyaStyles;
+export const turkce = helmetFontStyles;
+export const ukchina = chineseFontStyles;
+export const ukrainian = helmetFontStyles;
 export const urdu = nassimFontStyles(nassimUrduFontFamily);
-export const uzbek = newsReithFontStyles;
-export const vietnamese = newsReithFontStyles;
+export const uzbek = helmetFontStyles;
+export const vietnamese = helmetFontStyles;
 export const yoruba = helmetFontStyles;
-export const zhongwen = newsReithFontStyles;
+export const zhongwen = chineseFontStyles;
