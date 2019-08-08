e62cb107c0368d9373f1ea0bc0f3ef211b0f75d8
sadickisaac <isaac.odhiambo@andela.com>
2019-08-08 11:19:35 +0300

    Remove text knob from input-provider

--- packages/utilities/psammead-storybook-helpers/src/input-provider.jsx
+++ packages/utilities/psammead-storybook-helpers/src/input-provider.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { text, select } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 import { Helmet } from 'react-helmet';
 import * as scripts from '@bbc/gel-foundations/scripts';
 import LANGUAGE_VARIANTS from './text-variants';
@@ -16,18 +16,15 @@ const inputProvider = (slots, componentFunction, services) => () => {
   const service = LANGUAGE_VARIANTS[serviceName];
   const isNews = serviceName === 'news';
 
-  const slotTexts = (slots || []).map(({ name, defaultText }) =>
-    text(
-      `Content for ${name}`,
-      // Expect defaultText to be in English. When it is provided and we're
-      // displaying English language on the story, set the default text for
-      // this knob to defaultText.
-      // When we switch to a language other than English, set the default
-      // text for the knob to the snippet from LANGUAGE_VARIANTS for that
-      // language.
-      defaultText && isNews ? defaultText : service.text,
-    ),
-  );
+  const slotTexts = (slots || []).map(({ defaultText }) => {
+    // Expect defaultText to be in English. When it is provided and we're
+    // displaying English language on the story, set the default text for
+    // this knob to defaultText.
+    // When we switch to a language other than English, set the default
+    // text for the knob to the snippet from LANGUAGE_VARIANTS for that
+    // language.
+    return defaultText && isNews ? defaultText : service.text;
+  });
 
   const script = scripts[service.script];
   const dir = service.dir || 'ltr';
