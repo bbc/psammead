129bc6e6e542e98a191476fab121411929606bba
thekp <khoa.phan@bbc.co.uk>
2019-08-19 13:42:46 +0100

    update question validation tests

--- .yeoman/src/questions/index.test.js
+++ .yeoman/src/questions/index.test.js
@@ -1,40 +1,47 @@
 const questions = require('./index');
+const getExistingPackages = require('../get-existing/index.js');
 
-describe('question functions', () => {
-  it('package name validation works', () => {
-    expect(questions[0].validate()).toBe(
-      'Please enter a valid package name - lowercase letters, non-leading numbers and hyphens only please.',
-    );
-    expect(questions[0].validate('9999'.toString())).toBe(
-      'Please enter a valid package name - lowercase letters, non-leading numbers and hyphens only please.',
-    );
-    // expect(questions[0].validate('psammead-styles')).toBe(
-    //   'Sorry! That package name is taken! Please pick another.',
-    // );
-    expect(questions[0].validate('???&&')).toBe(
-      'Please enter a valid package name - lowercase letters, non-leading numbers and hyphens only please.',
-    );
+const componentDir = [
+  'foobar.js',
+  'README.md',
+  'psammead-foobar',
+  'psammead-foobar',
+];
+
+jest.mock('../get-existing/index.js', () => jest.fn());
+getExistingPackages.mockImplementation(() => {
+  return componentDir;
+});
+
+describe('Questions', () => {
+  it('should return validatation warnings', () => {
+    const packageNameWarning =
+      'Please enter a valid package name - lowercase letters, non-leading numbers and hyphens only please.';
+
+    const existingWarning =
+      'Sorry! That package name is taken! Please pick another.';
+
+    const emailWarning = 'Please enter a valid email address';
+
+    const authorWarning = 'Please enter a valid name';
+
+    const descriptionWarning = 'Please enter a valid module description';
+
+    expect(questions[0].validate()).toBe(packageNameWarning);
+    expect(questions[0].validate('9999'.toString())).toBe(packageNameWarning);
+    expect(questions[0].validate('???&&')).toBe(packageNameWarning);
+    expect(questions[0].validate('foo bar')).toBe(packageNameWarning);
+    expect(questions[0].validate('psammead-foobar')).toBe(existingWarning);
+    expect(questions[1].validate()).toBe(descriptionWarning);
+    expect(questions[2].validate()).toBe(authorWarning);
+    expect(questions[3].validate('foobar@foobar')).toBe(emailWarning);
+    expect(questions[3].validate('@.')).toBe(emailWarning);
   });
-  it('does not accept an empty description', () => {
-    expect(questions[1].validate('')).toBe(
-      'Please enter a valid module description',
-    );
-  });
-  it('accepts a valid description', () => {
+
+  it('should pass the validation checks', () => {
+    expect(questions[0].validate('new-package')).toBe(true);
     expect(questions[1].validate('a lovely description')).toBe(true);
-  });
-  it('rejects a short author name', () => {
-    expect(questions[2].validate('pi')).toBe('Please enter a valid name');
-  });
-  it('accepts a valid author name', () => {
     expect(questions[2].validate('Test User')).toBe(true);
-  });
-  it('rejects invalid email', () => {
-    expect(questions[3].validate('@@@@')).toBe(
-      'Please enter a valid email address',
-    );
-    expect(questions[3].validate('hello')).toBe(
-      'Please enter a valid email address',
-    );
+    expect(questions[3].validate('testUser@bbc.co.uk')).toBe(true);
   });
 });
