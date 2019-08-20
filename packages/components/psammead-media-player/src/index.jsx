87488c16ff3d4af1e29c977777ba92b6e26bc3bd
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-08-20 12:07:11 +0100

    Begin porting media player over

--- packages/components/psammead-media-player/src/index.jsx
+++ packages/components/psammead-media-player/src/index.jsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { string, bool, oneOf } from 'prop-types';
+import Placeholder from './Placeholder';
+import Canonical from './Canonical';
 
-const MediaPlayer = () => <React.Fragment>Media Player</React.Fragment>;
+const landscapeRatio = '56.25%'; // (9/16)*100 = 16:9
+const portraitRatio = '177.78%'; // (16/9)*100 = 9:16
+const StyledContainer = styled.div`
+  padding-top: ${({ orientation }) =>
+    orientation === 'Portrait' ? portraitRatio : landscapeRatio};
+  position: relative;
+  overflow: hidden;
+`;
+
+const MediaPlayer = ({
+  showPlaceholder,
+  placeholderSrc,
+  orientation,
+  isAmp,
+  src,
+}) => {
+  const shouldShowPlaceholder = !isAmp && showPlaceholder;
+  const [placeholderActive, setPlaceholderActive] = useState(
+    shouldShowPlaceholder,
+  );
+  const handlePlaceholderClick = () => setPlaceholderActive(false);
+
+  return (
+    <StyledContainer orientation={orientation}>
+      {placeholderActive ? (
+        <Placeholder onClick={handlePlaceholderClick} src={placeholderSrc} />
+      ) : (
+        <Canonical src={src} />
+      )}
+    </StyledContainer>
+  );
+};
+
+MediaPlayer.propTypes = {
+  showPlaceholder: bool,
+  placeholderSrc: string,
+  src: string.isRequired,
+  orientation: oneOf(['Portrait', 'Landscape']),
+  isAmp: bool,
+};
+
+MediaPlayer.defaultProps = {
+  showPlaceholder: true,
+  placeholderSrc: null,
+  orientation: 'Landscape',
+  isAmp: false,
+};
 
 export default MediaPlayer;
---
+++ packages/components/psammead-media-player/src/index.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MediaPlayer from '.';
+
+storiesOf('Components|Media Player', module).add('default', () => (
+  <MediaPlayer
+    src="https://www.test.bbc.co.uk/ws/av-embeds/c3wmq4d1y3wo/p01k6msp"
+    placeholderSrc="https://ichef.bbci.co.uk/news/640/cpsdevpb/4eb7/test/ba7482d0-cca8-11e8-b0bf-f33155223fc4.jpg"
+  />
+));
