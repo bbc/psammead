65c1067a15457feb810f763a91f82ffbd313267f
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-08-20 15:36:21 +0100

    Remove helmet

--- packages/components/psammead-media-player/src/Amp/index.jsx
+++ packages/components/psammead-media-player/src/Amp/index.jsx
@@ -1,33 +1,17 @@
-import React, { Fragment } from 'react';
-import Helmet from 'react-helmet';
+import React from 'react';
 import { string } from 'prop-types';
-
-const AmpHead = () => {
-  return (
-    <Helmet>
-      <script
-        async
-        custom-element="amp-iframe"
-        src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"
-      />
-    </Helmet>
-  );
-};
 
 const AmpMediaPlayer = ({ src, placeholderSrc }) => {
   return (
-    <Fragment>
-      <AmpHead />
-      <amp-iframe
-        sandbox="allow-scripts allow-same-origin"
-        layout="fill"
-        frameborder="0"
-        src={src}
-        allowfullscreen
-      >
-        <amp-img layout="fill" src={placeholderSrc} placeholder />
-      </amp-iframe>
-    </Fragment>
+    <amp-iframe
+      sandbox="allow-scripts allow-same-origin"
+      layout="fill"
+      frameborder="0"
+      src={src}
+      allowfullscreen
+    >
+      <amp-img layout="fill" src={placeholderSrc} placeholder />
+    </amp-iframe>
   );
 };
 
