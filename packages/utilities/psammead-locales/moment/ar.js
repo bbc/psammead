f58450471aae70d85956e0dae681a5d118a0509c
Rachael Njeri <rachael.njeri@andela.com>
2019-08-20 13:05:10 +0300

    Remove fields that do not need to be overridden
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- packages/utilities/psammead-locales/moment/ar.js
+++ packages/utilities/psammead-locales/moment/ar.js
@@ -1,19 +1,6 @@
 const moment = require('moment');
 require('moment/locale/ar');
 
-const symbolMap = {
-  '1': '1',
-  '2': '2',
-  '3': '3',
-  '4': '4',
-  '5': '5',
-  '6': '6',
-  '7': '7',
-  '8': '8',
-  '9': '9',
-  '0': '0',
-};
-
 const months = [
   'يناير/ كانون الثاني',
   'فبراير/ شباط',
@@ -29,55 +16,23 @@ const months = [
   'ديسمبر/ كانون الأول',
 ];
 
-export default moment.updateLocale(
-  ('ar',
-  {
-    months,
-    monthsShort: months,
-    weekdays: 'الأحد_الإثنين_الثلاثاء_الأربعاء_الخميس_الجمعة_السبت'.split('_'),
-    weekdaysShort: 'أحد_إثنين_ثلاثاء_أربعاء_خميس_جمعة_سبت'.split('_'),
-    weekdaysMin: 'ح_ن_ث_ر_خ_ج_س'.split('_'),
-    weekdaysParseExact: true,
-    longDateFormat: {
-      LT: 'HH:mm',
-      LTS: 'HH:mm:ss',
-      L: 'D/\u200FM/\u200FYYYY',
-      LL: 'D MMMM YYYY',
-      LLL: 'D MMMM YYYY HH:mm',
-      LLLL: 'dddd D MMMM YYYY HH:mm',
-    },
-    meridiemParse: /ص|م/,
-    isPM(input) {
-      return input === 'م';
-    },
-    // eslint-disable-next-line no-unused-vars
-    meridiem(hour, _minute, _isLower) {
-      if (hour < 12) {
-        return 'ص';
-      }
-      return 'م';
-    },
-    calendar: {
-      sameDay: '[اليوم عند الساعة] LT',
-      nextDay: '[غدًا عند الساعة] LT',
-      nextWeek: 'dddd [عند الساعة] LT',
-      lastDay: '[أمس عند الساعة] LT',
-      lastWeek: 'dddd [عند الساعة] LT',
-      sameElse: 'L',
-    },
-    preparse(string) {
-      return string.replace(/،/g, ',');
-    },
-    postformat(string) {
-      return string
-        .replace(/\d/g, match => {
-          return symbolMap[match];
-        })
-        .replace(/,/g, '،');
-    },
-    week: {
-      dow: 6, // Saturday is the first day of the week.
-      doy: 12, // The week that contains Jan 12th is the first week of the year.
-    },
-  }),
-);
+moment.updateLocale('ar', {
+  // these keys have been overridden to maintain western numerals
+  // some other keys have been left as set upstream
+  // see https://github.com/moment/moment/blob/develop/src/locale/ar.js
+
+  symbolMap: {
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    '0': '0',
+  },
+  months,
+  monthsShort: months,
+});
