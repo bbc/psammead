d50d62f393634d2e8335d0f2187069b2e137cef4
Fahad <34196381+FK78@users.noreply.github.com>
2019-08-21 17:36:01 +0100

    Update scripts/talos/createPullRequest/getPackageNames/index.test.js
    Co-Authored-By: Drew McMillan <drew.mcmillan@bbc.co.uk>

--- scripts/talos/createPullRequest/getPackageNames/index.test.js
+++ scripts/talos/createPullRequest/getPackageNames/index.test.js
@@ -1,7 +1,7 @@
 import getPackageNames from '.';
 
 describe('getPackageNames', () => {
-  it('should shorten the title', () => {
+  it('should shorten the title when more than 3 packages are provided', () => {
     expect(
       getPackageNames(['package1', 'package2', 'package3', 'package4']),
     ).toEqual('package1, package2, package3...');
