0cc274a6de80af92fa83eb0733b34be79983870c
Fahad Khan <fk17abb@protonmail.com>
2019-08-22 11:59:43 +0100

    move test helper functions

--- packages/utilities/psammead-test-helpers/src/index.js
+++ packages/utilities/psammead-test-helpers/src/index.js
@@ -1,6 +1,7 @@
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import 'jest-styled-components';
+import deepClone from 'ramda/src/clone';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
 export const shouldMatchSnapshot = (title, component) => {
@@ -31,6 +32,55 @@ export const isNull = (title, component) => {
     expect(tree).toBeNull();
   });
 };
+
+export const setWindowValue = (key, value) => {
+  const windowValue = window[key];
+  delete window[key];
+
+  let newValue = value;
+
+  if (value && typeof value === 'object') {
+    newValue = {
+      ...deepClone(windowValue),
+      ...value,
+    };
+  }
+
+  Object.defineProperty(window, key, {
+    value: newValue,
+    writable: true,
+  });
+};
+
+export const resetWindowValue = (key, value) => {
+  Object.defineProperty(window, key, {
+    value,
+    writable: true,
+  });
+};
+
+export const suppressPropWarnings = warnings => {
+  const { expectedWarnings } = window;
+  if (expectedWarnings && Array.isArray(expectedWarnings)) {
+    window.expectedWarnings = [...expectedWarnings, warnings];
+  } else {
+    window.expectedWarnings = [warnings];
+  }
+};
+
+// const mocks = {
+//   error: jest.fn(),
+//   warn: jest.fn(),
+//   info: jest.fn(),
+//   verbose: jest.fn(),
+//   debug: jest.fn(),
+//   silly: jest.fn(),
+// };
+
+// jest.mock('../app/lib/logger.node', () => jest.fn());
+// nodeLogger.mockImplementation(() => mocks);
+
+// export const loggerMock = mocks;
 
 const errorIfMissingKey = (keys, object, message) => {
   keys.forEach(key => {
