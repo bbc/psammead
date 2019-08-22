f4b4dbcfb43ad99b31f3d375f399d343ee82be84
Gerald Pharin <gerald.pharin@andela.com>
2019-08-22 11:25:43 +0000

    Combine all `getPackages` tests

--- scripts/utilities/getPackages/index.test.js
+++ scripts/utilities/getPackages/index.test.js
@@ -1,16 +1,17 @@
-const getPackages = require('.');
+/* eslint-disable global-require */
+describe(`Publish Script - getPackages`, () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
 
-jest.mock('shelljs', () => ({
-  exec: () => ({
-    stdout: `
-/psammead/packages/components/psammead-brand
-/psammead/packages/components/psammead-image
-`,
-  }),
-}));
-
-describe('getPackages', () => {
   it("should return array of all package's paths", () => {
+    jest.mock('shelljs', () => ({
+      exec: () => ({
+        stdout: `/psammead/packages/components/psammead-brand\n/psammead/packages/components/psammead-image`,
+      }),
+    }));
+
+    const getPackages = require('.');
     const actual = getPackages();
     const expected = [
       '/psammead',
@@ -18,5 +19,46 @@ describe('getPackages', () => {
       '/psammead/packages/components/psammead-image',
     ];
     expect(actual).toEqual(expected);
+  });
+
+  it('returns array of packages with the root package and two other packages found', () => {
+    jest.mock('shelljs', () => ({
+      exec: () => ({
+        stdout:
+          '/foobar/directory/packages/package1\n/foobar/directory/packages/package2',
+      }),
+    }));
+
+    const getPackages = require('.');
+    expect(getPackages()).toEqual([
+      '/foobar/directory',
+      '/foobar/directory/packages/package1',
+      '/foobar/directory/packages/package2',
+    ]);
+  });
+
+  it('returns array of packages with the root package and one other package found', () => {
+    jest.mock('shelljs', () => ({
+      exec: () => ({
+        stdout: '/foobar/directory/packages/package1',
+      }),
+    }));
+
+    const getPackages = require('.');
+    expect(getPackages()).toEqual([
+      '/foobar/directory',
+      '/foobar/directory/packages/package1',
+    ]);
+  });
+
+  it('returns empty array when no packages are found', () => {
+    jest.mock('shelljs', () => ({
+      exec: () => ({
+        stdout: '\n', // This is the real response if none are found
+      }),
+    }));
+
+    const getPackages = require('.');
+    expect(getPackages()).toEqual([]);
   });
 });
