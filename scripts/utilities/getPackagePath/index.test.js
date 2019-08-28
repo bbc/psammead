c7a54fc8faed34c8e451967410dd32f736054ba8
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-08-28 12:15:13 +0100

    :rocket: Fix base level psammead issue in talos

--- scripts/utilities/getPackagePath/index.test.js
+++ scripts/utilities/getPackagePath/index.test.js
@@ -1,20 +1,50 @@
 const getPackagePath = require('.');
+const getPackages = require('../getPackages');
 
-jest.mock('../getPackages', () => () => [
-  '/psammead/packages/components/psammead-brand',
-  '/psammead/packages/components/psammead-image',
-]);
+jest.mock('../getPackages');
 
 describe('getPackagePath', () => {
-  it('should return the path of the package', () => {
-    const packagePath1 = getPackagePath('psammead-brand');
-    const packagePath2 = getPackagePath('psammead-image');
+  describe('when locally', () => {
+    beforeEach(() => {
+      getPackages.mockImplementation(() => [
+        '/psammead',
+        '/psammead/packages/components/psammead-brand',
+        '/psammead/packages/components/psammead-image',
+      ]);
+    });
 
-    expect(packagePath1).toEqual(
-      '/psammead/packages/components/psammead-brand',
-    );
-    expect(packagePath2).toEqual(
-      '/psammead/packages/components/psammead-image',
-    );
+    it('should return the path of the package', () => {
+      expect(getPackagePath('psammead')).toEqual(
+        '/psammead',
+      );
+      expect(getPackagePath('psammead-brand')).toEqual(
+        '/psammead/packages/components/psammead-brand',
+      );
+      expect(getPackagePath('psammead-image')).toEqual(
+        '/psammead/packages/components/psammead-image',
+      );
+    });
+  });
+
+  describe('when on jenkins', () => {
+    beforeEach(() => {
+      getPackages.mockImplementation(() => [
+        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA',
+        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-brand',
+        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-image',
+      ]);
+    });
+
+    it('should return the path of the package', () => {
+      expect(getPackagePath('psammead')).toEqual(
+        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA',
+      );
+      expect(getPackagePath('psammead-brand')).toEqual(
+        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-brand',
+      );
+      expect(getPackagePath('psammead-image')).toEqual(
+        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-image',
+      );
+    });
   });
 });
