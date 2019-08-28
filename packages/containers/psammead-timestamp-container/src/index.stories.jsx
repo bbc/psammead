079c4f7bf297cdbb516015b8c6eaea6dc548c237
Gabriel Lorin <dev@lor.in>
2019-08-28 19:12:43 +0200

    Changes to Timestamp Container stories, Moment locales stories

--- packages/containers/psammead-timestamp-container/src/index.stories.jsx
+++ packages/containers/psammead-timestamp-container/src/index.stories.jsx
@@ -3,8 +3,8 @@ import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-e
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import {
   boolean,
-  button,
   number,
+  select,
   text,
   withKnobs,
 } from '@storybook/addon-knobs';
@@ -15,110 +15,102 @@ import '@bbc/psammead-locales/moment/ig';
 import '@bbc/psammead-locales/moment/pcm';
 import '@bbc/psammead-locales/moment/yo';
 
-let timestamp = moment();
+// Fixed timestamp for 27 August 2019, 14:54 BST
+const fixedTimestamp = 1566914061212;
 
-const date = name => {
-  button('now', () => {
-    timestamp = moment();
-  });
-  button('5 seconds ago', () => {
-    timestamp = moment().subtract(5, 'seconds');
-  });
-  button('30 seconds ago', () => {
-    timestamp = moment().subtract(30, 'seconds');
-  });
-  button('59 seconds ago', () => {
-    timestamp = moment().subtract(59, 'seconds');
-  });
-  button('1 minute ago', () => {
-    timestamp = moment().subtract(1, 'minute');
-  });
-  button('24 minutes ago', () => {
-    timestamp = moment().subtract(24, 'minutes');
-  });
-  button('59 minutes ago', () => {
-    timestamp = moment().subtract(59, 'minutes');
-  });
-  button('63 minutes ago', () => {
-    timestamp = moment().subtract(63, 'minutes');
-  });
-  button('95 minutes ago', () => {
-    timestamp = moment().subtract(95, 'minutes');
-  });
-  button('2 hours ago', () => {
-    timestamp = moment().subtract(2, 'hours');
-  });
-  button('23 hours ago', () => {
-    timestamp = moment().subtract(23, 'hours');
-  });
-  button('1 day ago', () => {
-    timestamp = moment().subtract(1, 'day');
-  });
-  button('30 hours ago', () => {
-    timestamp = moment().subtract(30, 'hours');
-  });
-  button('20 days ago', () => {
-    timestamp = moment().subtract(20, 'days');
-  });
-  button('21 days ago', () => {
-    timestamp = moment().subtract(21, 'days');
-  });
-  button('8.9999... months ago', () => {
-    timestamp = moment()
+const val = momentTimestamp => momentTimestamp.valueOf();
+
+const timestamps = {
+  'Fixed (27 Aug 2019)': fixedTimestamp,
+  Now: Date.now(),
+  '5 seconds ago': val(moment().subtract(5, 'seconds')),
+  '30 seconds ago': val(moment().subtract(30, 'seconds')),
+  '59 seconds ago': val(moment().subtract(59, 'seconds')),
+  '1 minute ago': val(moment().subtract(1, 'minute')),
+  '24 minutes ago': val(moment().subtract(24, 'minute')),
+  '59 minutes ago': val(moment().subtract(59, 'minute')),
+  '63 minutes ago': val(moment().subtract(63, 'minute')),
+  '95 minutes ago': val(moment().subtract(95, 'minute')),
+  '2 hours ago': val(moment().subtract(2, 'hours')),
+  '23 hours ago': val(moment().subtract(23, 'hours')),
+  '1 day ago': val(moment().subtract(1, 'day')),
+  '30 hours ago': val(moment().subtract(30, 'hours')),
+  '20 days ago': val(moment().subtract(20, 'day')),
+  '21 days ago': val(moment().subtract(21, 'day')),
+  '8.9999... months ago': val(
+    moment()
       .subtract(9, 'months')
-      .add(2, 'second');
-  });
-  button('9 months ago', () => {
-    timestamp = moment().subtract(9, 'months');
-  });
-
-  return number(name, timestamp.valueOf());
+      .add(2, 'second'),
+  ),
+  '9 months ago': val(moment().subtract(9, 'months')),
+  'Custom timestamp': 0,
 };
 
 storiesOf('Containers|TimestampContainer', module)
   .addDecorator(withKnobs)
   .add(
     'default',
-    inputProvider([], ({ locale, script, service }) => (
-      <Timestamp
-        timestamp={date('timestamp')}
-        dateTimeFormat="YYYY-MM-DD"
-        format="D MMMM YYYY"
-        isRelative={boolean('isRelative', false)}
-        script={script}
-        locale={locale}
-        service={service}
-      />
-    )),
+    inputProvider([], ({ locale, script, service }) => {
+      const storyTimestamp = select('Timestamp', timestamps, fixedTimestamp);
+      return (
+        <Timestamp
+          timestamp={
+            storyTimestamp === 0 // matches value for 'Custom timestamp'
+              ? number('Timestamp value', Date.now()) // default for custom timestamp is now
+              : storyTimestamp
+          }
+          dateTimeFormat="YYYY-MM-DD"
+          format={text('Format', 'D MMMM YYYY, HH:mm z')}
+          isRelative={boolean('isRelative', false)}
+          script={script}
+          locale={locale}
+          service={service}
+        />
+      );
+    }),
   )
   .add(
     'with prefix',
-    inputProvider([], ({ locale, script, service }) => (
-      <Timestamp
-        timestamp={date('timestamp')}
-        dateTimeFormat="YYYY-MM-DD"
-        format="D MMMM YYYY, HH:mm z"
-        isRelative={boolean('isRelative', true)}
-        prefix={text('Prefix text', 'Updated')}
-        script={script}
-        locale={locale}
-        service={service}
-      />
-    )),
+    inputProvider([], ({ locale, script, service }) => {
+      const storyTimestamp = select('Timestamp', timestamps, fixedTimestamp);
+      return (
+        <Timestamp
+          timestamp={
+            storyTimestamp === 0 // matches value for 'Custom timestamp'
+              ? number('Timestamp value', Date.now()) // default for custom timestamp is now
+              : storyTimestamp
+          }
+          dateTimeFormat="YYYY-MM-DD"
+          format={text('Format', 'D MMMM YYYY, HH:mm z')}
+          isRelative={boolean('isRelative', false)}
+          prefix={text('Prefix text', 'Updated')}
+          script={script}
+          locale={locale}
+          service={service}
+        />
+      );
+    }),
   )
   .add(
     'with prefix and suffix',
-    inputProvider([], ({ locale, script, service }) => (
-      <Timestamp
-        timestamp={date('timestamp')}
-        dateTimeFormat="YYYY-MM-DD"
-        format="D MMMM YYYY, HH:mm z"
-        isRelative={boolean('isRelative', false)}
-        prefix={text('Prefix text', 'This')}
-        suffix={text('Suffix text', 'is date of last update')}
-        script={script}
-        locale={locale}
-        service={service}
-      />
-    )),
+    inputProvider([], ({ locale, script, service }) => {
+      const storyTimestamp = select('Timestamp', timestamps, fixedTimestamp);
+      return (
+        <Timestamp
+          timestamp={
+            storyTimestamp === 0 // matches value for 'Custom timestamp'
+              ? number('Timestamp value', Date.now()) // default for custom timestamp is now
+              : storyTimestamp
+          }
+          dateTimeFormat="YYYY-MM-DD"
+          format={text('Format', 'D MMMM YYYY, HH:mm z')}
+          isRelative={boolean('isRelative', false)}
+          prefix={text('Prefix text', 'This')}
+          suffix={text('Suffix text', 'is date of last update')}
+          script={script}
+          locale={locale}
+          service={service}
+        />
+      );
+    }),
   );
