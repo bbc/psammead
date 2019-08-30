bee7d6c51b93d14f55968790c1f3f015093983ce
thekp <khoa.phan@bbc.co.uk>
2019-08-30 17:21:43 +0100

    update storybook template

--- .yeoman/templates/src/index.stories.jsx
+++ .yeoman/templates/src/index.stories.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withReadme } from 'storybook-readme';
-import Readme from '../README.md';
+import notes from '../README.md';
 import <%= componentName %> from './index';
 
 storiesOf('<%= componentName %>', module)
-  .addDecorator(withReadme(Readme))
-  .add('default', () => <<%= componentName %> />);
+  .add(
+    'default',
+    () => {
+
+      return (
+        <<%= componentName %> />
+      );
+    },
+    { notes },
+  );
