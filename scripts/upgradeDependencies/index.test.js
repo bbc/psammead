4b42ae954443a3d54cc7b7fcb004c3d102276426
Gerald Pharin <gerald.pharin@andela.com>
2019-09-02 15:16:41 +0000

    Update tests to match changelog update

--- scripts/upgradeDependencies/index.test.js
+++ scripts/upgradeDependencies/index.test.js
@@ -93,12 +93,22 @@ describe('parseUpgradedPackages', () => {
 @bbc/psammead-story-promo-list: }`;
 
   it('should parse upgraded packages', () => {
-    expect(parseUpgradedPackages(mockShellJsOutput)).toEqual([
-      '@bbc/psammead-figure',
-      '@bbc/psammead-story-promo',
-      '@bbc/psammead-navigation',
-      '@bbc/psammead-story-promo-list',
-    ]);
+    expect(parseUpgradedPackages(mockShellJsOutput)).toEqual({
+      '@bbc/psammead-figure': {
+        '@bbc/psammead-image': '^1.1.0',
+        '@bbc/psammead-image-placeholder': '^1.1.0',
+      },
+      '@bbc/psammead-story-promo': {
+        '@bbc/psammead-image': '^1.1.0',
+      },
+      '@bbc/psammead-navigation': {
+        '@bbc/psammead-brand': '^4.2.0',
+      },
+      '@bbc/psammead-story-promo-list': {
+        '@bbc/psammead-image': '^1.1.0',
+        '@bbc/psammead-story-promo': '^2.1.0',
+      },
+    });
 
     expect(
       parseUpgradedPackages(`
@@ -111,6 +121,8 @@ describe('parseUpgradedPackages', () => {
 @bbc/psammead-visually-hidden-text: {}
 @bbc/psammead-caption: {}
 @bbc/psammead-media-indicator: {}`),
-    ).toEqual(['@bbc/psammead-navigation']);
+    ).toEqual({
+      '@bbc/psammead-navigation': { '@bbc/psammead-brand': '^4.2.0' },
+    });
   });
 });
