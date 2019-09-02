2914988f531f6099e7e02762a07035333288917e
Gerald Pharin <gerald.pharin@andela.com>
2019-09-02 12:54:09 +0000

    Filter output to include upgraded packages only

--- scripts/upgradeDependencies/parseUpgradedPackages.js
+++ scripts/upgradeDependencies/parseUpgradedPackages.js
@@ -1,24 +1,31 @@
-module.exports = output => {
-  const parsedOutput = output.split('\n').reduce((accumulator, curr) => {
-    const sections = curr.split(': ');
-    if (sections.length) {
-      const key = sections[0];
-      const value = sections.splice(1).join(': ');
-      if (Object.prototype.hasOwnProperty.call(accumulator, key)) {
-        accumulator[key] += value;
-      } else {
-        accumulator[key] = value;
-      }
+const outputReducer = (accumulator, curr) => {
+  const sections = curr.split(': ');
+  if (sections.length) {
+    const key = sections[0];
+    const value = sections.splice(1).join(': ');
+    if (Object.prototype.hasOwnProperty.call(accumulator, key)) {
+      accumulator[key] += value;
+    } else {
+      accumulator[key] = value;
     }
-    return accumulator;
-  }, {});
+  }
+  return accumulator;
+};
 
+module.exports = output => {
+  const parsedOutput = output.split('\n').reduce(outputReducer, {});
   const cleanedOutput = {};
   Object.keys(parsedOutput).forEach(key => {
     if (key) {
-      const cleanedJson = parsedOutput[key].split('{');
-      cleanedJson.splice(0, 1, '{');
-      cleanedOutput[key] = cleanedJson.join('');
+      const cleanedValue = parsedOutput[key].split('{');
+      cleanedValue.splice(0, 1, '{');
+      try {
+        const cleanedJson = JSON.parse(cleanedValue.join(''));
+        if (Object.keys(cleanedJson).length) {
+          cleanedOutput[key] = cleanedJson;
+        }
+        // eslint-disable-next-line no-empty
+      } catch {}
     }
   });
   return cleanedOutput;
