a2a10dbd565727f7e1a54747ff0e3d08a18848d5
Gerald Pharin <gerald.pharin@andela.com>
2019-09-02 13:13:04 +0000

    Add details of bumped packages to changelog

--- scripts/bumpChangelogs/index.js
+++ scripts/bumpChangelogs/index.js
@@ -5,17 +5,25 @@ const readFile = require('../utilities/readFile');
 
 const CHANGELOG = 'CHANGELOG.md';
 
-module.exports = ({ packageNames, prLink, changesDescription }) => {
-  const changelogPaths = packageNames
+module.exports = ({ bumpedPackages, prLink, changesDescription }) => {
+  const packageNames = Object.keys(bumpedPackages);
+  const packageNamesNoBbcPrefix = packageNames.map(dep =>
+    dep.replace('@bbc/', ''),
+  );
+  const changelogPaths = packageNamesNoBbcPrefix
     .map(getPackagePath)
     .map(packagePath => `${packagePath}/${CHANGELOG}`);
+  const descriptions = packageNames.map(
+    dep =>
+      `${changesDescription} - ${Object.keys(bumpedPackages[dep]).join(', ')}`,
+  );
 
-  const updateChangelog = async changelogPath => {
+  const updateChangelog = async (changelogPath, index) => {
     const packageJsonPath = changelogPath.replace(CHANGELOG, 'package.json');
     const { version } = JSON.parse(await readFile(packageJsonPath));
 
     return readFile(changelogPath)
-      .then(getNewChangelogContent(version, prLink, changesDescription))
+      .then(getNewChangelogContent(version, prLink, descriptions[index]))
       .then(writeChangelog(changelogPath));
   };
   const updateChangelogs = changelogPaths.map(updateChangelog);
