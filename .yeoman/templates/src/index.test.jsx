6cc5a0dd6fb5c46a45322aa5f4c52d23ffacdf23
thekp <khoa.phan@bbc.co.uk>
2019-09-02 09:13:07 +0100

    add test that fails, to enforce dev to write new tests

--- .yeoman/templates/src/index.test.jsx
+++ .yeoman/templates/src/index.test.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { render } from '@testing-library/react';
 import <%= componentName %> from './index';
 
 describe('<%= componentName %>', () => {
   shouldMatchSnapshot('should render correctly', <<%= componentName %> />);
+
+  it('should test example template', () => {
+    const { container } = render(<<%= componentName %> />);
+    expect(container.querySelector('h1').textContent).toEqual('Hello World');
+  });
+
 });
