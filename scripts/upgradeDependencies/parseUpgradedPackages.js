8bac781e7c8c32d2b28f52b465ae7366d39fac8e
Gerald Pharin <gerald.pharin@andela.com>
2019-09-02 12:35:48 +0000

    Change parseUpgradedPackages to return object

--- scripts/upgradeDependencies/parseUpgradedPackages.js
+++ scripts/upgradeDependencies/parseUpgradedPackages.js
@@ -1,1 +1,25 @@
-module.exports = output => [...new Set(output.match(/^.+?(?=:\s{$)/gm))];
+module.exports = output => {
+  const parsedOutput = output.split('\n').reduce((accumulator, curr) => {
+    const sections = curr.split(': ');
+    if (sections.length) {
+      const key = sections[0];
+      const value = sections.splice(1).join(': ');
+      if (Object.prototype.hasOwnProperty.call(accumulator, key)) {
+        accumulator[key] += value;
+      } else {
+        accumulator[key] = value;
+      }
+    }
+    return accumulator;
+  }, {});
+
+  const cleanedOutput = {};
+  Object.keys(parsedOutput).forEach(key => {
+    if (key) {
+      const cleanedJson = parsedOutput[key].split('{');
+      cleanedJson.splice(0, 1, '{');
+      cleanedOutput[key] = cleanedJson.join('');
+    }
+  });
+  return cleanedOutput;
+};
