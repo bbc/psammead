1295232b53a44cca779846f9fae0940543f5228d
James Donohue <james.donohue@bbc.co.uk>
2019-09-05 17:23:39 +0100

    Prevent trailing commas in function args for IE11

---
+++ packages/utilities/psammead-locales/moment/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    // IE11 gives syntax error for trailing commas on functions and Moment
+    // locales are not transpiled
+    'comma-dangle': ['error', {
+      'functions': 'never',
+      'imports': 'only-multiline',
+      'objects': 'only-multiline',
+      'arrays': 'only-multiline'
+    }]
+  },
+};
