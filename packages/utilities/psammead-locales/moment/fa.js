3abacbdb3a792530b7a8c1c998bb40512f69959f
James Donohue <james.donohue@bbc.co.uk>
2019-09-06 13:25:58 +0100

    Avoid changing global moment locale

--- packages/utilities/psammead-locales/moment/fa.js
+++ packages/utilities/psammead-locales/moment/fa.js
@@ -22,7 +22,6 @@ const persianJalaliMonths = [
 // Moment formats that should have the Jalali date added
 const jalaliFormats = ['D MMMM YYYY', 'LL'];
 
-moment().locale('fa');
 moment.updateLocale('fa', {
   // eslint-disable-next-line object-shorthand
   postformat: function(string) {
