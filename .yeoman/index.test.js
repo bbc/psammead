b0ce010c2b479d00ff52a8761290d55e55ad752f
thekp <khoa.phan@bbc.co.uk>
2019-09-10 11:27:58 +0100

    test push

--- .yeoman/index.test.js
+++ .yeoman/index.test.js
@@ -36,7 +36,7 @@ describe('pacakge generator', () => {
         tempFiles.map(file => {
           expectedFiles.push(path.join(__dirname, file));
         });
-        shell.exec(`ls -R ${__dirname}`);
+        console.log('testing push');
 
         assert.file(expectedFiles);
         assert.fileContent(
