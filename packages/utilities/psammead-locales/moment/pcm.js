2df85c80ca3641860041a6846b93bb2e2bb31a52
Rachael Njeri <rachael.njeri@andela.com>
2019-09-10 11:44:20 +0300

    Disable prettier
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- packages/utilities/psammead-locales/moment/pcm.js
+++ packages/utilities/psammead-locales/moment/pcm.js
@@ -1,8 +1,10 @@
 const moment = require('moment');
 
 moment.defineLocale('pcm', {
+  // eslint-disable-next-line prettier/prettier
   months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
   monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
+  // eslint-disable-next-line prettier/prettier
   weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
   weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
   weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
