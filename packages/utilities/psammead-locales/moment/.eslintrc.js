77cb9f28e8074d21a050428c6d5af9ff34cd1a52
Rachael Njeri <rachael.njeri@andela.com>
2019-09-11 15:26:46 +0300

    Update .eslintrc rules
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- packages/utilities/psammead-locales/moment/.eslintrc.js
+++ packages/utilities/psammead-locales/moment/.eslintrc.js
@@ -5,19 +5,11 @@ module.exports = {
   ],
   plugins: ['es5'],
   rules: {
-    // IE11 gives syntax error for trailing commas on functions and Moment
-    'comma-dangle': ['error', {
-      'functions': 'never',
-      'imports': 'only-multiline',
-      'objects': 'only-multiline',
-      'arrays': 'only-multiline',
-    }],
-    'func-names': 0,
     'object-shorthand': 0,
     'vars-on-top': 0,
     'no-var': 0,
     'one-var':0,
-    'no-block-scoping': 2,
+    'no-block-scoping': 0,
     'no-template-literals': 0,
     'no-nested-ternary':0,
   },
