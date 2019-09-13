c435fcbee9a6f53efd8cc1e91b215d3f4ebe7936
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-09-13 09:19:30 +0100

    Add Ukrainian test

--- packages/utilities/psammead-locales/moment/uk.js
+++ packages/utilities/psammead-locales/moment/uk.js
@@ -3,8 +3,8 @@ require('moment/locale/uk');
 
 moment.updateLocale('uk', {
   longDateFormat: {
-    LL: 'D MMMM YYYY р',
-    LLL: 'D MMMM YYYY р, HH:mm',
-    LLLL: 'dddd, D MMMM YYYY р, HH:mm',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY, HH:mm',
+    LLLL: 'dddd, D MMMM YYYY, HH:mm',
   },
 });
