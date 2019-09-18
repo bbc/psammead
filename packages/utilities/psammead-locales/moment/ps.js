fd9e1bf85f5b20694be493f9ba07ed10025313b6
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-18 16:55:12 +0100

    Set first DoW and WoY for Pashto locale

--- packages/utilities/psammead-locales/moment/ps.js
+++ packages/utilities/psammead-locales/moment/ps.js
@@ -92,4 +92,9 @@ moment.defineLocale('ps', {
 
     return stringHelper.useEasternNumerals(str);
   },
+
+  week: {
+    dow: 6, // First day of week is Saturday
+    doy: 12, // First week of year must contain 1 January (7 + 6 - 1)
+  },
 });
