035eea03a12bdca40efa7fe651899f497d727bdb
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-19 21:07:43 +0100

    Refine relative time support

--- packages/utilities/psammead-locales/moment/ps.js
+++ packages/utilities/psammead-locales/moment/ps.js
@@ -27,22 +27,13 @@ moment.defineLocale('ps', {
   months: pashtoGregorianMonths,
   monthsShort: pashtoGregorianMonths,
 
-  // To do: Verify translation.
   relativeTime: {
-    future: 'په %s', // 'in' %s
     past: '%s مخکې', // %s 'ago'
     s: 'یو څو ثانیې', // 'a few seconds'
-    ss: '%d ثانیې', // %d 'seconds'
     m: 'یوه دقیقه', // 'a minute'
     mm: '%d دقیقې', // %d 'minutes'
     h: 'یو ساعت', // 'an hour'
     hh: '%d ساعتونه', // %d 'hours'
-    d: 'یوه ورځ', // 'a day ago'
-    dd: '%d ورځې', // %d 'days'
-    M: 'یوه میاشت', // 'a month'
-    MM: '%d میاشتې', // %d 'months'
-    y: 'یو کال', // 'a year'
-    yy: '%d کلونه', // %d 'years'
   },
 
   postformat: function(string) {
