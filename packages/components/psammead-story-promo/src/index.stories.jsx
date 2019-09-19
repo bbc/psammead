0ee867baa6a23aa3b9b49ee2c588ba3b3dcff879
Samora Dake <samoradake@gmail.com>
2019-09-19 13:00:01 +0000

    Revert "Simplifies story promo story"
    This reverts commit bbc0bb922ba93363eca48aeeafb7bd934b960abd.

--- packages/components/psammead-story-promo/src/index.stories.jsx
+++ packages/components/psammead-story-promo/src/index.stories.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import Image from '@bbc/psammead-image';
 import Timestamp from '@bbc/psammead-timestamp';
+import MediaIndicator from '@bbc/psammead-media-indicator';
+import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
 import StoryPromo, { Headline, Summary, Link, LiveLabel } from './index';
 import relatedItems from '../testHelpers/relatedItems';
 import IndexAlsosContainer from '../testHelpers/IndexAlsosContainer';
@@ -20,6 +22,17 @@ const buildImg = () => (
   />
 );
 
+const MediaIndicatorComponent = (type, service) => {
+  return (
+    <MediaIndicator
+      duration={type !== 'photogallery' && '2:15'}
+      datetime="PT2M15S"
+      service={service}
+      type={type}
+    />
+  );
+};
+
 /* eslint-disable-next-line react/prop-types */
 const LiveComponent = ({ headline, service, dir }) => (
   /* eslint-disable-next-line jsx-a11y/aria-role */
@@ -27,7 +40,18 @@ const LiveComponent = ({ headline, service, dir }) => (
     <LiveLabel service={service} dir={dir}>
       LIVE
     </LiveLabel>
+    <VisuallyHiddenText lang="en-GB">Live, </VisuallyHiddenText>
     {headline}
+  </span>
+);
+
+/* eslint-disable-next-line react/prop-types */
+const HiddenText = ({ type, headline }) => (
+  /* eslint-disable-next-line jsx-a11y/aria-role */
+  <span role="text">
+    <VisuallyHiddenText>{type}, </VisuallyHiddenText>
+    <span>{headline}</span>
+    <VisuallyHiddenText>, 2,15</VisuallyHiddenText>
   </span>
 );
 
@@ -38,13 +62,19 @@ const InfoComponent = ({
   script,
   topStory,
   service,
+  isLive,
   dir,
+  type,
   alsoItems,
 }) => (
   <>
     <Headline script={script} topStory={topStory} service={service}>
       <Link href="https://www.bbc.co.uk/news">
-        <LiveComponent service={service} dir={dir} headline={headlineText} />
+        {isLive ? (
+          <LiveComponent service={service} dir={dir} headline={headlineText} />
+        ) : (
+          <HiddenText headline={headlineText} type={type} />
+        )}
       </Link>
     </Headline>
     <Summary script={script} topStory={topStory} service={service}>
@@ -79,6 +109,12 @@ const generateStory = ({ topStory, alsoItems = null }) =>
       service,
       dir,
     }) => {
+      const mediaType = select(
+        'Media Type',
+        ['No media', 'video', 'audio', 'photogallery'],
+        'No media',
+      );
+
       const Info = (
         <InfoComponent
           headlineText={headlineText}
@@ -86,14 +122,26 @@ const generateStory = ({ topStory, alsoItems = null }) =>
           script={script}
           topStory={topStory}
           service={service}
+          isLive={boolean('isLive', false)}
           dir={dir}
+          type={mediaType}
           alsoItems={alsoItems}
         />
       );
 
       const Img = buildImg();
 
-      return <StoryPromo image={Img} info={Info} topStory={topStory} />;
+      return (
+        <StoryPromo
+          image={Img}
+          info={Info}
+          mediaIndicator={
+            mediaType !== 'No media' &&
+            MediaIndicatorComponent(mediaType, service)
+          }
+          topStory={topStory}
+        />
+      );
     },
   });
 
