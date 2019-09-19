5db801fa9ae942bd13fc8e3f2b7ce12e2b5dc505
Gerald Pharin <gerald.pharin@andela.com>
2019-09-19 13:23:26 +0000

    Bump package version for psammead

--- scripts/utilities/getPackageNames/index.test.js
+++ scripts/utilities/getPackageNames/index.test.js
@@ -1,18 +1,21 @@
 const getPackageNames = require('.');
 
-jest.mock('shelljs', () => ({
-  exec: () => ({
-    stdout: `
-  @bbc/psammead-brand
-  @bbc/psammead-image
-`,
-  }),
-}));
+jest.mock('../getPackages', () => [
+  'psammead',
+  '@bbc/gel-foundations',
+  '@bbc/psammead-brand',
+  '@bbc/psammead-image',
+]);
 
 describe('getPackageNames', () => {
   it('should return package names without the "@bbc" scope', () => {
     const actual = getPackageNames();
-    const expected = ['psammead-brand', 'psammead-image'];
+    const expected = [
+      'psammead',
+      'gel-foundations',
+      'psammead-brand',
+      'psammead-image',
+    ];
     expect(actual).toEqual(expected);
   });
 });
