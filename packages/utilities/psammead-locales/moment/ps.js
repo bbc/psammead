64cffec40a456abb9c56652307f6c98768b01966
Simon Sinclair <simonsinclair@users.noreply.github.com>
2019-09-19 20:48:36 +0100

    Revert last two commits
    For this PR, focus on adding relative time support only.

--- packages/utilities/psammead-locales/moment/ps.js
+++ packages/utilities/psammead-locales/moment/ps.js
@@ -25,13 +25,7 @@ var jalaliFormats = ['D MMMM YYYY', 'LL'];
 
 moment.defineLocale('ps', {
   months: pashtoGregorianMonths,
-  // To do: `monthsShort` is using full months - should they be short?
   monthsShort: pashtoGregorianMonths,
-
-  weekdays: 'اتور_ګول_نهه_شرور_زیارت_جمعه_خالی'.split('_'),
-
-  // To do: Pashto short weekday translation.
-  weekdaysShort: 'اتور_ګول_نهه_شرور_زیارت_جمعه_خالی'.split('_'),
 
   // To do: Verify translation.
   relativeTime: {
@@ -51,37 +45,6 @@ moment.defineLocale('ps', {
     yy: '%d کلونه', // %d 'years'
   },
 
-  // To do: Verify translation.
-  meridiem: function(hours) {
-    return hours < 12 ? 'د غرمې دمخه' : 'ماسپښین'; // AM : PM
-  },
-
-  isPM: function(input) {
-    return /ماسپښین/.test(input);
-  },
-
-  meridiemParse: /د غرمې دمخه|ماسپښین/,
-
-  // To do: Pashto translation.
-  // ordinal : function (number, token) {
-  //   var b = number % 10;
-  //   var output = (~~ (number % 100 / 10) === 1) ? 'th' :
-  //       (b === 1) ? 'st' :
-  //       (b === 2) ? 'nd' :
-  //       (b === 3) ? 'rd' : 'th';
-  //   return number + output;
-  // },
-
-  // To do: Verify translation.
-  calendar: {
-    lastDay: '[پرون په] LT', // [Yesterday at]
-    sameDay: '[نن ورځ] LT', // [Today at]
-    nextDay: '[سبا ته] LT', // [Tomorrow at]
-    lastWeek: '[تیره] dddd [په] LT', // [Last] ... [at]
-    nextWeek: 'dddd [په] LT', // [at]
-    sameElse: 'L',
-  },
-
   postformat: function(string) {
     var str = jalaaliHelper.addJalaliDate(
       'ps',
@@ -91,10 +54,5 @@ moment.defineLocale('ps', {
     );
 
     return stringHelper.useEasternNumerals(str);
-  },
-
-  week: {
-    dow: 6, // First day of week is Saturday
-    doy: 12, // First week of year must contain 1 January (7 + 6 - 1)
   },
 });
