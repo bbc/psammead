e07f9791d0d3244067c57f5604e93f774b48fea2
Ruth Ogendi <ruth.ogendi@andela.com>
2019-09-23 21:37:07 +0300

    Update hindi tests. Remove changes to pa-in

--- packages/utilities/psammead-locales/moment/pa-in.js
+++ packages/utilities/psammead-locales/moment/pa-in.js
@@ -2,7 +2,6 @@ var moment = require('moment');
 require('moment/locale/pa-in');
 
 moment.updateLocale('pa-in', {
-  months: ''.split('_'),
   postformat: null,
   relativeTime: {
     past: '%s ਪਹਿਲਾਂ',
