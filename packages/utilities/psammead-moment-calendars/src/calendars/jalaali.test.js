31f5f779936ad99a152f3b50d0d4722ffa985343
Harvey Peachey <harvey.peachey@bbc.co.uk>
2019-09-24 14:46:45 +0100

    Updated snapshot added dummy test

--- packages/utilities/psammead-moment-calendars/src/calendars/jalaali.test.js
+++ packages/utilities/psammead-moment-calendars/src/calendars/jalaali.test.js
@@ -1,2 +1,7 @@
 // import moment from 'moment';
 // const jalaaliHelper = require('./calendars/jalaali');
+import { isNull } from '@bbc/psammead-test-helpers';
+
+describe('test', () => {
+  isNull('should return null', null);
+});
