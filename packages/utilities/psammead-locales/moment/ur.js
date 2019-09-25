51ba96eb6afd1b4cac46d86bce86d2ed0e24a99c
Ruth Ogendi <ruth.ogendi@andela.com>
2019-09-25 11:43:43 +0300

    Use Urdu WS moment.

--- packages/utilities/psammead-locales/moment/ur.js
+++ packages/utilities/psammead-locales/moment/ur.js
@@ -2,7 +2,7 @@ var moment = require('moment');
 require('moment/locale/ur');
 
 moment.updateLocale('ur', {
-  months: 'جنوری_فروری_مارچ_اپریل_مئی_جون_جولائی_اگست_ستمبر_اکتوبر_نومبر_دسمبر'.split(
+  months: 'جنوری_فروری_مار چ_اپريل_مئ_جون_جولائی_اگست_ستمبر_اکتوبر_نومبر_دسمبر'.split(
     '_'
   ),
 });
