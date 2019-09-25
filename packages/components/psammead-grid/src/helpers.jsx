eb9327b6f8ffdf976f1ec0949a6d0cf8607f2dfb
Sareh Heidari <sareh@users.noreply.github.com>
2019-09-25 16:09:19 +0100

    Adding spacing below the ExampleImage
    Co-authored-by: Einstein Njoroge <forensicslabnet@gmail.com>

--- packages/components/psammead-grid/src/helpers.jsx
+++ packages/components/psammead-grid/src/helpers.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Paragraph from '@bbc/psammead-paragraph';
 import { cyrillicAndLatin } from '@bbc/gel-foundations/scripts';
 import Image from '@bbc/psammead-image';
+import { GEL_SPACING } from '@bbc/gel-foundations/dist/spacings';
 
 export const ExampleParagraph = ({ identifier }) => (
   <Paragraph script={cyrillicAndLatin} service="news">
@@ -22,12 +23,19 @@ export const ExampleFigure = styled.figure`
   padding: 0;
 `;
 
+const ImageSpacing = styled.div`
+  margin: 0;
+  padding: 0 0 ${GEL_SPACING} 0;
+`;
+
 export const ExampleImage = () => (
-  <Image
-    alt="Robert Downey Junior in Iron Man"
-    src="https://ichef.bbci.co.uk/news/660/cpsprodpb/11897/production/_106613817_999_al_.jpg"
-    width="640"
-  />
+  <ImageSpacing>
+    <Image
+      alt="Robert Downey Junior in Iron Man"
+      src="https://ichef.bbci.co.uk/news/660/cpsprodpb/11897/production/_106613817_999_al_.jpg"
+      width="640"
+    />
+  </ImageSpacing>
 );
 
 ExampleParagraph.propTypes = {
