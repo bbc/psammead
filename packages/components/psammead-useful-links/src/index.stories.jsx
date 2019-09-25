066a19d107000d2f9f7e8ea041ae0efb4a18e723
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2019-09-25 11:10:38 +0100

    Update storybook

--- packages/components/psammead-useful-links/src/index.stories.jsx
+++ packages/components/psammead-useful-links/src/index.stories.jsx
@@ -3,18 +3,26 @@ import { storiesOf } from '@storybook/react';
 import notes from '../README.md';
 import UsefulLinks from './index';
 
-storiesOf('UsefulLinks', module).add(
-  'default',
-  () => (
-    <UsefulLinks
-      usefulItems={[
-        'Mitocinmu da sauko da sautin labarai',
-        'Labaran BBC Hausa a text',
-        'Abokan huldar BBC Hausa',
-      ]}
-    />
-  ),
-  {
-    notes,
-  },
-);
+storiesOf('Components|UsefulLinks', module)
+  .add(
+    'one link',
+    () => <UsefulLinks usefulItems={['Labaran BBC Hausa a text']} />,
+    {
+      notes,
+    },
+  )
+  .add(
+    'multiple links',
+    () => (
+      <UsefulLinks
+        usefulItems={[
+          'Mitocinmu da sauko da sautin labarai',
+          'Labaran BBC Hausa a text',
+          'Abokan huldar BBC Hausa',
+        ]}
+      />
+    ),
+    {
+      notes,
+    },
+  );
