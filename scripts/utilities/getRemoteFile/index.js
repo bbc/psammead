e62da0a5f5eaa41de1fca2a221a5f13d548af55b
Gerald Pharin <gerald.pharin@andela.com>
2019-09-25 16:14:26 +0000

    Change argument name from filename to path

--- scripts/utilities/getRemoteFile/index.js
+++ scripts/utilities/getRemoteFile/index.js
@@ -1,12 +1,15 @@
 const GitHub = require('github-api');
 
-const getRemoteFile = async (username, repoName, branch, filename) => {
+const getRemoteFile = async (username, repoName, branch, path) => {
   const gh = new GitHub({
     token: process.env.GITHUB_TOKEN,
   });
   const repo = gh.getRepo(username, repoName);
-  const response = await repo.getContents(branch, filename);
-  return Buffer.from(response.data.content, 'base64').toString('binary');
+  const response = await repo.getContents(branch, path);
+  return {
+    contents: Buffer.from(response.data.content, 'base64').toString('binary'),
+    sha: response.data.sha,
+  };
 };
 
 module.exports = getRemoteFile;
