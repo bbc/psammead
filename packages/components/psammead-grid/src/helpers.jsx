5bedc4875dff3c62afd7498946b77649c7ee87ea
Sareh Heidari <sareh@users.noreply.github.com>
2019-09-25 02:50:39 +0100

    Remove unused examples

--- packages/components/psammead-grid/src/helpers.jsx
+++ packages/components/psammead-grid/src/helpers.jsx
@@ -5,33 +5,6 @@ import Paragraph from '@bbc/psammead-paragraph';
 import { cyrillicAndLatin } from '@bbc/gel-foundations/scripts';
 import Image from '@bbc/psammead-image';
 
-export const FullWidth = styled.div`
-  grid-column: 1 / -1;
-`;
-
-export const Item = styled.div`
-  grid-column: ${props => props.startAtCol} / span ${props => props.span};
-`;
-
-export const ItemWithConfig = styled.div`
-  @media (min-width: ${props => props.breakpointMin}) and (max-width: ${props =>
-      props.breakpointMax}) {
-    grid-column: ${props => props.start} / span ${props => props.span};
-  }
-`;
-
-export const ItemMultiConfig = styled.div`
-  @media (min-width: ${props =>
-      props.layouts[0].breakpointMin}) and (max-width: ${props =>
-      props.layouts[0].breakpointMax}) {
-    grid-column: ${props => props.layouts[0].start} /
-      ${props =>
-        props.layouts[0].end
-          ? props.layouts[0].end
-          : `span ${props.layouts[0].span}`};
-  }
-`;
-
 export const ExampleParagraph = ({ identifier }) => (
   <Paragraph script={cyrillicAndLatin} service="news">
     {identifier}This is a long paragraph that will wrap for several lines. This
