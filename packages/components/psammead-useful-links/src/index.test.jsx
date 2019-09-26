d3dfe9f910559d82359f1ca5f6cb9bee4fb31791
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2019-09-26 11:18:14 +0100

    Update tests

--- packages/components/psammead-useful-links/src/index.test.jsx
+++ packages/components/psammead-useful-links/src/index.test.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { render } from '@testing-library/react';
-import UsefulLinks from './index';
+import { UsefulLink } from './index';
 
-const usefulItems = [
-  'Mitocinmu da sauko da sautin labarai',
-  'Labaran BBC Hausa a text',
-  'Abokan huldar BBC Hausa',
-];
+const singleItem = ['Labaran BBC Hausa a text'];
 
-describe('UsefulLinks', () => {
+describe('One item', () => {
   shouldMatchSnapshot(
     'should render correctly',
-    <UsefulLinks usefulItems={usefulItems} />,
+    <UsefulLink usefulItems={singleItem} strapline={false} />,
   );
-
-  it('should test example template', () => {
-    const { container } = render(<UsefulLinks usefulItems={usefulItems} />);
-    expect(container.querySelector('h2').textContent).toEqual('Useful links');
-  });
 });
