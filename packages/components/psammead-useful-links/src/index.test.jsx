62cc251d8f64e174761bfac1f6975214087e88f0
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2019-09-30 15:26:55 +0100

    Update tests

--- packages/components/psammead-useful-links/src/index.test.jsx
+++ packages/components/psammead-useful-links/src/index.test.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { UsefulLink } from './index';
+import {
+  UsefulLink,
+  UsefulLinksLi,
+  UsefulLinksUl,
+  UsefulLinkItem,
+} from './index';
 
-const singleItem = ['Labaran BBC Hausa a text'];
+const usefulCaptions = [
+  'Mitocinmu da sauko da sautin labarai',
+  'Labaran BBC Hausa a text',
+  'Abokan huldar BBC Hausa',
+  'Timi Frank: Osinbajo ya maka mutum biyu',
+  'Gwaninta ba ta karbi wani dan Nijeriya',
+];
 
-describe('One item', () => {
+describe('One useful item', () => {
   shouldMatchSnapshot(
     'should render correctly',
-    <UsefulLink usefulItems={singleItem} strapline={false} />,
+    <UsefulLink usefulItems={usefulCaptions[0]} />,
+  );
+});
+
+describe('Multiple useful items', () => {
+  shouldMatchSnapshot(
+    'should render correctly',
+    <UsefulLinksUl>
+      {usefulCaptions.map(item => {
+        return (
+          <UsefulLinksLi>
+            <UsefulLinkItem>{item}</UsefulLinkItem>
+          </UsefulLinksLi>
+        );
+      })}
+    </UsefulLinksUl>,
   );
 });
