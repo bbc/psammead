97c97b2ac9d641e72ce55a80c63c4f450464291d
ibMadbouly <ibrahim.madbouly@andela.com>
2019-10-01 18:29:03 +0200

    src to live radio and placeholder update

--- packages/components/psammead-media-player/src/index.stories.jsx
+++ packages/components/psammead-media-player/src/index.stories.jsx
@@ -33,8 +33,8 @@ storiesOf('Components|Media Player', module)
   .add('Audio Skin AMP', () => (
     <AmpMediaPlayer
       isAmp
-      src="https://www.test.bbc.co.uk/ws/av-embeds/articles/c3wmq4d1y3wo/p01k6msp"
-      placeholderSrc="https://ichef.bbci.co.uk/news/640/cpsdevpb/4eb7/test/ba7482d0-cca8-11e8-b0bf-f33155223fc4.jpg"
+      src="https://www.test.bbc.com/ws/av-embeds/media/bbc_korean_radio/liveradio/amp"
+      placeholderSrc="https://news.files.bbci.co.uk/include/articles/public/images/audio-player-placeholder.png"
       skin="audio"
     />
   ));
