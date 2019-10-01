47ab0abebc6bccdc270e74d06af6c7ead75b8a77
ibMadbouly <ibrahim.madbouly@andela.com>
2019-10-01 18:18:14 +0200

    add ampDecorator and fix AMP player props

--- packages/components/psammead-media-player/src/index.stories.jsx
+++ packages/components/psammead-media-player/src/index.stories.jsx
@@ -28,11 +28,13 @@ storiesOf('Components|Media Player', module).add('Audio Skin', () => (
   />
 ));
 
-storiesOf('Components|Media Player', module).add('Audio Skin AMP', () => (
-  <AmpMediaPlayer
-    isAmp
-    src="https://www.test.bbc.com/ws/av-embeds/media/bbc_korean_radio/liveradio"
-    showPlaceholder={false}
-    skin="audio"
-  />
-));
+storiesOf('Components|Media Player', module)
+  .add(ampDecorator)
+  .add('Audio Skin AMP', () => (
+    <AmpMediaPlayer
+      isAmp
+      src="https://www.test.bbc.co.uk/ws/av-embeds/articles/c3wmq4d1y3wo/p01k6msp"
+      placeholderSrc="https://ichef.bbci.co.uk/news/640/cpsdevpb/4eb7/test/ba7482d0-cca8-11e8-b0bf-f33155223fc4.jpg"
+      skin="audio"
+    />
+  ));
