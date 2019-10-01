689af4d5b0d2a22ebdf9f534c0b5b5121b0b56b0
Gerald Pharin <gerald.pharin@andela.com>
2019-10-01 08:49:08 +0000

    Update package-lock

--- scripts/talos/bumpSimorghPackages/index.js
+++ scripts/talos/bumpSimorghPackages/index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const { exec, rm, mkdir } = require('shelljs');
+
+const readFileSync = require('../../utilities/readFileSync');
 const getRemoteFile = require('../../utilities/getRemoteFile');
 const createRemoteBranch = require('../../utilities/createRemoteBranch');
 const commitRemoteChanges = require('../../utilities/commitRemoteChanges');
@@ -7,6 +11,7 @@ const getPackageNames = require('../createPullRequest/getPackageNames');
 const getChangelogHead = require('../getChangelogHead');
 
 const path = 'package.json';
+const lockPath = 'package-lock.json';
 const repoName = 'simorgh';
 const branchName = getBranchName();
 
@@ -35,13 +40,45 @@ const getUpdates = (dependencies, publishedPackages) => {
   return updates;
 };
 
-const updatePackageJson = (packageJson, publishedPackages) => {
+const updatePackageJson = async (packageJson, publishedPackages) => {
   const newPackageJson = { ...packageJson };
   Object.keys(publishedPackages).forEach(key => {
     if (newPackageJson.dependencies[key])
       newPackageJson.dependencies[key] = publishedPackages[key];
   });
+  await commitRemoteChanges({
+    repoName,
+    branchName,
+    path,
+    content: JSON.stringify(newPackageJson),
+    message: 'Talos - Update package.json',
+  });
   return newPackageJson;
+};
+
+const updatePackageLock = async packageFile => {
+  const lockResponse = await getRemoteFile({
+    repoName,
+    branch: 'latest',
+    path: lockPath,
+  });
+  const packageLock = lockResponse.data.contents;
+  const folder = 'simorgh-package';
+  rm('-rf', `./${folder}`);
+  mkdir(`./${folder}`);
+  fs.writeFileSync(`./${folder}/package.json`, packageFile);
+  fs.writeFileSync(`./${folder}/package-lock.json`, packageLock);
+  exec('npm install', { cwd: `./${folder}` });
+  const newPackageLock = readFileSync(`./${folder}/package-lock.json`);
+  rm('-rf', `./${folder}`);
+
+  commitRemoteChanges({
+    repoName,
+    branchName,
+    path,
+    content: JSON.stringify(newPackageLock),
+    message: 'Talos - Update package-lock.json',
+  });
 };
 
 const getSimorghPullRequestBody = updates => {
@@ -63,19 +100,14 @@ const bumpSimorghPackages = async bumpedPackages => {
     ...getUpdates(oldPackageJson.dependencies, publishedPackages),
     ...getUpdates(oldPackageJson.devDependencies, publishedPackages),
   ];
-  const newPackageJson = updatePackageJson(oldPackageJson, publishedPackages);
-  const body = getSimorghPullRequestBody(updates);
   await createRemoteBranch({ newBranch: branchName, repoName });
-
-  commitRemoteChanges({
-    repoName,
-    branchName,
-    path,
-    content: JSON.stringify(newPackageJson),
-    message: 'Talos - Bump Dependencies',
-  }).then(() => {
-    createPullRequest({ branchName, repoName, title, body });
-  });
+  const newPackageJson = await updatePackageJson(
+    oldPackageJson,
+    publishedPackages,
+  );
+  await updatePackageLock(newPackageJson);
+  const body = getSimorghPullRequestBody(updates);
+  createPullRequest({ branchName, repoName, title, body });
 };
 
 module.exports = bumpSimorghPackages;
