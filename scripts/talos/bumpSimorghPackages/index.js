2b022070abdd67bcea49dbc4116d385f43925ca9
Gerald Pharin <gerald.pharin@andela.com>
2019-10-03 22:03:45 +0000

    Get branch name from talos tree

--- scripts/talos/bumpSimorghPackages/index.js
+++ scripts/talos/bumpSimorghPackages/index.js
@@ -1,7 +1,6 @@
 const getRemoteFile = require('../../utilities/getRemoteFile');
 const createRemoteBranch = require('../../utilities/createRemoteBranch');
 const createPullRequest = require('../../utilities/createPullRequest');
-const getBranchName = require('../getBranchName');
 const getPackageNames = require('../createPullRequest/getPackageNames');
 const getChangelogHead = require('../getChangelogHead');
 const getPublishedPackages = require('./getPublishedPackages');
@@ -11,13 +10,14 @@ const updatePackageJson = require('./updatePackageJson');
 
 const path = 'package.json';
 const repoName = 'simorgh';
-const branchName = getBranchName();
 
 const getSimorghPullRequestBody = updates => {
-  return updates.map(update => getChangelogHead(update)).join('\n\n');
+  return updates
+    .map(update => `${update}\n${getChangelogHead(update)}`)
+    .join('\n\n');
 };
 
-const bumpSimorghPackages = async bumpedPackages => {
+const bumpSimorghPackages = async (bumpedPackages, branchName) => {
   const publishedPackages = getPublishedPackages(bumpedPackages);
   const packageList = Object.keys(publishedPackages);
   const title = `Talos - Bump ${getPackageNames(packageList)}`;
@@ -29,17 +29,20 @@ const bumpSimorghPackages = async bumpedPackages => {
   });
   const oldPackageJson = JSON.parse(response.data.contents);
   const updates = [
-    ...getUpdates(oldPackageJson.dependencies, publishedPackages),
-    ...getUpdates(oldPackageJson.devDependencies, publishedPackages),
+    ...new Set([
+      ...getUpdates(oldPackageJson.dependencies, publishedPackages),
+      ...getUpdates(oldPackageJson.devDependencies, publishedPackages),
+    ]),
   ];
   await createRemoteBranch({ newBranch: branchName, repoName });
   const newPackageJson = await updatePackageJson(
     oldPackageJson,
     publishedPackages,
+    branchName,
   );
-  await updatePackageLock(newPackageJson);
+  await updatePackageLock(newPackageJson, branchName);
   const body = getSimorghPullRequestBody(updates);
-  createPullRequest({ branchName, repoName, title, body });
+  return createPullRequest({ branchName, repoName, title, body });
 };
 
 module.exports = bumpSimorghPackages;
