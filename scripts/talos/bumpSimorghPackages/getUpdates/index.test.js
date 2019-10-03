838326d14de9dde1678bf4127434f3411e16cc8c
Gerald Pharin <gerald.pharin@andela.com>
2019-10-03 22:13:45 +0000

    Write tests for getUpdates

--- scripts/talos/bumpSimorghPackages/getUpdates/index.test.js
+++ scripts/talos/bumpSimorghPackages/getUpdates/index.test.js
@@ -1,46 +1,31 @@
 const getUpdates = require('./index');
+const packageJson = require('../mockPackageJson');
 
-const packageJson = {
-  dependencies: {
-    '@bbc/gel-foundations': '^3.4.0',
-    '@bbc/moment-timezone-include': '^1.1.2',
-    '@bbc/psammead-amp-geo': '^1.1.2',
-    '@bbc/psammead-assets': '^2.4.0',
-    '@bbc/psammead-caption': '^2.2.9',
-    '@bbc/psammead-figure': '^1.2.5',
-  },
-  devDependencies: {
-    '@babel/core': '^7.6.0',
-    '@babel/polyfill': '^7.6.0',
-    '@bbc/apache2-license-checker': '^1.1.4',
-    '@bbc/psammead-storybook-helpers': '^6.0.1',
-  },
-};
+const updatedDependencies = [
+  '@bbc/gel-foundations  ^3.4.0  →  ^3.5.2',
+  '@bbc/psammead-assets  ^2.4.0  →  ^2.5.1',
+  '@bbc/psammead-caption  ^2.2.9  →  ^2.3.2',
+];
 
-const updatedDependencies = {
-  '@bbc/gel-foundations': '^3.5.2',
-  '@bbc/moment-timezone-include': '^1.1.2',
-  '@bbc/psammead-amp-geo': '^1.1.2',
-  '@bbc/psammead-assets': '^2.5.1',
-  '@bbc/psammead-caption': '^2.3.2',
-  '@bbc/psammead-figure': '^1.2.5',
-}
-
-const updatedDevDependencies = {
-  '@babel/core': '^7.6.0',
-  '@babel/polyfill': '^7.6.0',
-  '@bbc/apache2-license-checker': '^1.1.4',
-  '@bbc/psammead-storybook-helpers': '^6.0.1',
-};
+const updatedDevDependencies = [
+  '@bbc/psammead-storybook-helpers  ^6.0.1  →  ^6.2.1',
+];
 
 const publishedPackages = {
   '@bbc/gel-foundations': '^3.5.2',
   '@bbc/psammead-assets': '^2.5.1',
   '@bbc/psammead-caption': '^2.3.2',
+  '@bbc/psammead-storybook-helpers': '^6.2.1',
 };
 
 describe('getUpdates', () => {
   it('should update dependencies', () => {
     const updates = getUpdates(packageJson.dependencies, publishedPackages);
+    expect(updates).toEqual(updatedDependencies);
+  });
+
+  it('should update dev dependencies', () => {
+    const updates = getUpdates(packageJson.devDependencies, publishedPackages);
+    expect(updates).toEqual(updatedDevDependencies);
   });
 });
