13c85565b59de6c1c2914a5198e5df768b29ca9b
Gerald Pharin <gerald.pharin@andela.com>
2019-10-03 22:07:34 +0000

    Add dev dependencies to package json update

--- scripts/talos/bumpSimorghPackages/updatePackageJson/index.js
+++ scripts/talos/bumpSimorghPackages/updatePackageJson/index.js
@@ -1,20 +1,27 @@
 const commitRemoteChanges = require('../../../utilities/commitRemoteChanges');
 
+const mergeUpdates = (packageJson, publishedPackages) => {
+  const newPackageJson = { ...packageJson };
+  Object.keys(publishedPackages).forEach(key => {
+    if (newPackageJson.dependencies[key])
+      newPackageJson.dependencies[key] = publishedPackages[key];
+    if (newPackageJson.devDependencies[key])
+      newPackageJson.devDependencies[key] = publishedPackages[key];
+  });
+  return newPackageJson;
+};
+
 const updatePackageJson = async (
   packageJson,
   publishedPackages,
   branchName,
 ) => {
-  const newPackageJson = { ...packageJson };
-  Object.keys(publishedPackages).forEach(key => {
-    if (newPackageJson.dependencies[key])
-      newPackageJson.dependencies[key] = publishedPackages[key];
-  });
+  const newPackageJson = mergeUpdates(packageJson, publishedPackages);
   await commitRemoteChanges({
     repoName: 'simorgh',
     branchName,
     path: 'package.json',
-    content: JSON.stringify(newPackageJson),
+    content: JSON.stringify(newPackageJson, null, 2),
     message: 'Talos - Update package.json',
   });
   return newPackageJson;
