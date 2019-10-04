f39c8ff8546042aa6cca681c9b32c1ba8d2e39f2
Gerald Pharin <gerald.pharin@andela.com>
2019-10-04 14:25:32 +0000

    Properly name published packages

--- scripts/talos/bumpSimorghPackages/getPublishedPackages/index.js
+++ scripts/talos/bumpSimorghPackages/getPublishedPackages/index.js
@@ -1,8 +1,8 @@
 const getPublishedPackages = bumpedPackages => {
   const publishedPackages = {};
   Object.values(bumpedPackages).forEach(bumpedPackageList => {
-    bumpedPackageList.forEach(bumpedPackage => {
-      const sections = bumpedPackage.replace(/  +/g, ' ').split(' ');
+    bumpedPackageList.forEach(publishedPackage => {
+      const sections = publishedPackage.replace(/  +/g, ' ').split(' ');
       if (sections.length === 4) {
         const packageName = sections[0];
         const version = sections[3];
