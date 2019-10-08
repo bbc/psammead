389be43ea8547b1112bfbaae2904b861f9174e5b
Simon Sinclair <simon.sinclair@bbc.co.uk>
2019-10-08 16:15:21 +0100

    Rename `mediaInfo` to `withDuration`

--- packages/components/psammead-media-player/src/index.stories.jsx
+++ packages/components/psammead-media-player/src/index.stories.jsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { CanonicalMediaPlayer, AmpMediaPlayer } from '.';
 import { ampDecorator } from '../../../../.storybook/config';
 
-const mediaInfo = {
+const withDuration = {
   duration: '2:30',
   durationSpoken: '2 minutes 30 seconds',
   datetime: 'PT2M30S',
@@ -14,7 +14,7 @@ storiesOf('Components|Media Player', module).add('default', () => (
     src="https://www.test.bbc.co.uk/ws/av-embeds/articles/c3wmq4d1y3wo/p01k6msp/en"
     placeholderSrc="https://ichef.bbci.co.uk/news/640/cpsdevpb/4eb7/test/ba7482d0-cca8-11e8-b0bf-f33155223fc4.jpg"
     service="news"
-    mediaInfo={{ title: 'Dog chases cat.', ...mediaInfo }}
+    mediaInfo={{ title: 'Dog chases cat.', ...withDuration }}
   />
 ));
 
@@ -34,7 +34,7 @@ storiesOf('Components|Media Player', module).add('Audio Skin', () => (
     showPlaceholder={false}
     skin="audio"
     service="news"
-    mediaInfo={{ type: 'audio', title: 'Dog barks at cat.', ...mediaInfo }}
+    mediaInfo={{ type: 'audio', title: 'Dog barks at cat.', ...withDuration }}
   />
 ));
 
