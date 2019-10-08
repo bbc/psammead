801f3586f50b07e4c2b5a37cbb214e545e5d9467
Alistair Gempf <alistair.gempf@bbc.co.uk>
2019-10-08 13:55:13 +0100

    Change number of columns based on screen width

--- packages/components/psammead-navigation/src/index.jsx
+++ packages/components/psammead-navigation/src/index.jsx
@@ -82,7 +82,17 @@ const StyledUnorderedList = styled.ul`
     inMenu &&
     grid &&
     `display: grid;
-    grid-template-columns: repeat(3, 1fr);`}
+    grid-template-columns: repeat(3, 1fr);
+    @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+    @media (min-width: ${GEL_GROUP_5_SCREEN_WIDTH_MIN}) {
+      grid-template-columns: repeat(5, 1fr);
+    }
+    @media (max-width: ${GEL_GROUP_1_SCREEN_WIDTH_MAX}) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  `}
 `;
 
 const StyledListItem = styled.li`
