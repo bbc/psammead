99d4afaf33c10f7f1d1a0214b70900428af4de17
Harvey Peachey <harvey.peachey@bbc.co.uk>
2019-10-10 13:49:44 +0100

    Removed old Jalaali implementation

--- packages/utilities/psammead-locales/moment/ps.js
+++ packages/utilities/psammead-locales/moment/ps.js
@@ -1,27 +1,7 @@
 var moment = require('moment');
-var jalaaliHelper = require('./helpers/jalaali');
-var stringHelper = require('./helpers/stringHelper');
-
-var pashtoJalaliMonths = [
-  'وری',
-  'غویی',
-  'غبرګولی',
-  'چنګاښ',
-  'زمری',
-  'وږی',
-  'تله',
-  'لړم',
-  'لیندۍ',
-  'مرغومی',
-  'سلواغه',
-  'کب',
-];
 
 // eslint-disable-next-line prettier/prettier
 var pashtoGregorianMonths = 'جنوري_فبروري_مارچ_اپریل_می_جون_جولاې_اګست_سپتمبر_اکتوبر_نومبر_ډیسمبر'.split('_');
-
-// Moment formats that should have the Jalali date added
-var jalaliFormats = ['D MMMM YYYY', 'LL'];
 
 moment.defineLocale('ps', {
   months: pashtoGregorianMonths,
@@ -32,15 +12,5 @@ moment.defineLocale('ps', {
     mm: '%d دقیقې', // %d 'minutes'
     h: '۱ ساعت', // '1 hour'
     hh: '%d ساعتونه', // %d 'hours'
-  },
-  postformat: function(string) {
-    var str = jalaaliHelper.addJalaliDate(
-      'ps',
-      pashtoJalaliMonths,
-      jalaliFormats,
-      string
-    );
-
-    return stringHelper.useEasternNumerals(str);
   },
 });
