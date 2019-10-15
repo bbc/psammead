efd9d74f5fbaf6cfb6c36274cad58296608159be
Gerald Pharin <gerald.pharin@andela.com>
2019-10-15 10:49:59 +0000

    Create getPackageVersion module

---
+++ scripts/utilities/getPackageVersion/index.js
@@ -0,0 +1,10 @@
+const readFile = require('../readFileSync');
+const getPackagePath = require('../getPackagePath');
+
+const getPackageVersion = packageName => {
+  const path = getPackagePath(packageName.replace('@bbc', ''));
+  const packageJson = JSON.parse(readFile(`${path}/package.json`));
+  return packageJson.version;
+};
+
+module.exports = getPackageVersion;
