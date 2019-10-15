6e814d6508170c37e795cf9a10adba690a23858e
Gerald Pharin <gerald.pharin@andela.com>
2019-10-15 10:56:10 +0000

    Add slash to packageName replacement

--- scripts/utilities/getPackageVersion/index.js
+++ scripts/utilities/getPackageVersion/index.js
@@ -2,7 +2,7 @@ const readFile = require('../readFileSync');
 const getPackagePath = require('../getPackagePath');
 
 const getPackageVersion = packageName => {
-  const path = getPackagePath(packageName.replace('@bbc', ''));
+  const path = getPackagePath(packageName.replace('@bbc/', ''));
   const packageJson = JSON.parse(readFile(`${path}/package.json`));
   return packageJson.version;
 };
