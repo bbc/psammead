bc2079b9d09b47c35c2200daa52fdafe8cc9ee97
Ruth Ogendi <ruth.ogendi@andela.com>
2019-10-15 15:03:41 +0300

    Add snapshot test.

--- packages/components/psammead-story-promo-feature/src/index.test.jsx
+++ packages/components/psammead-story-promo-feature/src/index.test.jsx
@@ -1,6 +1,40 @@
-// import React from 'react';
-// import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-// import { render } from '@testing-library/react';
-// import FeaturedStoryPromo from './index';
+import React from 'react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { latin } from '@bbc/gel-foundations/scripts';
+import MediaIndicator from '@bbc/psammead-media-indicator';
+import { Headline, Summary, Link } from '@bbc/psammead-story-promo';
+import FeaturedStoryPromo from './index';
 
-describe('FeaturedStoryPromo', () => {});
+const Image = <img src="https://foobar.com/image.png" alt="Alt text" />;
+
+// eslint-disable-next-line react/prop-types
+const Info = () => (
+  <>
+    <Headline script={latin} service="news">
+      <Link href="https://www.bbc.co.uk/news">The headline of the promo</Link>
+    </Headline>
+    <Summary script={latin} service="news">
+      The summary of the promo
+    </Summary>
+  </>
+);
+
+const mediaInfo = (
+  <MediaIndicator duration="2:15" datetime="PT2M15S" service="news" />
+);
+
+describe('FeaturedStoryPromo', () => {
+  const leadingPromo = {
+    image: Image,
+    info: Info(),
+    mediaIndicator: mediaInfo,
+  };
+
+  shouldMatchSnapshot(
+    'should render correctly',
+    <FeaturedStoryPromo
+      leadingPromo={leadingPromo}
+      secondaryPromo={leadingPromo}
+    />,
+  );
+});
