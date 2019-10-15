118901fcbb1f896972f073caaeeb9622c82e2e7c
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-10-15 15:26:32 +0100

    grid fix

--- packages/components/psammead-sitewide-links/src/List/index.jsx
+++ packages/components/psammead-sitewide-links/src/List/index.jsx
@@ -41,13 +41,13 @@ const StyledList = styled.ul`
   @media (min-width: ${GEL_GROUP_1_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
     grid-column-gap: ${GEL_SPACING};
     grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(${({ links }) => getRowCount(links, 2)}, auto);
+    grid-template-rows: repeat(4, auto);
     column-count: 2;
   }
   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
     grid-column-gap: ${GEL_SPACING_DBL};
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(${({ links }) => getRowCount(links, 3)}, auto);
+    grid-template-rows: repeat(4, auto);
     column-count: 3;
   }
   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_4_SCREEN_WIDTH_MAX}) {
