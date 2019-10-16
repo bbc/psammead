e1162522866121ad504e85f1cc5af08951932bff
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-10-16 13:09:18 +0100

    link-6 overflow problem fix

--- packages/components/psammead-sitewide-links/src/List/index.jsx
+++ packages/components/psammead-sitewide-links/src/List/index.jsx
@@ -40,7 +40,7 @@ const StyledList = styled.ul`
   }
   @media (min-width: ${GEL_GROUP_1_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
     grid-column-gap: ${GEL_SPACING};
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(4, auto);
     column-count: 2;
   }
