c515a83967c13d470e8eb17e91d0c6491c292ee4
Ruth Ogendi <ruth.ogendi@andela.com>
2019-10-17 11:51:32 +0300

    Add leading story promo + secondary promo example.

--- packages/components/psammead-leading-story-promo/src/index.stories.jsx
+++ packages/components/psammead-leading-story-promo/src/index.stories.jsx
@@ -4,6 +4,7 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import Image from '@bbc/psammead-image';
 import { Headline, Summary, Link } from '@bbc/psammead-story-promo';
+import Grid from '@bbc/psammead-grid';
 import notes from '../README.md';
 import LeadingStoryPromo from './index';
 
@@ -57,11 +58,54 @@ const generateStory = () =>
       );
     },
   });
+
+const generate2FeatureStory = () =>
+  inputProvider({
+    slots: [{ name: 'Headline' }, { name: 'Summary' }],
+    componentFunction: ({
+      slotTexts: [headlineText, summaryText],
+      script,
+      service,
+      dir,
+    }) => {
+      const Info = (
+        <InfoComponent
+          headlineText={headlineText}
+          summaryText={summaryText}
+          script={script}
+          service={service}
+          dir={dir}
+        />
+      );
+
+      const Img = buildImg();
+
+      return (
+        <Grid columns={{ group4: 8 }} enableGelGutters>
+          <Grid item columns={{ group4: 6 }}>
+            <LeadingStoryPromo image={Img} info={Info} mediaIndicator={null} />
+          </Grid>
+          <Grid columns={{ group4: 2 }}>
+            <Grid item columns={{ group4: 2 }}>
+              {Img}
+            </Grid>
+            <Grid item columns={{ group4: 2 }}>
+              {Info}
+            </Grid>
+          </Grid>
+        </Grid>
+      );
+    },
+  });
 /* eslint-enable react/prop-types */
 
 storiesOf('Components|LeadingStoryPromo', module)
   .addDecorator(withKnobs)
   .add('default', generateStory(), {
+    notes,
+    knobs: { escapeHTML: false },
+  })
+  .add('leading promo + secondary story promo', generate2FeatureStory(), {
     notes,
     knobs: { escapeHTML: false },
   });
