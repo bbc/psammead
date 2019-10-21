fe06d9c37fc9ea1fbf7d02a2c77ff63ef52f621d
Rachael Njeri <rachael.njeri@andela.com>
2019-10-21 13:29:38 +0300

    Update index.test.jsx
    Update AMP test with `placeholdersrcset` props

--- packages/components/psammead-media-player/src/Amp/index.test.jsx
+++ packages/components/psammead-media-player/src/Amp/index.test.jsx
@@ -7,6 +7,7 @@ describe('Media Player: Amp', () => {
     'should render an amp-iframe with an amp-img nested inside',
     <Amp
       placeholderSrc="https://foo.bar/placeholder.png"
+      placeholderSrcset="http://foobar.com/images/ic/512xn/p01k6mtv.jpg"
       src="https://foo.bar/iframe"
       title="Media player"
     />,
