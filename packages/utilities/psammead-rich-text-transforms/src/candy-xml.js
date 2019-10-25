00a63a333bbb8498c5516814beb0401b3cee2c7e
Phil Lee <phil.lee02@bbc.co.uk>
2019-10-25 09:57:48 +0100

    correct syntax for path :facepalm:

--- packages/utilities/psammead-rich-text-transforms/src/candy-xml.js
+++ packages/utilities/psammead-rich-text-transforms/src/candy-xml.js
@@ -65,7 +65,7 @@ const convertToBlocks = (node, attributes = []) =>
 const xmlNodeToBlock = (node, attributes) => {
   if (!is(Object, node)) return undefined;
 
-  if (path(node.text)) {
+  if (path(['text'], node)) {
     return fragment(node.text, attributes);
   }
 
