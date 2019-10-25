6b0d4f23ae854c55b92887811dda2635ac4ec714
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2019-10-25 11:38:36 +0000

    Use styled-components to set background

--- packages/components/psammead-media-player/src/Canonical/index.jsx
+++ packages/components/psammead-media-player/src/Canonical/index.jsx
@@ -2,28 +2,34 @@ import React from 'react';
 import { string } from 'prop-types';
 import styled from 'styled-components';
 
-const StyledIframe = styled.iframe`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: 0;
-  overflow: hidden;
-`;
+const Canonical = ({ src, title, placeholderSrc }) => {
+  const backgroundStyle = `
+    background-image: url(${placeholderSrc});
+    background-repeat: no-repeat;
+    background-size: contain;
+  `;
 
-const Canonical = ({ src, title, placeholderSrc }) => (
-  <StyledIframe
-    src={src}
-    title={title}
-    allow="autoplay; fullscreen"
-    gesture="media"
-    allowFullScreen
-    style={
-      placeholderSrc ? { backgroundImage: `url(${placeholderSrc})` } : null
-    }
-  />
-);
+  const StyledIframe = styled.iframe`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+    overflow: hidden;
+    ${placeholderSrc ? backgroundStyle : ''}
+  `;
+
+  return (
+    <StyledIframe
+      src={src}
+      title={title}
+      allow="autoplay; fullscreen"
+      gesture="media"
+      allowFullScreen
+    />
+  );
+};
 
 Canonical.propTypes = {
   src: string.isRequired,
