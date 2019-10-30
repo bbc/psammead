f95f5737bffa824f83afcea09bdcf516f13bf0f4
rowland <rowland.henshaw@andela.com>
2019-10-30 14:18:58 +0100

    added guidance icon to media icons

--- packages/utilities/psammead-assets/src/svgs/mediaIcons.jsx
+++ packages/utilities/psammead-assets/src/svgs/mediaIcons.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { C_EBON } from '@bbc/psammead-styles/colours';
+import { C_EBON, C_WHITE } from '@bbc/psammead-styles/colours';
 import { GEL_SPACING_HLF } from '@bbc/gel-foundations/spacings';
 
 // `currentColor` has been used to better reflect user colour choices in Firefox.
@@ -24,6 +24,11 @@ const AudioMediaIcon = styled(MediaIcon)`
 const PhotoMediaIcon = styled(MediaIcon)`
   width: 1rem;
   height: 0.8125rem;
+`;
+
+const GuidanceIcon = styled(MediaIcon)`
+  width: 1.5rem;
+  height: 1.5rem;
 `;
 
 const mediaIcons = {
@@ -58,6 +63,22 @@ const mediaIcons = {
       <path d="M9,2V0H4V2H0V26H32V2ZM6.5,10A2.5,2.5,0,1,1,9,7.52,2.5,2.5,0,0,1,6.5,10ZM20,23a9,9,0,1,1,9-9A9,9,0,0,1,20,23Z" />
       <circle cx="20" cy="14.02" r="5.5" />
     </PhotoMediaIcon>
+  ),
+  guidance: (
+    <GuidanceIcon
+      viewBox="0 0 44 44"
+      focusable="false"
+      height="35px"
+      width="35px"
+    >
+      <g>
+        <circle fill="inherit" cx="22" cy="22" r="11" />
+        <path
+          fill={C_WHITE}
+          d="M27.1,27.5h-1.8L25,26.3c-0.8,1.1-2,1.4-3.1,1.4c-3.3,0-5.5-2.5-5.5-5.7c0-3.2,2.2-5.7,5.5-5.7c2.3,0,4.7,1.2,5,4h-2.9c-0.1-0.9-1-1.5-2.2-1.5c-1.8,0-2.6,1.6-2.6,3.3c0,1.6,0.7,3.3,2.6,3.3c1.4,0,2.4-0.7,2.5-1.7h-2v-2.1h4.6V27.5z"
+        />
+      </g>
+    </GuidanceIcon>
   ),
 };
 
