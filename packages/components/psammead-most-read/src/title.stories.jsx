bbba66b0093927f141250cb6ac51a179ad6acf81
Ho Ting Lok <hoting.lok@bbc.co.uk>
2019-10-30 16:22:26 +0000

    Updated storybook naming

--- packages/components/psammead-most-read/src/title.stories.jsx
+++ packages/components/psammead-most-read/src/title.stories.jsx
@@ -4,9 +4,9 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import MostReadTitle from './title';
 
-storiesOf('Components|MostRead/MostRead', module)
+storiesOf('Components|MostRead', module)
   .addDecorator(withKnobs)
   .addDecorator(withServicesKnob())
-  .add('Default', ({ script, service }) => (
+  .add('title', ({ script, service }) => (
     <MostReadTitle script={script} service={service} header="Most Read" />
   ));
