c3386e02a94fd78c6b5d193925b5d4bf9f5bc7b3
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2019-11-08 11:04:13 +0000

    Delete extra fill values

--- packages/utilities/psammead-assets/src/svgs/navigationIcons.jsx
+++ packages/utilities/psammead-assets/src/svgs/navigationIcons.jsx
@@ -9,16 +9,12 @@ const MenuIcon = styled.svg`
 const navigationIcons = {
   hamburger: (
     <MenuIcon width={44} height={44}>
-      <path
-        d="M12 29h21v-2.333H12V29zm0-5.833h21v-2.334H12v2.334zM12 15v2.333h21V15H12z"
-        fill="currentColor"
-      />
+      <path d="M12 29h21v-2.333H12V29zm0-5.833h21v-2.334H12v2.334zM12 15v2.333h21V15H12z" />
     </MenuIcon>
   ),
   cross: (
     <MenuIcon width={44} height={44}>
       <path
-        fill="currentColor"
         d="M26.741 15L21.6 20.142 16.458 15 15 16.458l5.142 5.142L15 26.742l1.458 1.458 5.142-5.141 5.141 5.141 1.459-1.458-5.142-5.142 5.142-5.142z"
         fillRule="evenodd"
       />
