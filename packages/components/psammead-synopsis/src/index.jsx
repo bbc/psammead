e759324bb62e05cdb54928bd609f58baab2f5ab7
jimjohnsonrollings <jim.johnson-rollings@bbc.co.uk>
2019-11-10 11:47:59 +0000

    Changes following review

--- packages/components/psammead-synopsis/src/index.jsx
+++ packages/components/psammead-synopsis/src/index.jsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 import { shape, string } from 'prop-types';
 import { C_METAL } from '@bbc/psammead-styles/colours';
-import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import { GEL_SPACING_QUAD } from '@bbc/gel-foundations/spacings';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
 import { getSansBold } from '@bbc/psammead-styles/font-styles';
 import { getDoublePica } from '@bbc/gel-foundations/typography';
 
 const Synopsis = styled.p`
   ${({ script }) => script && getDoublePica(script)};
-  ${({ service }) => getSansBold(service)}
+  ${({ service }) => getSansBold(service)};
   color: ${C_METAL};
-  padding-bottom: 2 * ${GEL_SPACING_DBL};
+  padding: 0 0 ${GEL_SPACING_QUAD} 0;
   margin: 0; /* Reset */
 `;
 
