aae4daec98172c2385575a77c0c1905ffe7f8957
Simon Sinclair <simon.sinclair@bbc.co.uk>
2019-11-14 17:25:49 +0000

    Update index.stories.jsx

--- packages/components/psammead-av-embed-error/src/index.stories.jsx
+++ packages/components/psammead-av-embed-error/src/index.stories.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
 import AvEmbedError from './index';
 
-storiesOf('AvEmbedError', module).add('default', () => <AvEmbedError />, {
-  notes,
-});
+storiesOf('Components|AvEmbedError', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withServicesKnob())
+  .add(
+    'default',
+    ({ service }) => (
+      <AvEmbedError
+        service={service}
+        message={text(
+          'message',
+          "Sorry, we're unable to bring you this media right now.",
+        )}
+      />
+    ),
+    { notes, knobs: { escapeHTML: false } },
+  );
