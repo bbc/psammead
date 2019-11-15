4e47e48688d418667c8072c2f0b060c7be877257
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2019-11-15 12:25:15 +0000

    Create styled components

--- packages/components/psammead-bulleted-list/src/index.jsx
+++ packages/components/psammead-bulleted-list/src/index.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import styled from 'styled-components';
+import { getBodyCopy } from '@bbc/gel-foundations/typography';
+import { getSansRegular } from '@bbc/psammead-styles/font-styles';
+import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
+import Bullet from './bullet.svg';
 
-const PsammeadBulletedList = () => <h1>Hello World</h1>;
+export const BulletedList = styled.ul`
+  ${({ script }) => script && getBodyCopy(script)};
+  ${({ service }) => getSansRegular(service)}
+  list-style-type: none;
+`;
 
-export default PsammeadBulletedList;
+export const BulletedListItem = styled.li`
+  &::before {
+    content: '\u00A0';
+    background: url(${Bullet}) no-repeat
+      ${({ dir }) => (dir === 'rtl' ? 'right' : 'left')} center;
+    display: inline-block;
+    width: ${GEL_SPACING_TRPL};
+    background-size: 30%;
+    ${({ dir }) =>
+      dir === 'rtl'
+        ? `margin-right: -${GEL_SPACING_TRPL}`
+        : `margin-left: -${GEL_SPACING_TRPL}`};
+  }
+`;
+
+export default BulletedList;
