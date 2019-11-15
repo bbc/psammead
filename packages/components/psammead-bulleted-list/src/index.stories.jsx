5126a282f54949e6a666f5fdd3fc1a7a48edcf64
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2019-11-15 12:25:40 +0000

    Create styled stories

--- packages/components/psammead-bulleted-list/src/index.stories.jsx
+++ packages/components/psammead-bulleted-list/src/index.stories.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
-import PsammeadBulletedList from './index';
+import { BulletedList, BulletedListItem } from './index';
 
-storiesOf('PsammeadBulletedList', module).add(
-  'default',
-  () => <PsammeadBulletedList />,
-  { notes },
-);
+storiesOf('Components|BulletedList', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withServicesKnob())
+  .add(
+    'default',
+    ({ text, script, service, dir }) => (
+      <BulletedList script={script} service={service}>
+        <BulletedListItem dir={dir}>{text}</BulletedListItem>
+        <BulletedListItem dir={dir}>
+          {text} {text}
+        </BulletedListItem>
+        <BulletedListItem dir={dir}>{text.substring(0, 10)}</BulletedListItem>
+        <BulletedListItem dir={dir}>
+          {text} {text} {text} {text}
+        </BulletedListItem>
+      </BulletedList>
+    ),
+    { notes },
+  );
