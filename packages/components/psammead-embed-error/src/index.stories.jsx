533af0de789328b88d9472f417525b16eba514b8
Simon Sinclair <simon.sinclair@bbc.co.uk>
2019-11-17 23:54:12 +0000

    Integrate styled container to default story

--- packages/components/psammead-embed-error/src/index.stories.jsx
+++ packages/components/psammead-embed-error/src/index.stories.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
 import EmbedError from './index';
+
+const Container = styled.div`
+  height: calc(100vw * (9 / 16));
+  width: 100vw;
+`;
 
 storiesOf('Components|EmbedError', module)
   .addDecorator(withKnobs)
@@ -11,13 +17,15 @@ storiesOf('Components|EmbedError', module)
   .add(
     'default',
     ({ service }) => (
-      <EmbedError
-        service={service}
-        message={text(
-          'message',
-          "Sorry, we're unable to bring you this media right now.",
-        )}
-      />
+      <Container>
+        <EmbedError
+          service={service}
+          message={text(
+            'message',
+            "Sorry, we're unable to bring you this media right now.",
+          )}
+        />
+      </Container>
     ),
     { notes, knobs: { escapeHTML: false } },
   )
