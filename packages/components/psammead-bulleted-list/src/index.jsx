1de15cd9a040d713c6a61213aa285247ff1005d4
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2019-11-18 13:24:49 +0000

    Encode Svg URI

--- packages/components/psammead-bulleted-list/src/index.jsx
+++ packages/components/psammead-bulleted-list/src/index.jsx
@@ -1,8 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { getBodyCopy } from '@bbc/gel-foundations/typography';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
 import Bullet from './bullet.svg';
+
+const dirBackground = css`
+  background: url("${encodeURIComponent(Bullet)}") no-repeat
+    ${({ dir }) => (dir === 'rtl' ? 'right' : 'left')} center;
+`;
+
+const dirMargin = css`
+  ${({ dir }) =>
+    dir === 'rtl'
+      ? `margin-right: -${GEL_SPACING_TRPL}`
+      : `margin-left: -${GEL_SPACING_TRPL}`};
+`;
 
 const BulletedList = styled.ul`
   ${({ script }) => script && getBodyCopy(script)};
@@ -12,12 +24,8 @@ const BulletedList = styled.ul`
     content: '\u00A0';
     display: inline-block;
     width: ${GEL_SPACING_TRPL};
-    background: url(${Bullet}) no-repeat
-      ${({ dir }) => (dir === 'rtl' ? 'right' : 'left')} center;
-    ${({ dir }) =>
-      dir === 'rtl'
-        ? `margin-right: -${GEL_SPACING_TRPL}`
-        : `margin-left: -${GEL_SPACING_TRPL}`};
+    ${dirBackground}
+    ${dirMargin}
     background-size: 30%;
   }
 `;
