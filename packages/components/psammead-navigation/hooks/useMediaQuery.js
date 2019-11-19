3073aac5435c3aee3fc4549cdc380a5a47154ed2
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-11-19 13:26:32 +0000

    Update useMediaQuery hook to check if the window is defined

--- packages/components/psammead-navigation/hooks/useMediaQuery.js
+++ packages/components/psammead-navigation/hooks/useMediaQuery.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react';
 
 const useMediaQuery = query => {
-  const mediaMatch = window.matchMedia(query);
-  const [matches, setMatches] = useState(mediaMatch.matches);
+  const isClient = typeof window === 'object';
+
+  const mediaMatch = isClient ? window.matchMedia(query) : undefined;
+  const mediaMatches = isClient ? mediaMatch.matches : undefined;
+
+  const [matches, setMatches] = useState(mediaMatches);
 
   useEffect(() => {
+    if (!isClient) {
+      return null;
+    }
+
     const handler = e => setMatches(e.matches);
+
     mediaMatch.addListener(handler);
     return () => mediaMatch.removeListener(handler);
   });
