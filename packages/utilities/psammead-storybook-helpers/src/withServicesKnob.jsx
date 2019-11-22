16cfcec8ff471810a93def2523984aabdfd841a3
thekp <khoa.phan@bbc.co.uk>
2019-11-22 10:17:57 +0000

    add variant as a story prop and add tests

--- packages/utilities/psammead-storybook-helpers/src/withServicesKnob.jsx
+++ packages/utilities/psammead-storybook-helpers/src/withServicesKnob.jsx
@@ -2,20 +2,41 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { select } from '@storybook/addon-knobs';
 import * as scripts from '@bbc/gel-foundations/scripts';
-import LANGUAGE_VARIANTS from './text-variants';
+import EXAMPLES from './text-variants';
+
+const DEFAULT_SERVICE = 'news';
+const SERVICES_LIST = Object.keys(EXAMPLES);
 
 export default ({
-  defaultService = 'news',
-  services = Object.keys(LANGUAGE_VARIANTS),
+  defaultService = DEFAULT_SERVICE,
+  services = SERVICES_LIST,
 } = {}) => storyFn => {
-  const includesService = service => services.includes(service);
-  const service = select(
+  const getServiceAndVariant = selectedItem =>
+    EXAMPLES[selectedItem].variant
+      ? {
+          service: EXAMPLES[selectedItem].service,
+          variant: EXAMPLES[selectedItem].variant,
+        }
+      : { service: selectedItem };
+
+  const includesSelectedService = selectedItem =>
+    services.includes(selectedItem);
+
+  const selectedItem = select(
     'Select a service',
-    services.filter(includesService),
+    services.filter(includesSelectedService),
     defaultService,
   );
-  const { text, script, locale, dir = 'ltr' } = LANGUAGE_VARIANTS[service];
-  const storyProps = { text, script: scripts[script], locale, dir, service };
+
+  const { text, script, locale, dir = 'ltr' } = EXAMPLES[selectedItem];
+
+  const storyProps = {
+    text,
+    script: scripts[script],
+    locale,
+    dir,
+    ...getServiceAndVariant(selectedItem),
+  };
   return (
     <>
       <Helmet htmlAttributes={{ dir }} />
