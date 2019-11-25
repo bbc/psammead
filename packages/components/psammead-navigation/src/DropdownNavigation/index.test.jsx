3cfc1e15d7768bfe6dbc284befae6979c9809f77
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2019-11-25 16:17:58 +0000

    Add AMP tests

--- packages/components/psammead-navigation/src/DropdownNavigation/index.test.jsx
+++ packages/components/psammead-navigation/src/DropdownNavigation/index.test.jsx
@@ -3,36 +3,51 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import { latin } from '@bbc/gel-foundations/scripts';
 import {
   CanonicalDropdown,
+  AmpDropdown,
   DropdownUl,
   DropdownLi,
   CanonicalHamburgerMenu,
+  AmpHamburgerMenu,
 } from './index';
 import pidginNavData from '../../testHelpers/pidgin';
+
+const dropdownList = (
+  <DropdownUl>
+    {pidginNavData.map((item, index) => {
+      const active = index === 3;
+      const { title, url } = item;
+
+      return (
+        <DropdownLi
+          script={latin}
+          service="news"
+          url={url}
+          key={title}
+          active={active}
+          currentPageText="Current page"
+        >
+          {title}
+        </DropdownLi>
+      );
+    })}
+  </DropdownUl>
+);
 
 describe('Canonical dropdown navigation', () => {
   shouldMatchSnapshot(
     'should render correctly',
     <CanonicalDropdown announcedText="Menu" onClose={() => {}}>
-      <DropdownUl>
-        {pidginNavData.map((item, index) => {
-          const active = index === 3;
-          const { title, url } = item;
-
-          return (
-            <DropdownLi
-              script={latin}
-              service="news"
-              url={url}
-              key={title}
-              active={active}
-              currentPageText="Current page"
-            >
-              {title}
-            </DropdownLi>
-          );
-        })}
-      </DropdownUl>
+      {dropdownList}
     </CanonicalDropdown>,
+  );
+});
+
+describe('Amp dropdown navigation', () => {
+  shouldMatchSnapshot(
+    'should render correctly',
+    <AmpDropdown announcedText="Menu" on="">
+      {dropdownList}
+    </AmpDropdown>,
   );
 });
 
@@ -40,5 +55,12 @@ describe('Canonical hamburger menu', () => {
   shouldMatchSnapshot(
     'should render correctly',
     <CanonicalHamburgerMenu announcedText="Menu" onOpen={() => {}} />,
+  );
+});
+
+describe('AMP hamburger menu', () => {
+  shouldMatchSnapshot(
+    'should render correctly',
+    <AmpHamburgerMenu announcedText="Menu" on="" />,
   );
 });
