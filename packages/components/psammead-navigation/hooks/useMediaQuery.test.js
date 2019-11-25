f37f3a2034ac6041d95b1d5286c1dc657116b6b5
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-11-25 15:03:07 +0000

    Apply Alistair feedback

--- packages/components/psammead-navigation/hooks/useMediaQuery.test.js
+++ packages/components/psammead-navigation/hooks/useMediaQuery.test.js
@@ -1,17 +1,35 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useMediaQuery from './useMediaQuery';
 
 describe('MediaQuery hook', () => {
-  it('it should test that the media query matches', async () => {
-    window.matchMedia = jest.fn().mockImplementation(query => {
-      return {
-        matches: query === '(max-width: 600px)',
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-      };
-    });
+  const mockAddListener = jest.fn();
+  const mockRemoveListener = jest.fn();
 
+  window.matchMedia = jest.fn().mockImplementation(query => {
+    return {
+      matches: query === '(max-width: 600px)',
+      addListener: mockAddListener,
+      removeListener: mockRemoveListener,
+    };
+  });
+
+  beforeEach(() => {
+    mockAddListener.mockReset();
+    mockRemoveListener.mockReset();
+  });
+
+  it('it should test that the media query matches', async () => {
     const { result } = renderHook(() => useMediaQuery('(max-width: 600px)'));
+    expect(result.current).toBe(true);
+    expect(mockAddListener).toBeCalled();
+  });
+
+  it('should add a listener that updates the state', async () => {
+    const { result } = renderHook(() => useMediaQuery('(max-width: 600px)'));
+
+    act(() => mockAddListener.mock.calls[0][0]({ matches: false }));
+    expect(result.current).toBe(false);
+    act(() => mockAddListener.mock.calls[0][0]({ matches: true }));
     expect(result.current).toBe(true);
   });
 });
