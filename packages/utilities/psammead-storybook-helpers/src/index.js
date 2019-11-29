ab00f1998ac4af6e5fc8a4155f203ec761c0af2b
thekp <khoa.phan@bbc.co.uk>
2019-11-29 17:37:01 +0000

    export styled paragraphs

--- packages/utilities/psammead-storybook-helpers/src/index.js
+++ packages/utilities/psammead-storybook-helpers/src/index.js
@@ -1,5 +1,19 @@
 import withServicesKnob from './withServicesKnob';
 import { buildRTLSubstories } from './buildRTLSubstories';
-import LANGUAGE_VARIANTS from './text-variants';
+import TEXT_EXAMPLES from './text-variants';
+import {
+  RegularParagraph,
+  BoldParagraph,
+  ItalicParagraph,
+  BoldItalicParagraph,
+} from './styledParagraph';
 
-export { LANGUAGE_VARIANTS, withServicesKnob, buildRTLSubstories };
+export {
+  TEXT_EXAMPLES,
+  withServicesKnob,
+  buildRTLSubstories,
+  RegularParagraph,
+  BoldParagraph,
+  ItalicParagraph,
+  BoldItalicParagraph,
+};
