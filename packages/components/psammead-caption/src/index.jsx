8a0bc2c7eefc48bb9292e0904819d05eb27e4518
RayNjeri <rachael.njeri@andela.com>
2019-12-10 12:05:17 +0300

    Fix for AV caption indentation on mobile
    Signed-off-by: RayNjeri <rachael.njeri@andela.com>

--- packages/components/psammead-caption/src/index.jsx
+++ packages/components/psammead-caption/src/index.jsx
@@ -3,14 +3,9 @@ import { shape, string } from 'prop-types';
 import {
   GEL_SPACING,
   GEL_SPACING_TRPL,
-  GEL_MARGIN_ABOVE_400PX,
   GEL_MARGIN_BELOW_400PX,
 } from '@bbc/gel-foundations/spacings';
-import {
-  GEL_GROUP_2_SCREEN_WIDTH_MIN,
-  GEL_GROUP_3_SCREEN_WIDTH_MAX,
-  GEL_GROUP_4_SCREEN_WIDTH_MIN,
-} from '@bbc/gel-foundations/breakpoints';
+import { GEL_GROUP_2_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 import { getLongPrimer } from '@bbc/gel-foundations/typography';
 import { C_METAL } from '@bbc/psammead-styles/colours';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
@@ -22,12 +17,9 @@ const Caption = styled.figcaption`
   color: ${C_METAL};
   padding: ${GEL_SPACING} ${GEL_MARGIN_BELOW_400PX} 0;
   width: 100%;
-  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-    padding: ${GEL_SPACING} ${GEL_MARGIN_ABOVE_400PX} 0;
-  }
-  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}){
     padding: ${GEL_SPACING} 0 0;
-  }
+    }
   & > p {
     padding-bottom: ${GEL_SPACING_TRPL};
     margin: 0; /* reset */
