d9aa4725c95a5806a946c0e9421356212d23d593
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-12-10 10:49:09 +0000

    Remove cursor:pointer for no-js

--- packages/components/psammead-media-player/src/Placeholder/index.jsx
+++ packages/components/psammead-media-player/src/Placeholder/index.jsx
@@ -13,6 +13,14 @@ const StyledPlaceholder = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+
+  ${({ noJsClassName }) =>
+    noJsClassName &&
+    css`
+      .${noJsClassName} & {
+        cursor: default;
+      }
+    `}
 `;
 
 const StyledPlayButton = styled(PlayButton)`
