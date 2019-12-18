3f1b3dc2faadee88aaf6cf0833335d5b78cacdaf
sadickisaac <isaac.odhiambo@andela.com>
2019-12-18 15:58:52 +0300

    Try another implementation

--- packages/components/psammead-script-link/src/index.jsx
+++ packages/components/psammead-script-link/src/index.jsx
@@ -1,70 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-import { getPica } from '@bbc/gel-foundations/typography';
-import {
-  GEL_GROUP_1_SCREEN_WIDTH_MAX,
-  GEL_GROUP_2_SCREEN_WIDTH_MIN,
-  GEL_GROUP_3_SCREEN_WIDTH_MIN,
-} from '@bbc/gel-foundations/breakpoints';
-import { getSansRegular } from '@bbc/psammead-styles/font-styles';
+import { GEL_GROUP_2_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 import { C_WHITE } from '@bbc/psammead-styles/colours';
+import { getPica } from '@bbc/gel-foundations/typography';
+import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 import { string, shape, node, func } from 'prop-types';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
-
-const touchAreaStyles = `
-&::after {
-  content: '';
-  position: absolute;
-  top: -0.25rem;
-  left: -0.25rem;
-  right: 0;
-  bottom: 0;
-  height: 2.75rem;
-  width: 2.75rem;
-  margin: 0 0.25rem;
-}`;
 
 const StyledLink = styled.a`
   ${({ script }) => script && getPica(script)}
   ${({ service }) => service && getSansRegular(service)}
-  position: relative;
   display: inline-block;
   color: ${C_WHITE};
   text-decoration: none;
-  padding: 0 1rem;
   height: 3rem;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    border: 0.0625rem solid ${C_WHITE};
-  }
-
-  &:hover::before,
-  &:focus::before {
-    border: 0.25rem solid ${C_WHITE};
-  }
-
-  @media (max-width: ${GEL_GROUP_1_SCREEN_WIDTH_MAX}) {
-    line-height: 2.5rem;
-    height: 2.5rem;
-    padding: 0 0.75rem;
-  }
+  border: 1px solid ${C_WHITE};
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-    line-height: 3rem;
-
-    ${touchAreaStyles}
+    line-height: calc(3rem - 8px);
   }
-  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
-    height: 2.25rem;
-    line-height: 2.25rem;
-    padding: 0 0.625rem;
+  > span {
+    margin: 3px;
+    display: inline-block;
+    height: calc(100%);
+    padding: 0 1rem;
+  }
 
-    ${touchAreaStyles}
+  &:hover span {
+    margin: 0;
+    border: 3px solid white;
   }
 `;
 
@@ -76,7 +39,7 @@ const ScriptLink = ({ children, script, service, href, variant, onClick }) => (
     data-variant={variant}
     onClick={onClick}
   >
-    {children}
+    <span>{children}</span>
   </StyledLink>
 );
 
