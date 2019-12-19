f282e0dda0429d37170f653c418fed7d48585e25
sadickisaac <isaac.odhiambo@andela.com>
2019-12-19 09:30:42 +0300

    Handle other breakpoints

--- packages/components/psammead-script-link/src/index.jsx
+++ packages/components/psammead-script-link/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GEL_GROUP_2_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
+import {
+  GEL_GROUP_2_SCREEN_WIDTH_MIN,
+  GEL_GROUP_2_SCREEN_WIDTH_MAX,
+} from '@bbc/gel-foundations/breakpoints';
 import { C_WHITE } from '@bbc/psammead-styles/colours';
 import { getPica } from '@bbc/gel-foundations/typography';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
@@ -13,21 +16,34 @@ const StyledLink = styled.a`
   display: inline-block;
   color: ${C_WHITE};
   text-decoration: none;
-  height: 3rem;
-  border: 1px solid ${C_WHITE};
-  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-    line-height: calc(3rem - 8px);
-  }
+  height: 2.25rem;
+  border: 0.0625rem solid ${C_WHITE};
+  margin: 0.25rem;
+
   > span {
-    margin: 3px;
+    margin: 0.1875rem;
     display: inline-block;
     height: calc(100%);
     padding: 0 1rem;
   }
 
-  &:hover span {
+  &:hover span,
+  &:focus span {
     margin: 0;
-    border: 3px solid white;
+    border: 0.1875rem solid ${C_WHITE};
+  }
+
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    line-height: calc(2.25rem - 0.5rem);
+  }
+
+  @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
+    height: 2.5rem;
+
+    > span {
+      padding: 0 0.75rem;
+      line-height: calc(2.5rem - 0.5rem);
+    }
   }
 `;
 
