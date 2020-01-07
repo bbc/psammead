310ebcd275d84e75d0cce068fabcd58f6ad0666f
Simon Sinclair <simon@simonsinclair.com>
2020-01-07 12:33:45 +0000

    Expand on hook comments

--- packages/components/psammead-media-player/src/Canonical/useTimeout.jsx
+++ packages/components/psammead-media-player/src/Canonical/useTimeout.jsx
@@ -2,11 +2,13 @@ import { useRef, useEffect } from 'react';
 
 /**
  * useTimeout Hook.
- * @param {function} callback A callback function that returns `true` or `false`.
+ * @param {function} callback A callback function that's passed `true` if the timer
+ *                            completes, and `false` if it's interrupted.
  * @param {object} iframeRef A React DOM reference to an iframe.
- * @param {number} timeout The number of milliseconds until the timeout returns `true`.
+ * @param {number} timeout The number of milliseconds until the timer completes.
  */
 const useTimeout = (callback, iframeRef, timeout) => {
+  // Persist a reference to the timer across renders. This is used to clearTimeout.
   const timer = useRef(null);
 
   const handleLoad = () => {
@@ -14,7 +16,10 @@ const useTimeout = (callback, iframeRef, timeout) => {
     callback(false);
   };
 
+  // This particular effect will run on mount and (the returned function) on unmount.
   useEffect(() => {
+    // The `current` property of a React DOM reference refers to the element we're
+    // attaching a `load` event handler to. This event will be removed after firing.
     iframeRef.current.addEventListener('load', handleLoad, { once: true });
     timer.current = setTimeout(() => {
       callback(true);
