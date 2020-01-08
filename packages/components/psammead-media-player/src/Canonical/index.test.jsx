b8395ad5ff91d5c2c12a818d1c3be89fb967282b
Simon Sinclair <simon@simonsinclair.com>
2020-01-08 17:00:12 +0000

    Add 'should render a timeout message...' test

--- packages/components/psammead-media-player/src/Canonical/index.test.jsx
+++ packages/components/psammead-media-player/src/Canonical/index.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render, act } from '@testing-library/react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import Canonical from '.';
 
@@ -11,4 +12,27 @@ describe('Media Player: Canonical', () => {
       service="news"
     />,
   );
+
+  it('should render a timeout message if timeout timer expires', () => {
+    jest.useFakeTimers();
+    const { getByText, unmount } = render(
+      <Canonical
+        src="https://foo.bar/iframe"
+        title="Media player"
+        service="news"
+        timeoutMs={2000}
+      />,
+    );
+
+    act(() => {
+      jest.runTimersToTime(3000);
+    });
+
+    expect(
+      getByText(
+        'The server did not respond. Please check your connection and try again.',
+      ),
+    );
+    unmount();
+  });
 });
