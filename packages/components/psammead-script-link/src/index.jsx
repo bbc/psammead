49c4248c1ce16efc0143bb95a6b611cc0d774f0e
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-01-09 11:11:54 +0000

    Single line margin

--- packages/components/psammead-script-link/src/index.jsx
+++ packages/components/psammead-script-link/src/index.jsx
@@ -18,8 +18,7 @@ const StyledLink = styled.a`
   text-decoration: none;
   height: 2.25rem;
   border: 0.0625rem solid ${C_WHITE};
-  margin: 0.5rem;
-  margin-right: 0rem;
+  margin: 0.5rem 0 0.5rem 0.5rem;
 
   > span {
     margin: 0.1875rem;
