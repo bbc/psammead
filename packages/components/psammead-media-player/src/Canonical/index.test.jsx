621414649ddbff85cc317fa31557b8dbe949c275
Simon Sinclair <simon@simonsinclair.com>
2020-01-14 18:31:38 +0000

    Add 'should not render a timeout message...' test

--- packages/components/psammead-media-player/src/Canonical/index.test.jsx
+++ packages/components/psammead-media-player/src/Canonical/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, act, cleanup } from '@testing-library/react';
+import { render, act, cleanup, fireEvent } from '@testing-library/react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import Canonical from '.';
 
@@ -35,5 +35,21 @@ describe('Media Player: Canonical', () => {
         'There was a problem loading this content. Please check your internet connection and try again.',
       ),
     );
+  });
+
+  it('should not render a timeout message if it loads successfully', () => {
+    const { getByTitle, getByText } = render(
+      <Canonical
+        src="https://foo.bar/iframe"
+        title="Media player"
+        service="news"
+      />,
+    );
+    fireEvent.load(getByTitle('Media player'));
+    expect(() =>
+      getByText(
+        'There was a problem loading this content. Please check your internet connection and try again.',
+      ),
+    ).toThrow();
   });
 });
