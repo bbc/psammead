811fb0f415f8547824462deb46453a95045b75cd
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-01-20 09:54:18 +0000

    Hide all icons from assistive tech

--- packages/utilities/psammead-assets/src/svgs/mediaIcons.jsx
+++ packages/utilities/psammead-assets/src/svgs/mediaIcons.jsx
@@ -49,6 +49,7 @@ const mediaIcons = {
       viewBox="0 0 32 32"
       width="12px"
       height="12px"
+      aria-hidden="true"
       {...defaultAttrs}
     >
       <polygon points="3,32 29,16 3,0" />
@@ -59,6 +60,7 @@ const mediaIcons = {
       viewBox="0 0 13 12"
       width="13px"
       height="12px"
+      aria-hidden="true"
       {...defaultAttrs}
     >
       <path d="M9.021 1.811l-.525.525c.938.938 1.5 2.25 1.5 3.675s-.563 2.738-1.5 3.675l.525.525c1.05-1.087 1.725-2.55 1.725-4.2s-.675-3.112-1.725-4.2z" />
@@ -70,6 +72,7 @@ const mediaIcons = {
       viewBox="0 0 32 26"
       width="16px"
       height="13px"
+      aria-hidden="true"
       {...defaultAttrs}
     >
       <path d="M9,2V0H4V2H0V26H32V2ZM6.5,10A2.5,2.5,0,1,1,9,7.52,2.5,2.5,0,0,1,6.5,10ZM20,23a9,9,0,1,1,9-9A9,9,0,0,1,20,23Z" />
@@ -77,7 +80,13 @@ const mediaIcons = {
     </PhotoMediaIcon>
   ),
   guidance: (
-    <GuidanceIcon viewBox="0 0 32 32" width="32" height="32" {...defaultAttrs}>
+    <GuidanceIcon
+      viewBox="0 0 32 32"
+      width="32"
+      height="32"
+      aria-hidden="true"
+      {...defaultAttrs}
+    >
       <path d="M32,16A16,16,0,1,1,16,0,16,16,0,0,1,32,16Zm-8.6-.8H16.6v3.1h2.9c-.2,1.5-1.6,2.4-3.7,2.4s-3.7-2.4-3.7-4.7,1-4.7,3.7-4.7,3,.8,3.2,2.2h4.2c-.4-4-3.9-5.8-7.2-5.8-4.8,0-8,3.7-8,8.3s3.1,8.3,8,8.3a5.4,5.4,0,0,0,4.5-2.1l.2,1.7h2.7Z" />
     </GuidanceIcon>
   ),
