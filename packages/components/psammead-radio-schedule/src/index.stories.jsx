b27d3f30ed7c501793125fcd7e49c5560b74cf98
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-01-22 10:01:21 +0000

    Update stories

--- packages/components/psammead-radio-schedule/src/index.stories.jsx
+++ packages/components/psammead-radio-schedule/src/index.stories.jsx
@@ -4,10 +4,24 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
 import RadioSchedule from './index';
-import { StartTime } from './StartTime';
+import StartTime from './StartTime';
 
 storiesOf('Components|RadioSchedule', module)
   .addDecorator(withKnobs)
   .addDecorator(withServicesKnob())
   .add('default', () => <RadioSchedule />, { notes })
-  .add('Start Time', () => <StartTime />, { notes });
+  .add(
+    'Start Time',
+    ({ locale, script, service }) => {
+      return (
+        <StartTime
+          timestamp={1566914061212}
+          timezone="Europe/London"
+          locale={locale}
+          script={script}
+          service={service}
+        />
+      );
+    },
+    { notes },
+  );
