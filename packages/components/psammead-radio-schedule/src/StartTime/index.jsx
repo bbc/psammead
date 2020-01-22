1d3f19d788934ceb4abb605fd9cdebedadb51f39
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-01-22 12:17:39 +0000

    Add styling for start time

--- packages/components/psammead-radio-schedule/src/StartTime/index.jsx
+++ packages/components/psammead-radio-schedule/src/StartTime/index.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import { number, string, shape } from 'prop-types';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
+import { C_RHINO } from '@bbc/psammead-styles/colours';
+import { getMinion } from '@bbc/gel-foundations/typography';
+import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 import TimestampContainer from '../../../../containers/psammead-timestamp-container';
 
-const StyledTimestamp = styled.div``;
+const StyledTimestamp = styled.div`
+  ${({ script }) => script && getMinion(script)}
+  ${({ service }) => service && getSansRegular(service)}
+  color: ${C_RHINO};
+`;
 
 const StartTime = ({ timestamp, timezone, locale, script, service }) => {
   return (
@@ -14,6 +21,7 @@ const StartTime = ({ timestamp, timezone, locale, script, service }) => {
         dateTimeFormat="YYYY-MM-DD"
         format="HH:mm"
         isRelative={false}
+        padding={false}
         timezone={timezone}
         script={script}
         locale={locale}
