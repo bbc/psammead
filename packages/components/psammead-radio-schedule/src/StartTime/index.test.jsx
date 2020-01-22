b9bc07ed7eacf96538d1e3f228569b6fbbb04920
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-01-22 12:18:12 +0000

    Pass props to test and update snapshot

--- packages/components/psammead-radio-schedule/src/StartTime/index.test.jsx
+++ packages/components/psammead-radio-schedule/src/StartTime/index.test.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { latin } from '@bbc/gel-foundations/scripts';
 import StartTime from './index';
 
 describe('StartTime', () => {
-  shouldMatchSnapshot('should render correctly', <StartTime />);
+  shouldMatchSnapshot(
+    'should render LTR correctly',
+    <StartTime
+      script={latin}
+      service="news"
+      timestamp={1566914061212}
+      timezone="Europe/London"
+      locale="en-gb"
+    />,
+  );
 });
