ab1755004bf1078b64aca47d9ba7d343ee60ab62
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-01-23 10:33:04 +0000

    Pass direction to test

--- packages/components/psammead-radio-schedule/src/StartTime/index.test.jsx
+++ packages/components/psammead-radio-schedule/src/StartTime/index.test.jsx
@@ -3,15 +3,17 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import { latin, arabic } from '@bbc/gel-foundations/scripts';
 import StartTime from './index';
 
+const testUnixTimestamp = 1566914061212;
+
 describe('StartTime', () => {
   shouldMatchSnapshot(
     'should render LTR correctly',
     <StartTime
       script={latin}
       service="news"
-      timestamp={1566914061212}
-      timezone="Europe/London"
+      timestamp={testUnixTimestamp}
       locale="en-gb"
+      dir="ltr"
     />,
   );
 
@@ -20,9 +22,9 @@ describe('StartTime', () => {
     <StartTime
       script={arabic}
       service="persian"
-      timestamp={1566914061212}
-      timezone="Europe/London"
+      timestamp={testUnixTimestamp}
       locale="fa"
+      dir="rtl"
     />,
   );
 });
