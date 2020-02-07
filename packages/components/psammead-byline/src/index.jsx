ae085a135b0076dea736c2f754f0283ef465fd16
Simon Sinclair <simon@simonsinclair.com>
2020-02-07 14:36:19 +0000

    Add style polish and RTL support

--- packages/components/psammead-byline/src/index.jsx
+++ packages/components/psammead-byline/src/index.jsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import { string, shape } from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
-import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import { GEL_SPACING, GEL_SPACING_HLF } from '@bbc/gel-foundations/spacings';
+import { C_STORM, C_RHINO } from '@bbc/psammead-styles/colours';
+import { GEL_LONG_PRIMER } from '@bbc/gel-foundations/typography';
 
-const AVATAR_DIAMETER = '64px';
+const AVATAR_DIAMETER = '4rem';
 
 const Container = styled.div`
   align-items: center;
   display: flex;
+
+  /* Support RTL */
+  ${({ avatar }) =>
+    avatar &&
+    css`
+      margin-right: -${GEL_SPACING};
+      margin-left: -${GEL_SPACING};
+    `}
 `;
 
 const Avatar = styled.img`
   border-radius: 50%;
   height: ${AVATAR_DIAMETER};
-  margin-right: ${GEL_SPACING_DBL};
+  margin-right: ${GEL_SPACING};
+  margin-left: ${GEL_SPACING};
   width: ${AVATAR_DIAMETER};
 `;
 
-const Identification = styled.div`
+const Person = styled.div`
   display: flex;
   flex-direction: column;
   ${({ service }) => getSansRegular(service)}
+  ${GEL_LONG_PRIMER}
+`;
+
+const Name = styled.span`
+  color: ${C_STORM};
+  margin-bottom: ${({ avatar }) => avatar && GEL_SPACING_HLF};
+`;
+const Title = styled.span`
+  color: ${C_RHINO};
 `;
 
 const Byline = ({ service, name, title, avatar }) => (
-  <Container>
+  <Container avatar={avatar}>
     {avatar && <Avatar src={avatar.src} alt={avatar.alt} />}
-    <Identification service={service}>
-      <span>{name}</span>
-      <span>{title}</span>
-    </Identification>
+    <Person service={service}>
+      <Name avatar={avatar}>{name}</Name>
+      <Title>{title}</Title>
+    </Person>
   </Container>
 );
 
