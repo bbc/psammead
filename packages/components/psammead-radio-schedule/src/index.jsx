de1bd380f9ae852d41b63c85b91e9d06bc1c3850
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-02-13 15:25:50 +0000

    Pass timezone and locale to RadioSchedules

--- packages/components/psammead-radio-schedule/src/index.jsx
+++ packages/components/psammead-radio-schedule/src/index.jsx
@@ -39,9 +39,6 @@ const renderSchedule = ({
     durationLabel,
   } = program;
 
-  // we should format the start-time here and pass the formatted start-time to the  program card
-  const formattedStartTime = 1566914061212;
-
   return (
     <>
       <StartTimeWrapper>
@@ -63,10 +60,12 @@ const renderSchedule = ({
         dir={dir}
         brandTitle={brandTitle}
         episodeTitle={episodeTitle}
-        startTime={formattedStartTime}
+        startTime={startTime}
         state={state}
         link={link}
         stateLabel={stateLabel}
+        timezone={timezone}
+        locale={locale}
       />
     </>
   );
