476f5ea218bd64d0e0ae4afa66f6c38bbdb4c596
rowland <rowland.henshaw@andela.com>
2020-03-04 09:36:47 +0000

    added review feedback suggestions

--- packages/components/psammead-sitewide-links/src/index.test.jsx
+++ packages/components/psammead-sitewide-links/src/index.test.jsx
@@ -7,39 +7,24 @@ describe(`SitewideLinks`, () => {
     href: 'https://www.bbc.co.uk/news',
     text: 'Link',
   };
-  const ccpaLink = {
-    href:
-      'https://www.bbc.com/usingthebbc/cookies/how-can-i-change-my-bbc-cookie-settings/',
-    text: 'AdChoices / Do Not Sell My Info',
-    lang: 'en-GB',
-  };
 
   const links = new Array(7).fill(link).map((linkItem, index) => ({
     ...linkItem,
     text: `${linkItem.text}${index}`,
   }));
 
-  const ccpaLinks = new Array(8).fill(link, 0, 7).map((linkItem, index) => ({
-    ...linkItem,
-    text: `${linkItem.text}${index}`,
-  }));
-
-  ccpaLinks[7] = ccpaLink;
+  // Add CCPA link to links array
+  links.push({
+    href:
+      'https://www.bbc.com/usingthebbc/cookies/how-can-i-change-my-bbc-cookie-settings/',
+    text: 'AdChoices / Do Not Sell My Info',
+    lang: 'en-GB',
+  });
 
   shouldMatchSnapshot(
     'should render correctly',
     <SitewideLinks
       links={links}
-      copyrightText={<span>Text here.</span>}
-      externalLink={link}
-      service="news"
-    />,
-  );
-
-  shouldMatchSnapshot(
-    'should render correctly with CCPA link',
-    <SitewideLinks
-      links={ccpaLinks}
       copyrightText={<span>Text here.</span>}
       externalLink={link}
       service="news"
