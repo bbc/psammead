906484766f55f524ec3d5232bc8a052ff324d4af
bennea27 <andrew.bennett07@bbc.co.uk>
2020-03-05 16:44:19 +0000

    reintroduce include render guard and update snapshot

--- packages/components/psammead-idt1-include/src/index.jsx
+++ packages/components/psammead-idt1-include/src/index.jsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 const Idt1Include = ({ html, requireJsSrc }) => {
+  const [isRequireLoaded, setIsRequireLoaded] = useState(false);
+
+  const handleOnLoad = () => {
+    setIsRequireLoaded(true);
+  };
+
   return (
     <>
       <Helmet>
-        <script async src={requireJsSrc} />
+        <script async onLoad={handleOnLoad} src={requireJsSrc} />
       </Helmet>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      {isRequireLoaded && <div dangerouslySetInnerHTML={{ __html: html }} />}
     </>
   );
 };
