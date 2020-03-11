fd2efbaa43ca3ba7046939f2d5e879bf2e421b8b
bennea27 <andrew.bennett07@bbc.co.uk>
2020-03-11 15:46:16 +0000

    Update snapshot

--- packages/components/psammead-include/src/index.test.jsx
+++ packages/components/psammead-include/src/index.test.jsx
@@ -3,5 +3,8 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import Include from './index';
 
 describe('Include', () => {
-  shouldMatchSnapshot('should render correctly', <Include />);
+  shouldMatchSnapshot(
+    'should render correctly',
+    <Include html="<div>hello</div>" />,
+  );
 });
