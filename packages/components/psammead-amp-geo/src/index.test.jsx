5e375311cbc4c0b658877f75997defb69a1effd6
Ruth Ogendi <ruth.ogendi@andela.com>
2020-04-14 10:52:39 +0300

    Add test.

--- packages/components/psammead-amp-geo/src/index.test.jsx
+++ packages/components/psammead-amp-geo/src/index.test.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import AmpGeo from '.';
+import AmpGeo, { AmpAd } from '.';
 
 describe('AmpGeo', () => {
   shouldMatchSnapshot(
     'it should render amp-geo with inlined JSON data',
     <AmpGeo />,
   );
+});
+
+describe('AmpAd', () => {
+  shouldMatchSnapshot('it should render amp-ad', <AmpAd />);
 });
