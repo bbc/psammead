86d67cc6a0d6f57748c941e43209dbb87b743760
Einstein <forensicslabnet@gmail.com>
2020-04-16 17:29:24 +0300

    Update react-helmet

--- packages/utilities/psammead-test-helpers/index.test.jsx
+++ packages/utilities/psammead-test-helpers/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import * as testHelpers from '.';
 import * as testHelpersFromSrc from './src/index';
 import renderWithHelmet from './src/renderWithHelmet';
