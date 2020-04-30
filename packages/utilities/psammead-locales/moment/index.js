aef2c59c2bf14d1dcf0af9d586cdfad4bd1fc748
Einstein <forensicslabnet@gmail.com>
2020-04-30 12:29:37 +0300

    Export updated locales

---
+++ packages/utilities/psammead-locales/moment/index.js
@@ -0,0 +1,34 @@
+// New locales
+require('./am');
+require('./ha');
+require('./ig');
+require('./om');
+require('./pcm');
+require('./ps');
+require('./rw');
+require('./so');
+require('./ti');
+
+// Updated locales
+require('./ar');
+require('./az');
+require('./bn');
+require('./es');
+require('./gu');
+require('./hi');
+require('./ky');
+require('./mr');
+require('./ne');
+require('./pa-in');
+require('./pt-br');
+require('./ru');
+require('./si');
+require('./sr');
+require('./sr-cyrl');
+require('./sw');
+require('./ta');
+require('./th');
+require('./uk');
+require('./ur');
+require('./uz');
+require('./yo');
