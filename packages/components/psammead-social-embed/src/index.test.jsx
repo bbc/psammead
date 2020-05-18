a1530891c96523df3a444b1a380e3753f8e36fd0
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-05-18 16:56:10 +0100

    Refactor tests to enable testing presence of provider scripts

--- packages/components/psammead-social-embed/src/index.test.jsx
+++ packages/components/psammead-social-embed/src/index.test.jsx
@@ -5,40 +5,102 @@ import { CanonicalSocialEmbed, AmpSocialEmbed } from './index';
 import fixtures from './fixtures';
 
 describe('CanonicalSocialEmbed', () => {
-  Object.values(fixtures).forEach(fixture => {
-    const { source: provider, embed } = fixture;
-    const caption =
-      provider === 'youtube'
-        ? {
-            textPrefixVisuallyHidden: 'Video caption, ',
-            text: 'Warning: Third party content may contain adverts',
-          }
-        : null;
+  it('should render correctly for Twitter', async () => {
+    const { container, unmount } = render(
+      <CanonicalSocialEmbed
+        provider={fixtures.twitter.source}
+        oEmbed={fixtures.twitter.embed.oembed}
+        skipLink={{
+          text: 'Skip %provider_name% content',
+          endTextId: 'skip-%provider%-content',
+          endTextVisuallyHidden: 'End of %provider_name% content',
+        }}
+        fallback={{
+          text: "Sorry but we're having trouble displaying this content",
+          linkText: 'View content on %provider_name%',
+          linkTextSuffixVisuallyHidden: ', external',
+          linkHref: 'embed-url',
+          warningText:
+            'Warning: BBC is not responsible for third party content',
+        }}
+        service="news"
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(
+      document.querySelector(
+        'head script[src="https://platform.twitter.com/widgets.js"]',
+      ),
+    ).toBeTruthy();
+    unmount();
+    expect(
+      document.querySelector(
+        'head script[src="https://platform.twitter.com/widgets.js"]',
+      ),
+    ).toBeFalsy();
+  });
 
-    it(`should render correctly for ${embed.oembed.provider_name}`, () => {
-      const { container } = render(
-        <CanonicalSocialEmbed
-          provider={provider}
-          oEmbed={embed.oembed}
-          skipLink={{
-            text: 'Skip %provider_name% content',
-            endTextId: 'skip-%provider%-content',
-            endTextVisuallyHidden: 'End of %provider_name% content',
-          }}
-          fallback={{
-            text: "Sorry but we're having trouble displaying this content",
-            linkText: 'View content on %provider_name%',
-            linkTextSuffixVisuallyHidden: ', external',
-            linkHref: 'embed-url',
-            warningText:
-              'Warning: BBC is not responsible for third party content',
-          }}
-          service="news"
-          caption={caption}
-        />,
-      );
-      expect(container).toMatchSnapshot();
-    });
+  it('should render correctly for Instagram', async () => {
+    const { container, unmount } = render(
+      <CanonicalSocialEmbed
+        provider={fixtures.instagram.source}
+        oEmbed={fixtures.instagram.embed.oembed}
+        skipLink={{
+          text: 'Skip %provider_name% content',
+          endTextId: 'skip-%provider%-content',
+          endTextVisuallyHidden: 'End of %provider_name% content',
+        }}
+        fallback={{
+          text: "Sorry but we're having trouble displaying this content",
+          linkText: 'View content on %provider_name%',
+          linkTextSuffixVisuallyHidden: ', external',
+          linkHref: 'embed-url',
+          warningText:
+            'Warning: BBC is not responsible for third party content',
+        }}
+        service="news"
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(
+      document.querySelector(
+        'head script[src="https://www.instagram.com/embed.js"]',
+      ),
+    ).toBeTruthy();
+    unmount();
+    expect(
+      document.querySelector(
+        'head script[src="https://www.instagram.com/embed.js"]',
+      ),
+    ).toBeFalsy();
+  });
+
+  it('should render correctly for YouTube', async () => {
+    const { container } = render(
+      <CanonicalSocialEmbed
+        provider={fixtures.youtube.source}
+        oEmbed={fixtures.youtube.embed.oembed}
+        skipLink={{
+          text: 'Skip %provider_name% content',
+          endTextId: 'skip-%provider%-content',
+          endTextVisuallyHidden: 'End of %provider_name% content',
+        }}
+        fallback={{
+          text: "Sorry but we're having trouble displaying this content",
+          linkText: 'View content on %provider_name%',
+          linkTextSuffixVisuallyHidden: ', external',
+          linkHref: 'embed-url',
+          warningText:
+            'Warning: BBC is not responsible for third party content',
+        }}
+        service="news"
+        caption={{
+          textPrefixVisuallyHidden: 'Video caption, ',
+          text: 'Warning: Third party content may contain adverts',
+        }}
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   shouldMatchSnapshot(
