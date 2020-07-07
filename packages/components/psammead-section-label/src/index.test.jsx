d3a5e9a5f1d17d8c9e6838074e8bb281434919b3
rowland <rowland.henshaw@andela.com>
2020-07-07 15:13:04 +0100

    removed unnecesary snapshot

--- packages/components/psammead-section-label/src/index.test.jsx
+++ packages/components/psammead-section-label/src/index.test.jsx
@@ -237,60 +237,18 @@ describe('SectionLabel', () => {
     });
   });
 
-  describe('Without bar', () => {
-    describe('With plain title', () => {
-      shouldMatchSnapshot(
-        'should render correctly',
-        <SectionLabel
-          script={latin}
-          bar={false}
-          labelId="test-section-label"
-          service="news"
-        >
-          This is text in a SectionLabel.
-        </SectionLabel>,
-      );
-
-      shouldMatchSnapshot(
-        'should render correctly with explicit text direction',
-        <SectionLabel
-          script={latin}
-          dir="ltr"
-          bar={false}
-          labelId="test-section-label"
-          service="news"
-        >
-          This is text in a SectionLabel rendering in ltr mode.
-        </SectionLabel>,
-      );
-
-      shouldMatchSnapshot(
-        'should render correctly with arabic script typography values',
-        <SectionLabel
-          script={arabic}
-          dir="rtl"
-          bar={false}
-          labelId="test-section-label"
-          service="persian"
-        >
-          بعض محتوى النص
-        </SectionLabel>,
-      );
-    });
-
-    describe('With heading overriden to be a strong element', () => {
-      shouldMatchSnapshot(
-        'should render correctly as a plain title with no bar',
-        <SectionLabel
-          script={latin}
-          bar={false}
-          labelId="test-section-label"
-          service="news"
-          overrideHeadingAs="strong"
-        >
-          This is text in a SectionLabel.
-        </SectionLabel>,
-      );
-    });
+  describe('With heading overriden to be a strong element', () => {
+    shouldMatchSnapshot(
+      'should render correctly as a plain title with no bar',
+      <SectionLabel
+        script={latin}
+        bar={false}
+        labelId="test-section-label"
+        service="news"
+        overrideHeadingAs="strong"
+      >
+        This is text in a SectionLabel.
+      </SectionLabel>,
+    );
   });
 });
