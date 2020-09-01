f85a82c3bf42afff2ead1c836e04843449bfa2ee
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-09-01 01:55:27 +0000

    Use css for bulleted list instead of svg

--- packages/components/psammead-bulleted-list/src/index.jsx
+++ packages/components/psammead-bulleted-list/src/index.jsx
@@ -2,13 +2,8 @@ import styled from 'styled-components';
 import { string, oneOf, shape } from 'prop-types';
 import { getBodyCopy } from '@bbc/gel-foundations/typography';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
-import {
-  GEL_SPACING_QUAD,
-  GEL_SPACING_DBL,
-} from '@bbc/gel-foundations/spacings';
+import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
-
-const BULLET_PADDING = '1.75rem';
 
 const BulletedList = styled.ul.attrs(() => ({
   role: 'list',
@@ -17,16 +12,19 @@ const BulletedList = styled.ul.attrs(() => ({
   ${({ service }) => getSansRegular(service)};
   margin-top: 0;
   list-style-type: none;
-  & > li::before {
-    content: '\u00A0';
-    padding-left: ${BULLET_PADDING};
-    background: url('data:image/svg+xml,%3Csvg%20height%3D%2710%27%20width%3D%2710%27%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%3E%3Cstyle%3E%40media%20screen%20and%20%28-ms-high-contrast%3A%20active%29%7Bsvg%7Bfill%3A%20%23fff%7D%7D%3C%2Fstyle%3E%3Ccircle%20cx%3D%275%27%20cy%3D%275%27%20r%3D%273%27%3E%3C%2Fcircle%3E%3C%2Fsvg%3E')
-      no-repeat center;
-    ${({ dir }) =>
-      dir === 'rtl'
-        ? `margin-right: -${GEL_SPACING_QUAD};`
-        : `margin-left: -${GEL_SPACING_QUAD};`}
+
+  & > li {
+    position: relative;
   }
+
+  & > li::before {
+    top: 0.5rem;
+    content: ' ';
+    position: absolute;
+    border-width: 1rem;
+    border: 0.1875rem solid #3f3f42;
+    border-radius: 50%;
+    ${({ dir }) => (dir === 'rtl' ? 'right: -1rem;' : 'left: -1rem;')}
 `;
 
 export const BulletedListItem = styled.li.attrs(() => ({ role: 'listitem' }))`
