8aebb2081652d407e2b85ef9da9d2cf9dd91c375
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-10-08 08:56:24 +0100

    Formatting

--- packages/components/psammead-recent-episodes/src/index.jsx
+++ packages/components/psammead-recent-episodes/src/index.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { C_SHADOW, C_METAL, C_EBON, C_CLOUD_LIGHT } from '@bbc/psammead-styles/colours';
-
-import PlayButton from './playButtonJoro';
-
-import { getBodyCopy } from '@bbc/gel-foundations/typography';
-import { scriptPropType } from '@bbc/gel-foundations/prop-types';
+import { C_METAL, C_EBON, C_CLOUD_LIGHT } from '@bbc/psammead-styles/colours';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
-
 import {
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
+import PlayButton from './playButtonJoro';
 
-// Wrapping Grid + adds the dividers
 const RecentEpisodes = styled.ul`
   list-style: none;
   li {
@@ -24,14 +18,12 @@ const RecentEpisodes = styled.ul`
   }
 `;
 
-// Get SVG
-// Align to top
 const PlayIcon = styled.div`
   padding-top: 2px;
   padding-right: 16px;
   display: inline-block;
   vertical-align: top;
-  @media(min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     font-size: 8px;
   }
 `;
@@ -41,13 +33,17 @@ const Wrapper = styled.div`
 `;
 
 // Most stuff goes
-RecentEpisodes.Episode = ({ children, Link, script, service}) => {
+RecentEpisodes.Episode = ({ children, Link, script, service }) => {
   return (
     <li>
       <Link>
-        <PlayIcon><PlayButton /></PlayIcon>
+        <PlayIcon>
+          <PlayButton />
+        </PlayIcon>
         <Wrapper>
-          {children.map(child => React.cloneElement(child, { script, service }))}
+          {children.map(child =>
+            React.cloneElement(child, { script, service }),
+          )}
         </Wrapper>
       </Link>
     </li>
@@ -58,13 +54,13 @@ const base = styled.span`
   ${({ service }) => getSansRegular(service)}
   color: ${C_EBON};
   display: block;
-`; 
+`;
 
 RecentEpisodes.BrandTitle = styled(base)`
   font-weight: 700;
   font-size: 15px;
   line-height: 20px;
-  @media(min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     font-size: 16px;
   }
 `;
@@ -72,7 +68,7 @@ RecentEpisodes.BrandTitle = styled(base)`
 RecentEpisodes.EpisodeTitle = styled(base)`
   font-size: 15px;
   line-height: 18px;
-  @media(min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
     font-size: 14px;
   }
   margin: 4px 0;
@@ -92,7 +88,5 @@ RecentEpisodes.Date = styled(base)`
 RecentEpisodes.propTypes = {};
 
 RecentEpisodes.defaultProps = {};
-
-const StyledSVG = styled.svg``
 
 export default RecentEpisodes;
--- packages/components/psammead-recent-episodes/src/index.stories.jsx
+++ packages/components/psammead-recent-episodes/src/index.stories.jsx
@@ -10,45 +10,45 @@ storiesOf('Components/RecentEpisodes', module)
   .addDecorator(withKnobs)
   .addDecorator(withServicesKnob())
   .add('default', ({ script, service }) => {
-  const episodes = [
-    {
-      id: '1',
-      url: 'https://www.bbc.com',
-      brandTitle: 'Magazine de la Culture',
-      episodeTitle: '4 Avril 2020',
-      duration: 'Duration 59:00',
-    },
-    {
-      id: '2',
-      url: 'https://www.bbc.com',
-      brandTitle: 'Some other Brand',
-      episodeTitle: '4 May 2020',
-      duration: 'Duration 59:00',
-    },
-  ];
-  return (
-    <RecentEpisodes>
-      {episodes.map(episode => (
-        <RecentEpisodes.Episode
-          Link={({ children }) => <a href={episode.url}>{children}</a>}
-          script={script}
-          service={service}
-        >
-          <RecentEpisodes.BrandTitle>
-            {episode.brandTitle}
-          </RecentEpisodes.BrandTitle>
-          {episode.episodeTitle ? (
-            <RecentEpisodes.EpisodeTitle>
-              {episode.episodeTitle}
-            </RecentEpisodes.EpisodeTitle>
-          ) : (
-            <RecentEpisodes.Date>{episode.date}</RecentEpisodes.Date>
-          )}
-          <RecentEpisodes.Duration>
-            {episode.duration}
-          </RecentEpisodes.Duration>
-        </RecentEpisodes.Episode>
-      ))}
-    </RecentEpisodes>
-  );
-});
+    const episodes = [
+      {
+        id: '1',
+        url: 'https://www.bbc.com',
+        brandTitle: 'Magazine de la Culture',
+        episodeTitle: '4 Avril 2020',
+        duration: 'Duration 59:00',
+      },
+      {
+        id: '2',
+        url: 'https://www.bbc.com',
+        brandTitle: 'Some other Brand',
+        episodeTitle: '4 May 2020',
+        duration: 'Duration 59:00',
+      },
+    ];
+    return (
+      <RecentEpisodes>
+        {episodes.map(episode => (
+          <RecentEpisodes.Episode
+            Link={({ children }) => <a href={episode.url}>{children}</a>}
+            script={script}
+            service={service}
+          >
+            <RecentEpisodes.BrandTitle>
+              {episode.brandTitle}
+            </RecentEpisodes.BrandTitle>
+            {episode.episodeTitle ? (
+              <RecentEpisodes.EpisodeTitle>
+                {episode.episodeTitle}
+              </RecentEpisodes.EpisodeTitle>
+            ) : (
+              <RecentEpisodes.Date>{episode.date}</RecentEpisodes.Date>
+            )}
+            <RecentEpisodes.Duration>
+              {episode.duration}
+            </RecentEpisodes.Duration>
+          </RecentEpisodes.Episode>
+        ))}
+      </RecentEpisodes>
+    );
+  });
