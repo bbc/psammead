4e6eef4b0d021c2e9ed3f970da1a69bb027bc8de
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-10-13 08:43:01 +0100

    cheeky hover transition

--- packages/components/psammead-episode-list/src/link.jsx
+++ packages/components/psammead-episode-list/src/link.jsx
@@ -4,6 +4,11 @@ import { C_METAL, C_POSTBOX } from '@bbc/psammead-styles/colours';
 
 const Link = styled.a`
   display: block;
+  .rounded-play-button__outer-circle,
+  .rounded-play-button__inner-circle,
+  .rounded-play-button__triangle {
+    transition: fill ease-in-out 0.2s;
+  }
   &:hover,
   &:focus {
     span {
