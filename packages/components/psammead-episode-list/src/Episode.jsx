739f9d95663a85b12f2e5d1f3bc00b9295b1f499
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-10-16 20:16:09 +0100

    Add padding between icon and text

--- packages/components/psammead-episode-list/src/Episode.jsx
+++ packages/components/psammead-episode-list/src/Episode.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { arrayOf, element } from 'prop-types';
-import { GEL_SPACING_QUIN } from '@bbc/gel-foundations/spacings';
+import {
+  GEL_SPACING_DBL,
+  GEL_SPACING_QUIN,
+} from '@bbc/gel-foundations/spacings';
 import MediaIndicator from './MediaIndicator';
 
 const Wrapper = styled.div`
+  padding-left: ${GEL_SPACING_DBL};
   display: inline-block;
 `;
 
