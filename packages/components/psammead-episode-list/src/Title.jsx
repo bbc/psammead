61650dde610df8711bc772fb90bdff182e403d49
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-10-16 20:05:18 +0100

    Remove hardcoded font size

--- packages/components/psammead-episode-list/src/Title.jsx
+++ packages/components/psammead-episode-list/src/Title.jsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 import { C_EBON } from '@bbc/psammead-styles/colours';
 import { getPica } from '@bbc/gel-foundations/typography';
-import { GEL_GROUP_2_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 
 const Title = styled.span`
@@ -10,9 +9,6 @@ const Title = styled.span`
   color: ${C_EBON};
   display: block;
   font-weight: 700;
-  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-    font-size: 16px;
-  }
 `;
 
 export default Title;
