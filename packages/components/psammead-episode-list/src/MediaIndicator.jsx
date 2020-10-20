6c4ab4c870ccb56a3e326f1c0d911c1e1dad08c5
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-10-20 15:52:49 +0100

    Add svg width for no-css scenario and fill for high contrast mode

--- packages/components/psammead-episode-list/src/MediaIndicator.jsx
+++ packages/components/psammead-episode-list/src/MediaIndicator.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { string } from 'prop-types';
 
+import { C_WHITE } from '@bbc/psammead-styles/colours';
+
 const Wrapper = styled.div`
   display: inline-block;
   width: ${props => props.size};
   height: ${props => props.size};
+  margin-left: 8px;
+  margin-right: 16px;
+  vertical-align: top;
 `;
 
 const MediaIndicator = ({ size }) => (
@@ -14,9 +19,9 @@ const MediaIndicator = ({ size }) => (
     size={size}
     dangerouslySetInnerHTML={{
       __html: `
-      <svg class="rounded-play-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+      <svg class="rounded-play-button" focusable="false" width=${size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${size} ${size}">
           <path class="rounded-play-button__outer-circle" d="M20,0C8.954,0,0,8.954,0,20s8.954,20,20,20s20-8.954,20-20S31.046,0,20,0z"/>
-          <path class="rounded-play-button__inner-circle" fill="#fff" d="M20,1.765C9.929,1.765,1.765,9.929,1.765,20S9.929,38.235,20,38.235S38.235,30.071,38.235,20
+          <path class="rounded-play-button__inner-circle" fill=${C_WHITE} d="M20,1.765C9.929,1.765,1.765,9.929,1.765,20S9.929,38.235,20,38.235S38.235,30.071,38.235,20
           S30.071,1.765,20,1.765z"/>
           <polygon class="rounded-play-button__triangle" points="15.799,26.94 27.062,20.009 15.799,13.078 "/>
       </svg>
