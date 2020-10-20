731771ab31e635204b7f3db4ad344b66cc8e123b
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-10-20 15:51:54 +0100

    Removing padding and extra div

--- packages/components/psammead-episode-list/src/Episode.jsx
+++ packages/components/psammead-episode-list/src/Episode.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { arrayOf, element } from 'prop-types';
-import {
-  GEL_SPACING_DBL,
-  GEL_SPACING_QUIN,
-} from '@bbc/gel-foundations/spacings';
+import { GEL_SPACING_QUIN } from '@bbc/gel-foundations/spacings';
 import MediaIndicator from './MediaIndicator';
 
 const Wrapper = styled.div`
-  padding-left: ${GEL_SPACING_DBL};
   display: inline-block;
 `;
 
 const Episode = ({ children }) => {
   return (
-    <div>
+    <>
       <MediaIndicator size={GEL_SPACING_QUIN} />
       <Wrapper>{children}</Wrapper>
-    </div>
+    </>
   );
 };
 
