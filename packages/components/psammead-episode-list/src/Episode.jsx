124b6bbfec85fe136b6db052f7650dc3fe4789e3
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2020-11-05 16:15:36 +0000

    Update Episode component to use image

--- packages/components/psammead-episode-list/src/Episode.jsx
+++ packages/components/psammead-episode-list/src/Episode.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { arrayOf, element, string } from 'prop-types';
 import { GEL_SPACING_QUIN } from '@bbc/gel-foundations/spacings';
+import tail from 'ramda/src/tail';
+import is from 'ramda/src/is';
+import isEmpty from 'ramda/src/isEmpty';
+import pathOr from 'ramda/src/pathOr';
 import MediaIndicator from './MediaIndicator';
+import Image from './Image';
 
 const Wrapper = styled.div`
   display: inline-block;
   max-width: calc(100% - 64px);
+  vertical-align: top;
 `;
 
-const Episode = ({ children, dir }) => (
-  <>
-    <MediaIndicator size={GEL_SPACING_QUIN} dir={dir} />
-    <Wrapper>{children}</Wrapper>
-  </>
-);
+const Episode = ({ children, dir }) => {
+  const firstChildIsImage = pathOr({}, '0', children).type === Image;
+  return (
+    <>
+      {firstChildIsImage ? (
+        children[0]
+      ) : (
+        <MediaIndicator size={GEL_SPACING_QUIN} dir={dir} />
+      )}
+      <Wrapper>{firstChildIsImage ? tail(children) : children}</Wrapper>
+    </>
+  );
+};
 
 Episode.propTypes = {
   children: arrayOf(element),
