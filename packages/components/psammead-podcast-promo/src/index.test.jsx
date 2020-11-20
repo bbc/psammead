a1aaf53ffce763b49e2cc096cfea80000725dedf
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2020-11-20 11:36:30 +0000

    CR Feedback 2

--- packages/components/psammead-podcast-promo/src/index.test.jsx
+++ packages/components/psammead-podcast-promo/src/index.test.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import * as scripts from '@bbc/gel-foundations/scripts';
 
 import BasicExample from './examples/basic';
+import CardCallToAction from './components/card-call-to-action';
 
 describe('Podcast Promo', () => {
   shouldMatchSnapshot(
     'Basic example should match snapshot',
     <BasicExample script={scripts.latin} service="russian" />,
   );
+
+  describe('card-call-to-action', () => {
+    it('should be aria-hidden', () => {
+      const { container } = render(
+        <CardCallToAction script={scripts.latin} service="russian">
+          Click Me
+        </CardCallToAction>,
+      );
+
+      expect(container.querySelector('span')).toHaveAttribute(
+        'aria-hidden',
+        'true',
+      );
+    });
+  });
 });
