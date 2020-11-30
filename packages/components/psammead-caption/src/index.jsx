45ee56a3e5b14222e93e5bfc342556fddb82befa
Joshua Coventry <josh.coventry@bbc.co.uk>
2020-11-30 23:47:55 +0000

    Adjust GEL spacing, add border, update Captions to 4.1.0

--- packages/components/psammead-caption/src/index.jsx
+++ packages/components/psammead-caption/src/index.jsx
@@ -2,31 +2,32 @@ import styled from '@emotion/styled';
 import { shape, string } from 'prop-types';
 import {
   GEL_SPACING,
+  GEL_SPACING_DBL,
   GEL_SPACING_TRPL,
-  GEL_MARGIN_ABOVE_400PX,
   GEL_MARGIN_BELOW_400PX,
 } from '@bbc/gel-foundations/spacings';
 import {
-  GEL_GROUP_2_SCREEN_WIDTH_MIN,
+  GEL_GROUP_1_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MAX,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
 import { getLongPrimer } from '@bbc/gel-foundations/typography';
 import { C_METAL } from '@bbc/psammead-styles/colours';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
-import { getSansRegularItalic } from '@bbc/psammead-styles/font-styles';
+import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 
 const Caption = styled.figcaption`
   ${({ script }) => script && getLongPrimer(script)};
-  ${({ service }) => getSansRegularItalic(service)}
+  ${({ service }) => getSansRegular(service)}
   color: ${C_METAL};
-  padding: ${GEL_SPACING} ${GEL_MARGIN_BELOW_400PX} 0;
+  padding: 0 ${GEL_MARGIN_BELOW_400PX} 0;
   width: 100%;
-  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-    padding: ${GEL_SPACING} ${GEL_MARGIN_ABOVE_400PX} 0;
+  border-left: 1px solid ${C_METAL};
+  @media (min-width: ${GEL_GROUP_1_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+    margin-left: ${GEL_SPACING};
   }
   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
-    padding: ${GEL_SPACING} 0 0;
+    margin-left: ${GEL_SPACING_DBL};
   }
   & > p {
     padding-bottom: ${GEL_SPACING_TRPL};
