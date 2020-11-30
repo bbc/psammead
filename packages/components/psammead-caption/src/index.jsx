861df835dc3c7482f426f60873a4598b47c2e526
Joshua Coventry <josh.coventry@bbc.co.uk>
2020-11-30 23:43:40 +0000

    Revert "Add left border to Caption component, adjust GEL spacing, bump to 4.1.0"
    This reverts commit 62f8f31992b20d28df3cc6ae104b21de783b4a74.

--- packages/components/psammead-caption/src/index.jsx
+++ packages/components/psammead-caption/src/index.jsx
@@ -2,32 +2,31 @@ import styled from '@emotion/styled';
 import { shape, string } from 'prop-types';
 import {
   GEL_SPACING,
-  GEL_SPACING_DBL,
   GEL_SPACING_TRPL,
+  GEL_MARGIN_ABOVE_400PX,
   GEL_MARGIN_BELOW_400PX,
 } from '@bbc/gel-foundations/spacings';
 import {
-  GEL_GROUP_1_SCREEN_WIDTH_MIN,
+  GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MAX,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
 import { getLongPrimer } from '@bbc/gel-foundations/typography';
 import { C_METAL } from '@bbc/psammead-styles/colours';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
-import { getSansRegular } from '@bbc/psammead-styles/font-styles';
+import { getSansRegularItalic } from '@bbc/psammead-styles/font-styles';
 
 const Caption = styled.figcaption`
   ${({ script }) => script && getLongPrimer(script)};
-  ${({ service }) => getSansRegular(service)}
+  ${({ service }) => getSansRegularItalic(service)}
   color: ${C_METAL};
-  padding: 0 ${GEL_MARGIN_BELOW_400PX} 0;
+  padding: ${GEL_SPACING} ${GEL_MARGIN_BELOW_400PX} 0;
   width: 100%;
-  border-left: 1px solid ${C_METAL};
-  @media (min-width: ${GEL_GROUP_1_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-    margin-left: ${GEL_SPACING};
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+    padding: ${GEL_SPACING} ${GEL_MARGIN_ABOVE_400PX} 0;
   }
   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
-    margin-left: ${GEL_SPACING_DBL};
+    padding: ${GEL_SPACING} 0 0;
   }
   & > p {
     padding-bottom: ${GEL_SPACING_TRPL};
