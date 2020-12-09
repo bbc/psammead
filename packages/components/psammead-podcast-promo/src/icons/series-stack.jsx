73e22ed3a13f3a5b2daaab8a0d2c1eee95b36832
RichardPK <r.phillips.kerr@gmail.com>
2020-12-09 16:58:20 +0000

    Set correct SVG color within span, use unset value to remove default that is assigned in the styled component the svg inherits from

--- packages/components/psammead-podcast-promo/src/icons/series-stack.jsx
+++ packages/components/psammead-podcast-promo/src/icons/series-stack.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { mediaIcons } from '@bbc/psammead-assets/svgs';
 import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
+import { C_METAL } from '@bbc/psammead-styles/colours';
 
 const StyledSeriesStackIcon = styled.span`
+  color: ${C_METAL};
+
   > svg {
+    fill: currentColor;
+    color: unset;
     width: ${GEL_SPACING_DBL};
     height: ${GEL_SPACING_DBL};
-    fill: currentColor;
     position: relative;
     bottom: 2px;
     right: 3px;
