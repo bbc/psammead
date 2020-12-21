a8865d1bd13257c69cb8eb79e37bbc20747c01f4
RichardPK <r.phillips.kerr@gmail.com>
2020-12-21 14:28:26 +0000

    Use flex instead of max-width

--- packages/components/psammead-podcast-promo/src/components/card-content.jsx
+++ packages/components/psammead-podcast-promo/src/components/card-content.jsx
@@ -3,27 +3,19 @@ import styled from '@emotion/styled';
 import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import {
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
-  GEL_GROUP_3_SCREEN_WIDTH_MIN,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
 
 const CardContent = styled.div`
   padding: ${GEL_SPACING};
   display: inline-block;
-  max-width: calc(100% - 96px);
-  /* minus the width of the left element  */
-  /* can this use flex? */
+
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     padding: ${GEL_SPACING} ${GEL_SPACING_DBL};
-    max-width: calc(100% - 117px);
   }
-  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
-    padding: ${GEL_SPACING} ${GEL_SPACING_DBL};
-    max-width: calc(100% - 178px);
-  }
+
   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
     padding: ${GEL_SPACING_DBL};
-    max-width: initial;
   }
 `;
 
--- packages/components/psammead-podcast-promo/src/components/card.jsx
+++ packages/components/psammead-podcast-promo/src/components/card.jsx
@@ -1,9 +1,15 @@
 import styled from '@emotion/styled';
 import { C_GHOST } from '@bbc/psammead-styles/colours';
+import { GEL_GROUP_4_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 
 const Card = styled.div`
   position: relative;
   background-color: ${C_GHOST};
+  display: flex;
+
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    flex-direction: column;
+  }
   &:hover {
     .podcast-promo--hover {
       text-decoration: underline;
