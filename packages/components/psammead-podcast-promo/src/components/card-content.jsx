6edd0e7e38cbb6a38dbe043ba0633f54f5d32bf2
emilysaffron <emiy.saffron@bbc.co.uk>
2021-01-11 11:59:12 +0000

    box-shadow for card content

--- packages/components/psammead-podcast-promo/src/components/card-content.jsx
+++ packages/components/psammead-podcast-promo/src/components/card-content.jsx
@@ -9,7 +9,7 @@ import {
 const CardContent = styled.div`
   padding: ${GEL_SPACING};
   display: inline-block;
-
+  box-shadow: 0 0 5px 5px rgba(34, 34, 34, 0.03);
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     padding: ${GEL_SPACING} ${GEL_SPACING_DBL};
   }
