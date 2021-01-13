3b12d97a4d998e08bf283e01c5e8f75340c1e799
emilysaffron <emiy.saffron@bbc.co.uk>
2021-01-13 16:01:24 +0000

    hide image below 320px

--- packages/components/psammead-podcast-promo/src/components/card-image-wrapper.jsx
+++ packages/components/psammead-podcast-promo/src/components/card-image-wrapper.jsx
@@ -4,11 +4,15 @@ import {
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
+  GEL_GROUP_B_MIN_WIDTH,
 } from '@bbc/gel-foundations/breakpoints';
 
 const CardImageWrapper = styled.div`
   flex: 0 0 5.5rem;
   margin: ${GEL_SPACING} 0 0 ${GEL_SPACING};
+  @media (max-width: ${GEL_GROUP_B_MIN_WIDTH}rem) {
+    display: none;
+  }
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     flex: 0 0 6.8125rem;
   }
