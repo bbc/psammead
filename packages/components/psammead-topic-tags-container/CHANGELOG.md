910e5efd4188bcaad40d3b3ceb7e8cac6ef5cbd0
HarryVerhoef <harry.verhoef1999@gmail.com>
2021-01-22 10:52:37 +0000

    Removed topic-tags-container dir

--- packages/components/psammead-topic-tags-container/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Psammead Consent Banner Changelog
-
-<!-- prettier-ignore -->
-| 1.0.0 | [PR#4248](https://github.com/bbc/psammead/pull/4248) Create initial package. |
--- packages/components/psammead-topic-tags/src/index.stories.jsx
+++ packages/components/psammead-topic-tags/src/index.stories.jsx
@@ -1,11 +1,30 @@
+/* eslint-disable */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
+import {
+  withServicesKnob,
+  buildRTLSubstories,
+} from '@bbc/psammead-storybook-helpers';
+import { oneOf, string } from 'prop-types';
+import { TopicTagsContainer, TopicTag } from '.';
 import notes from '../README.md';
-import TopicTags from './index';
 
-storiesOf('Components/TopicTags', module)
+const STORY_KIND = 'Components/TopicTagsContainer';
+
+storiesOf(STORY_KIND, module)
   .addDecorator(withKnobs)
   .addDecorator(withServicesKnob())
-  .add('default', () => <TopicTags />, { notes });
+  .add(
+    'default',
+    ({ dir }) => {
+      return (
+        <TopicTagsContainer dir={dir}>
+          <TopicTag topicName={"test1"} topicLink={"#"}/>
+        </TopicTagsContainer>
+      );
+    },
+    { notes, knobs: { escapeHTML: false } },
+  );
+
+buildRTLSubstories(STORY_KIND, { include: ['default'] });
