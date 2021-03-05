67bd2c9c3b36cd56ee60a0dead30be091436e077
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-03-05 15:01:22 +0000

    add back to start in psammead wizard

--- tooling/psammead/index.js
+++ tooling/psammead/index.js
@@ -11,43 +11,62 @@ const checkPackages = require('./checkPackages');
 const fixPackages = require('./fixPackages');
 const fetchRandomWisdom = require('./fetchRandomWisdom');
 
-prompt({
-  message:
-    "Hi there. I'm Psammead - I'm a mythical sand goblin thing and I'm here to guide you through managing this monorepo. What would you like to do?",
-  type: 'select',
-  name: 'task',
-  choices: ['I want to version package(s)', 'I want to update changelog(s)'],
-})
-  .then(promptTask)
-  .then(({ task, packages }) => {
-    const isVersioning = task === 'versioning';
+const psammeadWizard = () =>
+  prompt({
+    message:
+      "Hi there. I'm Psammead - I'm a mythical sand goblin thing and I'm here to guide you through managing this monorepo. What would you like to do?",
+    type: 'select',
+    name: 'task',
+    choices: ['I want to version package(s)', 'I want to update changelog(s)'],
+  })
+    .then(promptTask)
+    .then(({ task, packages }) => {
+      const isVersioning = task === 'versioning';
 
-    return isVersioning
-      ? promptSemanticVersion(packages).then(versionPackages)
-      : promptChangelogUpdates(packages).then(updateChangelogs);
-  })
-  .then(async () => {
-    try {
-      await checkPackages();
-    } catch (error) {
-      await fixPackages();
-    }
-  })
-  .then(promptStageAndCommit)
-  .then(async ({ shouldCommitChanges }) => {
-    if (shouldCommitChanges) {
-      return commitChanges('TODO - GENERATE THIS MESSAGE');
-    }
-    return Promise.resolve();
-  })
-  .then(() => {
-    console.log(
-      "We are all finished here. Good job. It's been a pleasure serving you! I'll leave you with these words of wisdom:\n",
-    );
+      return isVersioning
+        ? promptSemanticVersion(packages).then(versionPackages)
+        : promptChangelogUpdates(packages).then(updateChangelogs);
+    })
+    .then(async () => {
+      try {
+        await checkPackages();
+      } catch (error) {
+        await fixPackages();
+      }
+    })
+    .then(promptStageAndCommit)
+    .then(async ({ shouldCommitChanges }) => {
+      if (shouldCommitChanges) {
+        return commitChanges('TODO - GENERATE THIS MESSAGE');
+      }
+      return Promise.resolve();
+    })
+    .then(async () => {
+      const { backToMainMenu } = await prompt({
+        type: 'toggle',
+        name: 'backToMainMenu',
+        message:
+          'We are all finished here. What next? Do you want to go back to the beginning?',
+        enabled: 'Yes',
+        disabled: 'No',
+      });
 
-    const wisdom = fetchRandomWisdom();
+      if (backToMainMenu) {
+        return psammeadWizard();
+      }
 
-    console.log(wisdom.concat('\n'));
-  })
-  // eslint-disable-next-line no-console
-  .catch(console.error);
+      return Promise.resolve();
+    })
+    .then(() => {
+      console.log(
+        "Good job. It's been a pleasure serving you! I'll leave you with these words of wisdom:\n",
+      );
+
+      const wisdom = fetchRandomWisdom();
+
+      console.log(wisdom.concat('\n'));
+    })
+    // eslint-disable-next-line no-console
+    .catch(console.error);
+
+psammeadWizard();
