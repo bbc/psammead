9d02bff43f3c71af9d7f776c36b6df157ee60ab8
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-03-24 12:25:36 +0000

    function tidy-up

--- scripts/utilities/getPackagePath/index.js
+++ scripts/utilities/getPackagePath/index.js
@@ -1,14 +1,12 @@
 const getPackages = require('../getPackages');
 
 module.exports = packageName => {
-  const matchesPath = packagePath =>
-    new RegExp(`/${packageName}$`).test(packagePath);
-
-  const match = getPackages().find(matchesPath);
-
   if (packageName === 'psammead') {
     return '.';
   }
 
-  return match;
+  const matchesPath = packagePath =>
+    new RegExp(`/${packageName}$`).test(packagePath);
+
+  return getPackages().find(matchesPath);
 };
