936ce89b68d745dbe8187d3f1a67a350f5125fb5
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-03-24 10:00:55 +0000

    remove unused function

--- scripts/utilities/stageFile/index.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
-module.exports = filePath =>
-  execSync(`git add ${filePath}`, {
-    cwd: path.resolve(__dirname, '../../../'),
-  });
