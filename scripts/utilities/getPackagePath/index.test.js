6376cadd0dadc1344c23d4bb17adc3197b27ef27
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-03-24 09:54:33 +0000

    fix getPackagePath test

--- scripts/utilities/getPackagePath/index.test.js
+++ scripts/utilities/getPackagePath/index.test.js
@@ -7,41 +7,19 @@ describe('getPackagePath', () => {
   describe('when locally', () => {
     beforeEach(() => {
       getPackages.mockImplementation(() => [
-        '/psammead',
-        '/psammead/packages/components/psammead-brand',
-        '/psammead/packages/components/psammead-image',
+        'psammead',
+        'packages/components/psammead-brand',
+        'packages/components/psammead-image',
       ]);
     });
 
     it('should return the path of the package', () => {
-      expect(getPackagePath('psammead')).toEqual('/psammead');
+      expect(getPackagePath('psammead')).toEqual('.');
       expect(getPackagePath('psammead-brand')).toEqual(
-        '/psammead/packages/components/psammead-brand',
+        'packages/components/psammead-brand',
       );
       expect(getPackagePath('psammead-image')).toEqual(
-        '/psammead/packages/components/psammead-image',
-      );
-    });
-  });
-
-  describe('when on jenkins', () => {
-    beforeEach(() => {
-      getPackages.mockImplementation(() => [
-        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA',
-        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-brand',
-        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-image',
-      ]);
-    });
-
-    it('should return the path of the package', () => {
-      expect(getPackagePath('psammead')).toEqual(
-        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA',
-      );
-      expect(getPackagePath('psammead-brand')).toEqual(
-        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-brand',
-      );
-      expect(getPackagePath('psammead-image')).toEqual(
-        '/psammead-branchname-DIXQWB5T3V53L2PKQUIWNQJXA/packages/components/psammead-image',
+        'packages/components/psammead-image',
       );
     });
   });
