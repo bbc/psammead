0db60dcafdda98eb4c754c91a82d568d22e34851
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-04-06 11:14:49 +0100

    remove unnecessary const

--- scripts/publish/index.js
+++ scripts/publish/index.js
@@ -6,14 +6,13 @@ const getPackages = require('../utilities/getPackages');
 
 const attempted = { success: [], failure: [] };
 const ROOT_PACKAGE = 'psammead';
-const PACKAGE_FILE = 'package.json';
 
 const publishPackage = packageDir => {
   if (packageDir === ROOT_PACKAGE) {
     // we do not publish the root package
     return;
   }
-  const packageFilePath = `./${packageDir}/${PACKAGE_FILE}`;
+  const packageFilePath = `./${packageDir}/package.json`;
   const packageJson = JSON.parse(fs.readFileSync(packageFilePath));
 
   if (shouldPublish(packageJson)) {
