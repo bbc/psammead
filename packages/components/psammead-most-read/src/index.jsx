a4308c4c719309692606066a64a516af70fab388
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-06-10 16:20:38 +0100

    Remove psammead-most-read dir

--- packages/components/psammead-most-read/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import MostReadList from './List';
-import MostReadRank from './Rank';
-import { MostReadLink, MostReadItemWrapper } from './Item';
-
-export { MostReadList, MostReadLink, MostReadItemWrapper, MostReadRank };
