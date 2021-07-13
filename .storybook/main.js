a5d2841576cdd9855bf97b43563b0bfbc2b00dec
Ryan McCombe <ryanmccombe@gmail.com>
2021-07-13 19:12:54 +0100

    Revert "Psammead - Upgrade Storybook"

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,8 +11,6 @@ module.exports = {
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
     '@storybook/addon-viewport',
-    '@storybook/addon-docs',
-    '@storybook/addon-postcss',
   ],
   webpackFinal: async config => {
     return {
