8edc6ab35eb4347d5520dfea1da0466fc8185873
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-07-14 17:08:22 +0100

    removed redundant try catch for CI in getPackages script

--- scripts/utilities/getPackages/index.js
+++ scripts/utilities/getPackages/index.js
@@ -2,14 +2,6 @@ const { exec } = require('shelljs');
 
 module.exports = () => {
   const output = exec('yarn workspaces list --json', { silent: true });
-  console.log(output);
 
-  try {
-    return output.stdout.split('\n').map(JSON.parse);
-  } catch (error) {
-    // handles CI shelljs output
-    return JSON.parse(output);
-  }
+  return output.stdout.split('\n').map(JSON.parse);
 };
-
-module.exports();
