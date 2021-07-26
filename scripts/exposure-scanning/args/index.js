9e2438e36fbe506c122e9e889adad3f5eb5f170f
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-07-26 11:56:46 +0100

    Add flags to cli invocation

--- scripts/exposure-scanning/args/index.js
+++ scripts/exposure-scanning/args/index.js
@@ -7,16 +7,16 @@ const isValidId = id => {
 };
 
 const parseArgs = argv => {
-  if (argv.length !== 6) {
+  if (argv.length !== 8) {
     throw new Error(
-      'Error: Incorrect number of args.\nUsage: node scan.js <repo> <-pr/-issue> <id> <regex>',
+      'Error: Incorrect number of args.\nUsage: node scan.js --experimental-modules --unhandled-rejections=strict <repo> <-pr/-issue> <id> <regex>',
     );
   }
 
-  const repo = argv[2];
-  const flag = argv[3]; // -pr or -issue
-  const id = argv[4];
-  const regexString = argv[5];
+  const repo = argv[4];
+  const flag = argv[5]; // -pr or -issue
+  const id = argv[6];
+  const regexString = argv[7];
 
   if (isValidId(id)) {
     return { repo, flag, id, regexString };
