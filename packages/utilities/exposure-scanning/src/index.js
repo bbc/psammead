db927019701a31d8cf258d69eb977d2261c8081d
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-08-03 13:04:01 +0100

    Put package dependencies in local package.json and refactor index invocations

--- packages/utilities/exposure-scanning/src/index.js
+++ packages/utilities/exposure-scanning/src/index.js
@@ -5,42 +5,44 @@ import { fetchPr, fetchIssue } from '../fetch/index.js';
 import { scanPr, scanIssue } from '../scan/index.js';
 import { patchPr, patchIssue } from '../patch/index.js';
 
+const redactPr = async (reqBody, regex) => {
+  const pr = await fetchPr(reqBody);
+
+  const scannedPr = await scanPr(pr, regex);
+
+  if (scannedPr.foundMatch) {
+    await patchPr(reqBody, scannedPr);
+    throw new Error('Match found.');
+  }
+};
+
+const redactIssue = async (reqBody, regex) => {
+  const issue = await fetchIssue(reqBody);
+
+  const scannedIssue = await scanIssue(issue, regex);
+
+  if (scannedIssue.foundMatch) {
+    await patchIssue(reqBody, scannedIssue);
+    throw new Error('Match found.');
+  }
+};
+
 const scanExposures = async () => {
   const { repo, flag, id, regexString } = parseArgs(process.argv);
 
   const regex = new RegExp(regexString, 'gi');
 
-  if (flag === '-pr') {
-    const reqBody = {
-      owner: 'bbc',
-      repo,
-      prId: id,
-    };
+  const reqBody = {
+    owner: 'bbc',
+    repo,
+    prId: id,
+  };
 
-    const pr = await fetchPr(reqBody);
+  await {
+    '-pr': redactPr,
+    '-issue': redactIssue,
+  }[flag](reqBody, regex);
 
-    const scannedPr = await scanPr(pr, regex);
-
-    if (scannedPr.foundMatch) {
-      await patchPr(reqBody, scannedPr);
-      return Promise.reject(new Error('Match found.'));
-    }
-  } else if (flag === '-issue') {
-    const reqBody = {
-      owner: 'bbc',
-      repo,
-      issueId: id,
-    };
-
-    const issue = await fetchIssue(reqBody);
-
-    const scannedIssue = await scanIssue(issue, regex);
-
-    if (scannedIssue.foundMatch) {
-      await patchIssue(reqBody, scannedIssue);
-      return Promise.reject(new Error('Match found.'));
-    }
-  }
   return Promise.resolve('No matches found.');
 };
 
