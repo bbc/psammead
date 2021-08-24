f89a5aafe1b7e3e1fe852effc281176776e2513f
Akeel Mughal <akeelm_uk@hotmail.com>
2021-08-24 13:59:08 +0100

    Update props to use script and service params
    Co-authored-by: Ryan McCombe <ryanmccombe@gmail.com>

--- packages/components/psammead-content-anchor/src/index.stories.jsx
+++ packages/components/psammead-content-anchor/src/index.stories.jsx
@@ -107,7 +107,7 @@ storiesOf('Components/ContentAnchor', module)
     ({ text, script, service }) => {
       return (
         <Wrapper>
-          <Headline script={latin} service="news">
+          <Headline script={script} service={service}>
             {text}
           </Headline>
           {renderParagraphs({ text, script, service })}
