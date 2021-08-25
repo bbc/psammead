28a99129af395ae3d888232d011e70373e0d3323
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-08-25 09:47:54 +0100

    Log error and set exitCode to 1 on error

--- scripts/scan-exposures/index.js
+++ scripts/scan-exposures/index.js
@@ -1,5 +1,11 @@
+/* eslint-disable no-console */
 const { scanExposures } = require('@bbc/exposure-scanning');
 
 (async () => {
-  await scanExposures();
+  try {
+    await scanExposures();
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1; // This is important to ensure that the GitHub action fails.
+  }
 })();
