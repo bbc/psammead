34dc42f78228e20f17bbca84910e5c2919ed7267
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-09-15 16:37:23 +0100

    removed unintentional z-index change

--- packages/components/psammead-brand/src/SkipLink/index.jsx
+++ packages/components/psammead-brand/src/SkipLink/index.jsx
@@ -25,7 +25,6 @@ const SkipLink = styled.a`
   border: ${SKIP_LINK_BORDER} solid #000;
   color: ${SKIP_LINK_COLOR};
   text-decoration: none;
-  z-index: 10;
   ${({ script }) => script && getPica(script)};
   ${({ service }) => service && getSansRegular(service)};
 
