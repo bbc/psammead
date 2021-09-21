96f6a46574f5c91fd5020ef1d5bdf2f793271ff8
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-09-21 15:52:48 +0100

    yarns 3 versioning commands must run synchronously

--- scripts/bumpPackages/index.js
+++ scripts/bumpPackages/index.js
@@ -1,56 +1,40 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
+const { readFileSync } = require('fs');
 const getPackagePath = require('../utilities/getPackagePath');
 const getPackages = require('../utilities/getPackages');
 
-const isAlpha = packageDir =>
-  new Promise((resolve, reject) => {
-    exec(
-      'npm version',
-      {
-        cwd: packageDir,
-      },
-      (error, stdout) => {
-        if (error) {
-          reject(error);
-        } else {
-          const alphaRegex = new RegExp(/@bbc.*-alpha/);
-          resolve(alphaRegex.test(stdout));
-        }
-      },
-    );
-  });
+const isAlpha = packageDir => {
+  const { version } = JSON.parse(readFileSync(`${packageDir}/package.json`));
 
-const runExec = async (version, packageDir) => {
+  return version.includes('-alpha');
+};
+
+const runExec = (version, packageDir) => {
   if (!packageDir) {
-    return Promise.resolve();
+    return null;
   }
-  const isAlphaVersion = await isAlpha(packageDir);
+  const isAlphaVersion = isAlpha(packageDir);
   const versionTag = isAlphaVersion ? 'prerelease' : version;
 
-  return new Promise((resolve, reject) => {
-    exec(
-      `yarn version ${versionTag}`,
-      {
-        cwd: packageDir,
-      },
-      error => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      },
-    );
-  });
+  return execSync(
+    `yarn version ${versionTag}`,
+    {
+      cwd: packageDir,
+    },
+    error => {
+      if (error) {
+        console.log(error);
+      }
+    },
+  );
 };
 
 module.exports = ({ packageNames, version }) => {
   const packagePaths = getPackages().map(({ location }) => location);
   const bumpVersion = packageName =>
     runExec(version, getPackagePath(packageName));
-  return Promise.all([
-    packageNames,
-    packagePaths,
-    ...packageNames.map(bumpVersion),
-  ]);
+
+  packageNames.map(bumpVersion);
+
+  return [packageNames, packagePaths];
 };
--- scripts/bumpPackages/index.test.js
+++ scripts/bumpPackages/index.test.js
@@ -1,28 +1,122 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
+const { readFileSync } = require('fs');
 const bumpPackages = require('.');
 
-const packageVersionStub = `{ '@bbc/psammead-test-package': '1.0.1-alpha.4' }`;
-
 jest.mock('../utilities/getPackages', () => () => [
-  { name: '@bbc/psammead-test-package', location: '/psammead-test-package' },
+  {
+    name: '@bbc/psammead-test-package-1',
+    location: '/psammead-test-package-1',
+  },
+  {
+    name: '@bbc/psammead-test-package-2',
+    location: '/psammead-test-package-2',
+  },
+  {
+    name: '@bbc/psammead-test-package-3',
+    location: '/psammead-test-package-3',
+  },
 ]);
 
 jest.mock('child_process', () => ({
-  exec: jest.fn(),
+  execSync: jest.fn(),
 }));
 
-exec.mockImplementation((command, options, cb) => cb(null, packageVersionStub));
+jest.mock('fs', () => ({
+  readFileSync: jest.fn(),
+}));
+
+readFileSync.mockImplementation(() =>
+  JSON.stringify({
+    version: '1.0.0',
+  }),
+);
+
+beforeEach(() => {
+  jest.clearAllMocks();
+
+  readFileSync.mockImplementation(() =>
+    JSON.stringify({
+      version: '1.0.0',
+    }),
+  );
+});
 
 describe('bumpPackages', () => {
   it('should bump alpha versions correctly', async () => {
-    await bumpPackages({
-      packageNames: ['@bbc/psammead-test-package'],
+    readFileSync.mockImplementation(() =>
+      JSON.stringify({
+        version: '1.0.1-alpha.4',
+      }),
+    );
+    bumpPackages({
+      packageNames: ['@bbc/psammead-test-package-1'],
       version: 'minor',
     });
 
-    const command = exec.mock.calls[1][0];
+    const [[command, options]] = execSync.mock.calls;
 
-    expect(exec).toHaveBeenCalledTimes(2);
+    expect(execSync).toHaveBeenCalledTimes(1);
+    expect(options).toEqual({ cwd: '/psammead-test-package-1' });
     expect(command).toBe('yarn version prerelease');
+  });
+
+  it('should bump patch versions correctly', async () => {
+    bumpPackages({
+      packageNames: ['@bbc/psammead-test-package-1'],
+      version: 'patch',
+    });
+
+    const [[command, options]] = execSync.mock.calls;
+
+    expect(execSync).toHaveBeenCalledTimes(1);
+    expect(options).toEqual({ cwd: '/psammead-test-package-1' });
+    expect(command).toBe('yarn version patch');
+  });
+
+  it('should bump minor versions correctly', async () => {
+    bumpPackages({
+      packageNames: ['@bbc/psammead-test-package-1'],
+      version: 'minor',
+    });
+
+    const [[command, options]] = execSync.mock.calls;
+
+    expect(execSync).toHaveBeenCalledTimes(1);
+    expect(options).toEqual({ cwd: '/psammead-test-package-1' });
+    expect(command).toBe('yarn version minor');
+  });
+
+  it('should bump major versions correctly', async () => {
+    bumpPackages({
+      packageNames: ['@bbc/psammead-test-package-1'],
+      version: 'major',
+    });
+
+    const [[command, options]] = execSync.mock.calls;
+
+    expect(execSync).toHaveBeenCalledTimes(1);
+    expect(options).toEqual({ cwd: '/psammead-test-package-1' });
+    expect(command).toBe('yarn version major');
+  });
+
+  it('should bump mulitple packages correctly', async () => {
+    bumpPackages({
+      packageNames: [
+        '@bbc/psammead-test-package-1',
+        '@bbc/psammead-test-package-2',
+        '@bbc/psammead-test-package-3',
+      ],
+      version: 'major',
+    });
+
+    const { calls } = execSync.mock;
+
+    expect(execSync).toHaveBeenCalledTimes(3);
+    expect(calls[0][1]).toEqual({ cwd: '/psammead-test-package-1' });
+    expect(calls[0][0]).toBe('yarn version major');
+    expect(calls[1][1]).toEqual({ cwd: '/psammead-test-package-2' });
+    expect(calls[1][0]).toBe('yarn version major');
+    expect(calls[2][1]).toEqual({ cwd: '/psammead-test-package-3' });
+    expect(calls[2][0]).toBe('yarn version major');
   });
 });
