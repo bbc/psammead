fbe65d7c1beaaa7d4635e331bf7e68994037f8eb
nicola-d-bbc <nicola.dochnenko@bbc.co.uk>
2021-09-29 14:38:12 +0100

    remove old font scripts from gel_foundations

--- packages/utilities/gel-foundations/src/scripts.js
+++ packages/utilities/gel-foundations/src/scripts.js
@@ -1,36 +1,24 @@
 /* eslint-disable import/no-duplicates */
 import arabic from './scripts/arabic';
-
-import bengali from './scripts/bengali';
 import bengaliNotoSerif from './scripts/bengali-noto-serif';
-
 import burmese from './scripts/burmese';
-
 import devanagariAndGurmukhi from './scripts/devanagari-and-gurmukhi';
 import hindi from './scripts/devanagari-and-gurmukhi';
 import nepali from './scripts/devanagari-and-gurmukhi';
-
 import cyrillic from './scripts/latin-and-cyrillic';
 import cyrillicAndLatin from './scripts/latin-and-cyrillic';
 import latin from './scripts/latin-and-cyrillic';
-
 import latinDiacritics from './scripts/latin-with-diacritics';
-
 import chinese from './scripts/no-ascenders-or-descenders';
 import ethiopic from './scripts/no-ascenders-or-descenders';
 import korean from './scripts/no-ascenders-or-descenders';
 import noAscendersOrDescenders from './scripts/no-ascenders-or-descenders';
-
-import sinhalese from './scripts/sinhalese';
 import sinhaleseNotoSerif from './scripts/sinhalese-noto-serif';
-
 import tamil from './scripts/tamil';
-
 import thai from './scripts/thai';
 
 export {
   arabic,
-  bengali,
   bengaliNotoSerif,
   burmese,
   devanagariAndGurmukhi,
@@ -44,7 +32,6 @@ export {
   ethiopic,
   korean,
   noAscendersOrDescenders,
-  sinhalese,
   sinhaleseNotoSerif,
   tamil,
   thai,
