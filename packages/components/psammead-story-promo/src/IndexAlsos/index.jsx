d488115fba6e91e86c320d447287249bbe8728bf
Aaron Moore <aaron.moore@bbc.co.uk>
2021-11-25 11:32:21 +0000

    Update index.jsx

--- packages/components/psammead-story-promo/src/IndexAlsos/index.jsx
+++ packages/components/psammead-story-promo/src/IndexAlsos/index.jsx
@@ -61,7 +61,7 @@ const IndexAlsosLink = ({
       href={url}
       script={script}
       service={service}
-      // Line 63 and id={`IndexAlsosLink-${children}`} in line 68 are temporary fix for the a11y nested span's bug experienced in TalkBack, refer to the following issue: https://github.com/bbc/simorgh/issues/9652
+      // Line 63 and id={`IndexAlsosLink-${sanitisedUrl}`} in line 68 are temporary fix for the a11y nested span's bug experienced in TalkBack, refer to the following issue: https://github.com/bbc/simorgh/issues/9652
       aria-labelledby={`IndexAlsosLink-${sanitisedUrl}`}
     >
       {mediaIndicator ? (
