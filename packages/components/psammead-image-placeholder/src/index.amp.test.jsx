454e6370756db8e1234b5e87a91787562bc504ee
Dario Russo <dario.russo@bbc.co.uk>
2022-02-09 11:46:16 +0000

    feat: Add data-hero to Psammead's amp-img

--- packages/components/psammead-image-placeholder/src/index.amp.test.jsx
+++ packages/components/psammead-image-placeholder/src/index.amp.test.jsx
@@ -5,13 +5,13 @@ import ImagePlaceholderAmp from './index.amp';
 describe('ImagePlaceholderAmp', () => {
   shouldMatchSnapshot(
     'should render normal version correctly',
-    <amp-img src="foo" width="645px" height="128px">
+    <amp-img src="foo" width="645px" height="128px" data-hero>
       <ImagePlaceholderAmp />
     </amp-img>,
   );
   shouldMatchSnapshot(
     'should render dark mode version correctly',
-    <amp-img src="foo" width="645px" height="128px">
+    <amp-img src="foo" width="645px" height="128px" data-hero>
       <ImagePlaceholderAmp darkMode />
     </amp-img>,
   );
